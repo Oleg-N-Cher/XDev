256b21d560b81ae77e350e3a37630e13c8450e3b
Oleg N. Cher <allot@bk.ru>
2012-03-13 20:29:17 +0200

    Add +Clear.bat script.

---
+++ ZXDev/+Clear.bat
@@ -0,0 +1,10 @@
+@DEL *.ihx
+@DEL *.lst
+@DEL *.map
+@DEL *.noi
+@DEL *.rel
+@DEL *.bin
+@DEL *.asm
+@DEL *.lk
+@attrib -H *.sbm
+@DEL *.SBM
---
+++ ZXDev/Libs/+Clear.bat
@@ -0,0 +1,11 @@
+@DEL *.ihx
+@DEL *.lst
+@DEL *.map
+@DEL *.noi
+@DEL *.rel
+@DEL *.sym
+@DEL *.bin
+@DEL *.asm
+@DEL *.lk
+@attrib -H *.sbm
+@DEL *.SBM
