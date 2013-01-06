679ab071e3a9b75111301b91057642aa72be8fb8
Oleg N. Cher <allot@bk.ru>
2013-01-07 01:31:10 +0200

    Update ZXDev building scripts - now paused when SDCC returns error(s) + autostart an emulator.

--- ZXDev/HelloWorld.bat
+++ ZXDev/HelloWorld.bat
@@ -1,23 +1,24 @@
 @REM Compile HelloWorld.c
 @REM ====================
+
+@CD Obj
 @SET bin=..\Bin
 @SET libs=..\Libs
 @SET obj=..\..\Ofront\Obj
-@CD Obj
 
 @IF EXIST %obj%\HelloWorld.h @DEL %obj%\HelloWorld.h
 @IF EXIST %obj%\HelloWorld.sym @DEL %obj%\HelloWorld.sym
 @IF EXIST %obj%\HelloWorld.c @MOVE /Y %obj%\HelloWorld.c
 %bin%\sdcc -mz80 --code-loc 26000 --data-loc 0xF800 --no-std-crt0 --opt-code-size --funsigned-char --disable-warning 126 -I "." -I %libs% -L %libs%/z80 HelloWorld.c Basic.lib
+@IF errorlevel 1 PAUSE
 
 @REM Convert Intel hex format to binary
 @REM ==================================
 %bin%\Hex2bin HelloWorld.ihx
-@REM Bin\stripbin HelloWorld.bin
 
 @REM Link the target to TR-DOS format
 @REM ================================
-REM WARNING! bin2trd is a MS-DOS program, must use a short filename
+@REM WARNING! bin2trd is a MS-DOS program, must use a short filename
 @DEL /Q HELLOW~1.TRD
 %bin%\bin2trd.exe HelloW~1.trl
 @MOVE /Y HELLOW~1.TRD ../HelloWorld.trd
@@ -26,4 +27,5 @@ REM WARNING! bin2trd is a MS-DOS program, must use a short filename
 @ECHO Now you can run the image "HelloWorld.trd" in your favourite emulator
 @REM ======================================================================
 
-@PAUSE
+@CD ..
+START HelloWorld.trd
