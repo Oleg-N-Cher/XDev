09b48873b13f0a20adc5c973a5b683c4f6bd5159
Oleg N. Cher <allot@bk.ru>
2013-02-19 13:36:34 +0200

    Update TCC to ver 0.9.26 from http://download.savannah.gnu.org/releases/tinycc/

--- WinDev/Bin/tcc/include/stddef.h
+++ WinDev/Bin/tcc/include/stddef.h
@@ -1,23 +1,28 @@
 #ifndef _STDDEF_H
 #define _STDDEF_H
 
-#define NULL ((void *)0)
 typedef __SIZE_TYPE__ size_t;
+typedef __PTRDIFF_TYPE__ ssize_t;
 typedef __WCHAR_TYPE__ wchar_t;
 typedef __PTRDIFF_TYPE__ ptrdiff_t;
-#define offsetof(type, field) ((size_t) &((type *)0)->field)
+typedef __PTRDIFF_TYPE__ intptr_t;
+typedef __SIZE_TYPE__ uintptr_t;
 
-/* need to do that because of glibc 2.1 bug (should have a way to test
-   presence of 'long long' without __GNUC__, or TCC should define
-   __GNUC__ ? */
-#if !defined(__int8_t_defined) && !defined(__dietlibc__)
+#ifndef __int8_t_defined
 #define __int8_t_defined
-typedef char int8_t;
-typedef short int int16_t;
-typedef int int32_t;
-typedef long long int int64_t;
+typedef signed char int8_t;
+typedef signed short int int16_t;
+typedef signed int int32_t;
+typedef signed long long int int64_t;
+typedef unsigned char uint8_t;
+typedef unsigned short int uint16_t;
+typedef unsigned int uint32_t;
+typedef unsigned long long int uint64_t;
 #endif
 
-void *alloca(size_t);
+#define NULL ((void*)0)
+#define offsetof(type, field) ((size_t)&((type *)0)->field)
+
+void *alloca(size_t size);
 
 #endif
