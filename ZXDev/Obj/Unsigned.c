dc813df92b7c1647f5a3d6972fd290f89f4f65a5
Oleg N. Cher <allot@bk.ru>
2013-02-25 12:59:58 +0200

    Add to 'ZXDev/Mod/Unsigned' print unsigned byte & word.

--- ZXDev/Obj/Unsigned.c
+++ ZXDev/Obj/Unsigned.c
@@ -35,5 +35,10 @@ export void *Unsigned__init(void)
 		Basic_PRCHAR(' ');
 		Unsigned_byte += 1;
 	}
+	Basic_INK(4);
+	Unsigned_byte = Platform_Unsigned(-1);
+	Basic_PRWORD(Unsigned_byte);
+	Basic_PRCHAR(' ');
+	Basic_PRWORD(Platform_Unsigned(-1));
 	__ENDMOD;
 }
