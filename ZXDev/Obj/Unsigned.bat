2cbd2df319bc85564450235887599da857cfd99b
Oleg N. Cher <allot@bk.ru>
2013-02-25 05:11:37 +0200

    Add 'ZXDev/Mod/Unsigned' example 'how to work with unsigned numbers in ZXDev/Ofront'.

---
+++ ZXDev/Obj/Unsigned.bat
@@ -0,0 +1,27 @@
+@REM Compile Unsigned.c
+@REM ==================
+
+@SET bin=..\Bin
+@SET libs=..\Libs
+@SET obj=..\..\Ofront\Obj
+
+@IF EXIST %obj%\Unsigned.h @DEL %obj%\Unsigned.h
+@IF EXIST %obj%\Unsigned.sym @DEL %obj%\Unsigned.sym
+@IF EXIST %obj%\Unsigned.c @MOVE /Y %obj%\Unsigned.c
+%bin%\sdcc -mz80 --code-loc 26000 --data-loc 0xF800 --no-std-crt0 --opt-code-size --funsigned-char -I "." -I %libs% -L %libs%/z80 Unsigned.c Basic.lib
+@IF errorlevel 1 PAUSE
+
+@REM Convert Intel hex format to binary
+@REM ==================================
+%bin%\Hex2bin Unsigned.ihx
+
+@REM Link the target to TAP format
+@REM =============================
+@DEL /Q Unsigned.tap
+%bin%\bin2data.exe -rem -org 26000 Unsigned.bin ..\Unsigned.tap Unsigned
+
+@REM =====================================================================
+@ECHO Now you can run the image "Unsigned.tap" in your favourite emulator.
+@REM =====================================================================
+
+START ..\Unsigned.tap
