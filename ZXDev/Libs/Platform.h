3081f87bbc18dff68897abe89216e63b6a852a1c
Oleg N. Cher <allot@bk.ru>
2013-02-25 05:08:57 +0200

    Add 'ZXDev/Libs/Platform'.

---
+++ ZXDev/Libs/Platform.h
@@ -0,0 +1,15 @@
+/*  Ofront 1.2 -xtspkae */
+
+#ifndef Platform__h
+#define Platform__h
+
+#include "SYSTEM.h"
+
+
+
+
+#define Platform_Unsigned(i) ((unsigned)(i))
+#define Platform__init()
+
+
+#endif
