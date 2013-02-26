7bc68665819dc416593f2409c18c5aaf60b1f467
Oleg N. Cher <allot@bk.ru>
2013-02-26 21:17:01 +0200

    Update 'ZXDev/' scripts (to lowercase 'Hex2bin').

--- ZXDev/Obj/HelloWorld.bat
+++ ZXDev/Obj/HelloWorld.bat
@@ -13,7 +13,7 @@
 
 @REM Convert Intel hex format to binary
 @REM ==================================
-%bin%\Hex2bin HelloWorld.ihx
+%bin%\hex2bin HelloWorld.ihx
 
 @REM Link the target to TAP format
 @REM =============================
