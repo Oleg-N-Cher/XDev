c376a9767eff161003d177a403ae54571baca63b
Oleg N. Cher <allot@bk.ru>
2013-02-26 21:39:32 +0200

    Update script 'ZXDev/Libs/+BasicSmart.bat'.

--- ZXDev/Libs/+BasicSmart.bat
+++ ZXDev/Libs/+BasicSmart.bat
@@ -1,12 +1,13 @@
 @REM Compile the library Basic
 @REM =========================
+@SET rootbin=..\..\Bin
 @SET bin=..\Bin
 @SET obj=..\..\Ofront\Obj
 
 @IF EXIST %obj%\Basic.h @DEL %obj%\Basic.h
 @IF EXIST %obj%\Basic.c @DEL %obj%\Basic.c
 
-..\Bin\smartlinkrel Basic.c
+%rootbin%\smartlinkrel Basic.c
 @FOR %%i IN (Basic_0??.c) DO %bin%\sdcc -c -mz80 --no-std-crt0 --opt-code-size --funsigned-char --disable-warning 59 --disable-warning 85 -I "..\Obj" -L z80 %%i
 
 @IF EXIST Basic.lib DEL Basic.lib
