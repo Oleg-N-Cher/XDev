59c0ae58d239877740f21b37f4ac56bba96c18aa
Oleg N. Cher <allot@bk.ru>
2013-03-02 06:14:39 +0200

    Update 'ZXDev/SYSTEM' (add var SYSTEM_str_arg).

---
+++ ZXDev/Lib/SYSTEM.c
@@ -0,0 +1,8 @@
+#include "SYSTEM.h"
+
+/* runtime system routines */
+
+/*================================== Header ==================================*/
+/* runtime system variables */
+CHAR *SYSTEM_str_arg;
+
