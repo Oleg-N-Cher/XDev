7acdb0917ef587228948e8c621dcd174e4fecf1f
Oleg N. Cher <allot@bk.ru>
2013-03-04 10:00:58 +0200

    Update scripts 'ZXDev/Bin/compile.bat' & 'build.bat' (fix a bug).

--- ZXDev/Bin/build.bat
+++ ZXDev/Bin/build.bat
@@ -4,8 +4,8 @@
 @SET Lib=..\Lib
 @SET Obj=..\..\Ofront\Obj
 
-@IF EXIST %Obj%\%1.h @DEL %Obj%\HelloWorld.h
-@IF EXIST %Obj%\%1.sym @DEL %Obj%\HelloWorld.sym
+@IF EXIST %Obj%\%1.h @DEL %Obj%\%1.h
+@IF EXIST %Obj%\%1.sym @DEL %Obj%\%1.sym
 @IF EXIST %Obj%\%1.c @MOVE /Y %Obj%\%1.c
 %Bin%\sdcc %1.c -mz80 --code-loc %CodeAddr% --data-loc %DataAddr% --no-std-crt0 --opt-code-size -I "." -I %Lib% XDev.lib Basic.lib Laser.lib
 @IF errorlevel 1 PAUSE
