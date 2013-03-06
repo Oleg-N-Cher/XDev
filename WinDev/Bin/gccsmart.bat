289b5299010eae6836478c47aed183af3b3ba327
Oleg N. Cher <allot@bk.ru>
2013-03-06 07:15:06 +0200

    Rename 'Bin/smartlinkrel' -> 'smartlib'.

--- WinDev/Bin/gccsmart.bat
+++ WinDev/Bin/gccsmart.bat
@@ -1,8 +1,8 @@
 @SET path=d:\WIN32APP\Dev\MinGW\bin
 @SET bin=..\Bin
-@SET gcc=gcc.exe -O3 -s -fno-strict-aliasing
+@SET gcc=gcc.exe -O3 -s -flto -fno-strict-aliasing -fomit-frame-pointer
 %gcc% -c %1.c
 @IF ERRORLEVEL 1 @PAUSE
-@%bin%\smartlinkrel %1.c
+@%bin%\smartlib %1.c
 @FOR %%i IN (%1_0??.c) DO %gcc% -c %%i
 @FOR %%i IN (%1_0??.o) DO %bin%\ar -rc OfrontGCC.a %%i
