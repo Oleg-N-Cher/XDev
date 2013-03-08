93e6d82a0d32714468e809012016c5028f1125c0
Oleg N. Cher <allot@bk.ru>
2013-03-08 21:34:37 +0200

    Add to ZXDev Obj/Sym subdirectories.

---
+++ ZXDev/Obj/HelloWorld.h
@@ -0,0 +1,15 @@
+/*  Ofront 1.2 -xtspkae */
+
+#ifndef HelloWorld__h
+#define HelloWorld__h
+
+#include "SYSTEM.h"
+
+
+
+
+import void HelloWorld_Main (void);
+import void *HelloWorld__init(void);
+
+
+#endif
