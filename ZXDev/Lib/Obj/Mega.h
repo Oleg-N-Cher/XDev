6d68a04dccc97dc28b3e66d2bdd4b0583389e234
Oleg N. Cher <allot@bk.ru>
2013-04-19 22:00:36 +0300

    Add 'ZXDev/Lib/Mega' & sample 'DemoInvert'.

---
+++ ZXDev/Lib/Obj/Mega.h
@@ -0,0 +1,16 @@
+/*  Ofront 1.2 -xtspkae */
+
+#ifndef Mega__h
+#define Mega__h
+
+#include "SYSTEM.h"
+
+
+
+
+import void Mega_INVERT (void);
+import void Mega_PLAY (BOOLEAN sound, INTEGER stepLen, INTEGER startFreq, LONGINT stepNum, INTEGER incFreq);
+import void *Mega__init(void);
+
+
+#endif
