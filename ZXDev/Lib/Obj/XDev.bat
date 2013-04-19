55f4ab084bebfdf013b92d79a9b0653e8be89888
Oleg N. Cher <allot@bk.ru>
2013-04-19 20:00:45 +0300

    Update /WinDev scripts.

--- ZXDev/Lib/Obj/XDev.bat
+++ ZXDev/Lib/Obj/XDev.bat
@@ -2,8 +2,10 @@
 @REM ======================
 
 @IF EXIST ..\XDev.lib ..\DEL XDev.lib
+@CALL ..\Bin\build-xdev.bat SYSTEM
+@CALL ..\Bin\build-xdev.bat Platform
 @CALL ..\Bin\build-xdev.bat Console
 @CALL ..\Bin\build-xdev.bat GrPixel
 @CALL ..\Bin\build-xdev.bat Math
-@CALL ..\Bin\build-xdev.bat SYSTEM
+
 @MOVE /Y ..\XDev.lib ..\z80\XDev.lib
