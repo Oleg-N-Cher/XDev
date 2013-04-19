dfb6d249205c1fc6d9b542c3069f7558253e8b30
Oleg N. Cher <allot@bk.ru>
2013-04-19 20:00:45 +0300

    Update /WinDev scripts.

--- ZXDev/Lib/Bin/compile.bat
+++ ZXDev/Lib/Bin/compile.bat
@@ -1,3 +1,3 @@
 @CD ..
-..\Bin\sdcc -c %1.c -mz80 --opt-code-size --disable-warning 59 --disable-warning 85 -I "." -L z80
+..\Bin\sdcc -c %1.c -mz80 --opt-code-size --disable-warning 59 --disable-warning 85 -I "." -I Obj -L z80
 @IF errorlevel 1 PAUSE
