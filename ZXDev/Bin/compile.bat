918ed1afddcbeed3297518cbda9546aec52ea23a
Oleg N. Cher <allot@bk.ru>
2013-04-27 18:08:43 +0300

    Update 'ZXDev/*' scripts.

--- ZXDev/Bin/compile.bat
+++ ZXDev/Bin/compile.bat
@@ -1,9 +1,5 @@
 @SET Bin=..\Bin
 @SET Lib=..\Lib
-@SET Obj=..\..\Ofront\Obj
 
-@IF EXIST %Obj%\%1.h @DEL %Obj%\%1.h
-@IF EXIST %Obj%\%1.sym @DEL %Obj%\%1.sym
-@IF EXIST %Obj%\%1.c @MOVE /Y %Obj%\%1.c
 %Bin%\sdcc -c %1.c -mz80 --opt-code-size -I "." -I %Lib%
 @IF errorlevel 1 PAUSE
