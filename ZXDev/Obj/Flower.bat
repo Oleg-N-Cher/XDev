c7b1ad091beb9fdeefbc3c869f6951f2bae279b6
Oleg N. Cher <allot@bk.ru>
2013-04-28 14:38:45 +0300

    Add 'ZXDev/Flower' example - http://zx.pk.ru/showpost.php?p=596484&postcount=67

---
+++ ZXDev/Obj/Flower.bat
@@ -0,0 +1,28 @@
+@REM Compile Flower.c
+@REM ================
+
+@SET CodeAddr=38000
+@SET DataAddr=45000
+@SET StartCode=
+@SET bin=..\Bin
+@SET lib=..\Lib
+
+%bin%\sdcc -mz80 --code-loc %CodeAddr% --data-loc %DataAddr% --no-std-crt0 --opt-code-size --funsigned-char --disable-warning 126 -I "." -I %lib% -L %lib%/z80 Flower.c Graph.lib XDev.lib
+@IF errorlevel 1 PAUSE
+
+@REM Convert Intel hex format to binary
+@REM ==================================
+%bin%\hex2bin Flower.ihx
+
+@REM %bin%\stripbin Flower.bin
+
+@REM Link the target to TAP format
+@REM =============================
+@DEL /Q Flower.tap
+%bin%\bin2data.exe -rem -org %CodeAddr% Flower.bin ..\Flower.tap Flower
+
+@REM ======================================================================
+@ECHO Now you can run the image "Flower.tap" in your favourite emulator
+@REM ======================================================================
+
+START ..\Flower.tap
---
+++ ZXDev/Obj/Flower.h
@@ -0,0 +1,14 @@
+/*  Ofront 1.2 -xtspkae */
+
+#ifndef Flower__h
+#define Flower__h
+
+#include "SYSTEM.h"
+
+
+
+
+import void *Flower__init(void);
+
+
+#endif
