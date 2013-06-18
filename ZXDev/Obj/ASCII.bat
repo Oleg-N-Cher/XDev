860947007ed96843fef56eb18644133f189fa81a
Oleg N. Cher <allot@bk.ru>
2013-06-18 22:14:41 +0300

    Add script 'ZXDev/Obj/ASCII.bat'.

---
+++ ZXDev/Obj/ASCII.bat
@@ -0,0 +1,14 @@
+@SET Mod=ASCII
+@SET Bin=..\Bin
+@SET Lib=..\Lib
+@SET CodeAddr=45056
+@SET DataAddr=63488
+
+%Bin%\sdcc %Mod%.c -mz80 --code-loc %CodeAddr% --data-loc %DataAddr% --no-std-crt0 --opt-code-size --funsigned-char --disable-warning 126 -I "." -I %Lib% -L %Lib%/z80 Basic.lib XDev.lib
+@IF errorlevel 1 PAUSE
+
+@REM Convert Intel hex format to binary
+@REM ==================================
+%Bin%\hex2bin %Mod%.ihx
+%Bin%\bin2data.exe -rem -org %CodeAddr% %Mod%.bin ..\%Mod%.tap %Mod%
+@START ..\%Mod%.tap
