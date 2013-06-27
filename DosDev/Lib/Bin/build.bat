6ece8b719153f3553c1550639c075bc89cab2d66
Oleg N. Cher <allot@bk.ru>
2013-06-28 00:58:38 +0300

    Update DosDev. Now 'smartlib' tool is used for build libraries.

--- DosDev/Lib/Bin/build.bat
+++ DosDev/Lib/Bin/build.bat
@@ -1,5 +1,22 @@
 @CD ..
 @SET path=..\BIN\TC201\BIN
-tcc -1 -O -d -f87 -Z -I..\BIN\TC201\INCLUDE -IOBJ -L..\BIN\TC201\LIB -c %1.c
-@IF errorlevel 1 PAUSE
-TLIB /C dosdev +Console
+@SET RootBin=..\..\Bin
+@IF EXIST %1.lib DEL %1.lib
+%RootBin%\smartlib %1.c %1
+@FOR %%i IN (%1???.c) DO (
+  tcc -1 -O -d -f87 -Z -I..\BIN\TC201\INCLUDE -IOBJ -L..\BIN\TC201\LIB -c %%i
+  @IF errorlevel 1 PAUSE
+)
+@FOR %%i IN (%1???.o) DO TLIB /C %1 +%%i
+@PAUSE
+@Bin\clear
+
+
+
+
+
+
+
+
+
+
