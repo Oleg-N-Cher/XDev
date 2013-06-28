566a255e7c4d585cca3622b426cccdfc1e06fd02
Oleg N. Cher <allot@bk.ru>
2013-06-28 17:11:18 +0300

    Update 'ZXDev' scritps.

--- ZXDev/Obj/+Clear.bat
+++ ZXDev/Obj/+Clear.bat
@@ -9,3 +9,15 @@
 @DEL *.lk
 @ATTRIB -H *.sbm
 @DEL *.sbm
+@CD IM2
+@DEL *.ihx
+@DEL *.lst
+@DEL *.map
+@DEL *.noi
+@DEL *.rel
+@DEL *.sym
+@DEL *.bin
+@DEL *.asm
+@DEL *.lk
+@ATTRIB -H *.sbm
+@DEL *.sbm
--- ZXDev/Obj/TinyHello.bat
+++ ZXDev/Obj/TinyHello.bat
@@ -3,12 +3,8 @@
 
 @SET bin=..\Bin
 @SET lib=..\Lib
-@SET obj=..\..\Ofront\Obj
 
-@IF EXIST %obj%\TinyHello.h @DEL %obj%\TinyHello.h
-@IF EXIST %obj%\TinyHello.sym @DEL %obj%\TinyHello.sym
-@IF EXIST %obj%\TinyHello.c @MOVE /Y %obj%\TinyHello.c
-%bin%\sdcc -mz80 --code-loc 26000 --data-loc 0xF800 --no-std-crt0 --opt-code-size --funsigned-char -I "." -I %lib% -L %lib%/z80 TinyHello.c Basic.lib
+%bin%\sdcc TinyHello.c -mz80 --code-loc 26000 --data-loc 0xF800 --no-std-crt0 --opt-code-size --funsigned-char -I "." -I %lib% -L %lib%/z80 Basic.lib
 @IF errorlevel 1 PAUSE
 
 @REM Convert Intel hex format to binary
