acbca74957320181614f20e249db17d60a3eaeb7
Oleg N. Cher <allot@bk.ru>
2013-07-04 01:05:03 +0300

    ZXDev: Convert example 'HelloWorld' to .Mod, add (**), rebuild.

--- ZXDev/Obj/HelloWorld.c
+++ ZXDev/Obj/HelloWorld.c
@@ -1,30 +1,37 @@
-/*  Ofront 1.2 -xtspkae */
+/*  Ofront 1.2 -xtspkaem */
 #include "SYSTEM.h"
 #include "Basic.h"
 
 
+static INTEGER HelloWorld_x, HelloWorld__for__1;
 
 
-export void HelloWorld_Main (void);
 
 
-void HelloWorld_Main (void)
+
+export main(int argc, char **argv)
 {
-	INTEGER x;
+	__INIT(argc, argv);
+	__IMPORT(Basic);
+	__REGMAIN("HelloWorld", 0);
+/* BEGIN */
 	Basic_Init();
 	Basic_BORDER(4);
 	Basic_PAPER(0);
 	Basic_CLS();
-	x = 0;
-	while (x <= 245) {
+	HelloWorld_x = 0;
+	HelloWorld__for__1 = 245;
+	HelloWorld__for__1 = __ASHR(HelloWorld__for__1 - HelloWorld_x, 1) + 1;
+	do {
 		Basic_INK(6);
-		Basic_PLOT(x, 127);
+		Basic_PLOT(HelloWorld_x, 127);
 		Basic_DRAW(10, 10);
 		Basic_INK(3);
-		Basic_PLOT(x, 37);
+		Basic_PLOT(HelloWorld_x, 37);
 		Basic_DRAW(10, 10);
-		x += 2;
-	}
+		HelloWorld_x += 2;
+		HelloWorld__for__1 -= 1;
+	} while (!(HelloWorld__for__1 == 0));
 	Basic_AT(11, 1);
 	Basic_PAPER(1);
 	Basic_INK(5);
@@ -34,15 +41,5 @@ void HelloWorld_Main (void)
 	Basic_FLASH(0);
 	Basic_PAUSE(0);
 	Basic_Quit();
-}
-
-
-export void *HelloWorld__init(void)
-{
-	__DEFMOD;
-	__IMPORT(Basic);
-	__REGMOD("HelloWorld", 0);
-	__REGCMD("Main", HelloWorld_Main);
-/* BEGIN */
-	__ENDMOD;
+	__FINI;
 }
