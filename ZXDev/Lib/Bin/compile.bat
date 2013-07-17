b87f0cecd8279e3ac1a756ef12d8f5805ac39272
Oleg N. Cher <allot@bk.ru>
2013-07-17 18:29:30 +0300

    ZXDev: Update 'Lib/Bin' scripts to work with 'malloc', add 'Lib/include'.

--- ZXDev/Lib/Bin/compile.bat
+++ ZXDev/Lib/Bin/compile.bat
@@ -1,3 +1,3 @@
 @CD ..
-..\Bin\sdcc %1.c -mz80 --opt-code-size --no-std-crt0 --disable-warning 59 --disable-warning 85 -I "." -I Obj -L z80 XDev.lib
+..\Bin\sdcc %1.c -mz80 --opt-code-size --no-std-crt0 --disable-warning 59 --disable-warning 85 -I "." -I include -I Obj -L z80 XDev.lib
 @IF errorlevel 1 PAUSE
