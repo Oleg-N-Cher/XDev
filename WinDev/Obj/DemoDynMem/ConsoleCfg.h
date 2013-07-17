d86b774bbd7cb4b01f170e05c750623f52ebb24c
Oleg N. Cher <allot@bk.ru>
2013-07-17 18:35:11 +0300

    WinDev: Add example 'DemoDynMem'.

---
+++ WinDev/Obj/DemoDynMem/ConsoleCfg.h
@@ -0,0 +1,10 @@
+#ifndef __Console_Cfg_h
+#define __Console_Cfg_h
+
+/* Output way - conio.h, stdio.h or windows.h */
+
+//#define OUTPUT_CONIO
+#define OUTPUT_STDIO
+//#define OUTPUT_WINAPI
+
+#endif __Console_Cfg_h
