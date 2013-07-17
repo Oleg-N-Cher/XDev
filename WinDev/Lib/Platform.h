1b7d4e5ead136cd08ec2b2340ecaefc323522178
Oleg N. Cher <allot@bk.ru>
2013-07-17 18:43:38 +0300

    WinDev: Update 'Lib/Platform'.

--- WinDev/Lib/Platform.h
+++ WinDev/Lib/Platform.h
@@ -12,7 +12,7 @@ typedef
 
 
 import SET Platform_BITS (INTEGER i);
-import void Platform_DISPOSE (SYSTEM_PTR mem);
+import void Platform_DISPOSE (SYSTEM_PTR *mem);
 import INTEGER Platform_ORD (SET s);
 #define Platform_Unsigned(i) ((unsigned)(i))
 #define Platform__init()
