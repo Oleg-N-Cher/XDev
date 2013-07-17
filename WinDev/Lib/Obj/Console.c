619068cf1a7c2d6d475494ba2289a25f5554eb5e
Oleg N. Cher <allot@bk.ru>
2013-07-17 18:38:38 +0300

    WinDev: Update 'Lib/Console'.

--- WinDev/Lib/Obj/Console.c
+++ WinDev/Lib/Obj/Console.c
@@ -7,7 +7,9 @@
 export void Console_At (INTEGER x, INTEGER y);
 export void Console_SetColors (INTEGER n);
 export void Console_WriteCh (CHAR ch);
+export void Console_WriteHex (INTEGER x);
 export void Console_WriteInt (LONGINT n);
+export void Console_WriteIntWidth (INTEGER x, INTEGER n);
 export void Console_WriteLn (void);
 export void Console_WriteStr (CHAR *str, LONGINT str__len);
 export void Console_WriteStrLn (CHAR *str, LONGINT str__len);
@@ -47,6 +49,60 @@ void Console_WriteCh (CHAR ch)
 void Console_WriteLn (void)
 {
 	Console_writeLn();
+}
+
+void Console_WriteIntWidth (INTEGER x, INTEGER n)
+{
+	INTEGER i, x0;
+	CHAR a[10];
+	if (x < 0) {
+		if (x == (-2147483647-1)) {
+			Console_writeStr((CHAR*)"-2147483648", (LONGINT)12);
+			return;
+		}
+		n -= 1;
+		x0 = -x;
+	} else {
+		x0 = x;
+	}
+	i = 0;
+	do {
+		a[__X(i, 10)] = (CHAR)((int)__MOD(x0, 10) + 48);
+		x0 = __DIV(x0, 10);
+		i += 1;
+	} while (!(x0 == 0));
+	while (n > i) {
+		Console_writeCh(' ');
+		n -= 1;
+	}
+	if (x < 0) {
+		Console_writeCh('-');
+	}
+	do {
+		i -= 1;
+		Console_writeCh(a[__X(i, 10)]);
+	} while (!(i == 0));
+}
+
+void Console_WriteHex (INTEGER x)
+{
+	INTEGER i, y;
+	CHAR a[10];
+	i = 0;
+	do {
+		y = __MASK(x, -16);
+		if (y < 10) {
+			a[__X(i, 10)] = (CHAR)(y + 48);
+		} else {
+			a[__X(i, 10)] = (CHAR)(y + 55);
+		}
+		x = __ASHR(x, 4);
+		i += 1;
+	} while (!(i == 8));
+	do {
+		i -= 1;
+		Console_writeCh(a[__X(i, 10)]);
+	} while (!(i == 0));
 }
 
 void Console_SetColors (INTEGER n)
--- WinDev/Lib/Obj/Console.h
+++ WinDev/Lib/Obj/Console.h
@@ -11,7 +11,9 @@
 import void Console_At (INTEGER x, INTEGER y);
 import void Console_SetColors (INTEGER n);
 import void Console_WriteCh (CHAR ch);
+import void Console_WriteHex (INTEGER x);
 import void Console_WriteInt (LONGINT n);
+import void Console_WriteIntWidth (INTEGER x, INTEGER n);
 import void Console_WriteLn (void);
 import void Console_WriteStr (CHAR *str, LONGINT str__len);
 import void Console_WriteStrLn (CHAR *str, LONGINT str__len);
