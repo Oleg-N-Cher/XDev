4f2472582dccf3ce240e270b7c7f817346a3c22c
Oleg N. Cher <allot@bk.ru>
2013-08-09 00:56:16 +0300

    WinDev/Lib: Add 'GrFonts'.

---
+++ WinDev/Lib/Obj/GrFonts.h
@@ -0,0 +1,19 @@
+/*  Ofront 1.2 -xtspkae */
+
+#ifndef GrFonts__h
+#define GrFonts__h
+
+#include "SYSTEM.h"
+
+typedef
+	SYSTEM_BYTE *GrFonts_Font;
+
+
+import SYSTEM_BYTE GrFonts_ZxSpecROM8x8[768];
+import SYSTEM_BYTE GrFonts_MsDosCGA8x8[2048];
+
+
+import void *GrFonts__init(void);
+
+
+#endif
