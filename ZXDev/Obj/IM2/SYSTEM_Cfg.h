2ad598e35fa73f8e026a76f2826b494c2c8027ea
Oleg N. Cher <allot@bk.ru>
2013-08-08 12:11:25 +0300

    ZXDev: fix a bug in SYSTEM.h (to init modules just once).

--- ZXDev/Obj/IM2/SYSTEM_Cfg.h
+++ ZXDev/Obj/IM2/SYSTEM_Cfg.h
@@ -4,17 +4,14 @@
 #define __SYSTEM_Cfg__h
 
 /* runtime system routines */
-
 //#define SYSTEM_Cfg_IncRef
 
 /* module registry */
-
 //#define SYSTEM_Cfg_RegisterModules
 //#define SYSTEM_Cfg_RegisterMain
 //#define SYSTEM_Cfg_RegisterCommands
 
 /* record type descriptors */
-
 //#define SYSTEM_Cfg_RecTypeDesc
 //#define SYSTEM_Cfg_InitTypes
 
@@ -22,11 +19,16 @@
 //#define SYSTEM_Cfg_TypeBoundProcDynCalls
 
 /* std procs and operator mappings */
+#define SYSTEM_Cfg_DIV_as_in_C
 #define SYSTEM_Cfg_MOD_as_in_C
 #define SYSTEM_Cfg_NoASSERT
 
 /* runtime checks */
 #define SYSTEM_Cfg_NoCheck_X
 #define SYSTEM_Cfg_NoCheck_CASE
+
+/* Garbage collector */
+//#define SYSTEM_Cfg_NoGC
+//#define SYSTEM_Cfg_Use_HEAP
 
 #endif
