0ea6ac6311eed18194723c38d506122cbaa25b8a
Oleg N. Cher <allot@bk.ru>
2013-08-09 00:43:37 +0300

    WinDev: Update build scripts.

--- WinDev/Bin/build.bat
+++ WinDev/Bin/build.bat
@@ -2,7 +2,15 @@
 @SET Lib=..\Lib
 @SET tcc=%Bin%\tcc\tcc
 
+@IF EXIST %1 GOTO config
+
 %tcc% %1.c -I "." -I %Lib% %Lib%\WinDev.a %Lib%\Sdl\SdlLib.a %Bin%\tcc\lib\SDL.def -o %1.exe
+GOTO exit
+
+:config
+%tcc% %1.c -I %1 -I %Lib% %Lib%\WinDev.a %Lib%\Sdl\SdlLib.a %Bin%\tcc\lib\SDL.def -o %1.exe
+
+:exit
 @IF errorlevel 1 PAUSE
 @IF EXIST %1.exe MOVE %1.exe ..
 @CLS
--- WinDev/Bin/compile.bat
+++ WinDev/Bin/compile.bat
@@ -2,5 +2,13 @@
 @SET Lib=..\Lib
 @SET tcc=%Bin%\tcc\tcc
 
+@IF EXIST %1 GOTO config
+
 %tcc% -c %1.c -I "." -I %Lib%
+GOTO exit
+
+:config
+%tcc% -c %1.c -I %1 -I %Lib%
+
+:exit
 @IF errorlevel 1 PAUSE
