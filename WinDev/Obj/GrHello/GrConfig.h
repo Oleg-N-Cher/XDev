a1e6d3030fea66f064f485eca91877d297c980cf
Oleg N. Cher <allot@bk.ru>
2013-08-21 16:50:15 +0300

    WinDev: Add sample 'GrHello'.

---
+++ WinDev/Obj/GrHello/GrConfig.h
@@ -0,0 +1,17 @@
+#ifndef __GrConfig__h
+#define __GrConfig__h
+
+
+#define GrConfig_Init \
+  GrScr_AppTitle	= "XDev/SDL Graphic"; \
+  GrScr_ActualWidth	= 256; \
+  GrScr_ActualHeight	= 192; \
+  GrScr_VirtualWidth	= 256; \
+  GrScr_VirtualHeight	=  64; \
+  GrScr_DepthBits	=   0; \
+  GrScr_NormalColor	=   0xAA
+
+#define ChkOutOfScreen	1
+
+
+#endif __GrConfig__h
