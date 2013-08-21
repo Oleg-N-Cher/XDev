bb4476d5313adba24dccab8c018b10cc4a51f0d0
Oleg N. Cher <allot@bk.ru>
2013-08-21 15:52:04 +0300

    WinDev/Lib: Update 'GrConfig'.

--- WinDev/Lib/Obj/GrConfig.h
+++ WinDev/Lib/Obj/GrConfig.h
@@ -2,25 +2,10 @@
 #define __GrConfig__h
 
 
-/* Virtual Width, Height in pixels: */
-#define VirtualWidth	320
-#define VirtualHeight	240
+#define GrConfig_Init
 
-/* Actual Width, Height in pixels: */
-#define ActualWidth	640
-#define ActualHeight	480
-#define MaxActualX	(ActualWidth - 1)
-#define MaxActualY	(ActualHeight - 1)
-
-/* Depth in bits per pixel (8, 16, or 32): */
-#define AutoDetect	0
-#define DepthBits	AutoDetect
-
-#define AppTitle "XDev/SDL graphic"
 
 #define ChkOutOfScreen	1
 
-
-#define GrConfig__init()
 
 #endif __GrConfig__h
