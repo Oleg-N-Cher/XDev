211e8f2ece714303148ee83330d67a31b9db6560
Oleg N. Cher <allot@bk.ru>
2013-09-10 00:42:19 +0300

    ZXDev: Add library 'Best40' & example 'Best40Test'.

---
+++ ZXDev/Obj/Best40Test.h
@@ -0,0 +1,15 @@
+/*  Ofront 1.2 -xtspkae */
+
+#ifndef Best40Test__h
+#define Best40Test__h
+
+#include "SYSTEM.h"
+
+
+
+
+import void Best40Test_Main (void);
+import void *Best40Test__init(void);
+
+
+#endif
