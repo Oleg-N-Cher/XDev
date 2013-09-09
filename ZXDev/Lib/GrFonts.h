48f127bdf31f9355d888871603297b015896f869
Oleg N. Cher <allot@bk.ru>
2013-09-09 20:51:32 +0300

    ZXDev/Lib: Add 'GrFonts'.

---
+++ ZXDev/Lib/GrFonts.h
@@ -0,0 +1,18 @@
+/*  Ofront 1.2 -xtspkae */
+
+#ifndef GrFonts__h
+#define GrFonts__h
+
+#include "SYSTEM.h"
+
+typedef
+	SYSTEM_BYTE *GrFonts_Font;
+
+
+#define GrFonts_ZxSpecRom8x8 0x3D00
+import SYSTEM_BYTE GrFonts_MsDosCga8x8[768];
+
+
+#define GrFonts__init()
+
+#endif
---
+++ ZXDev/Lib/Obj/GrFonts.h
@@ -0,0 +1,18 @@
+/*  Ofront 1.2 -xtspkae */
+
+#ifndef GrFonts__h
+#define GrFonts__h
+
+#include "SYSTEM.h"
+
+typedef
+	SYSTEM_BYTE *GrFonts_Font;
+
+
+import SYSTEM_BYTE GrFonts_ZxSpecRom8x8[768], GrFonts_MsDosCga8x8[768];
+
+
+import void *GrFonts__init(void);
+
+
+#endif
