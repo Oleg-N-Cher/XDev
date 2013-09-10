7ad987f3046e20b65e50882fa1bd766a314f9624
Oleg N. Cher <allot@bk.ru>
2013-09-10 23:48:56 +0300

    ZXDev: Update example 'IM2'.

--- ZXDev/Obj/IM2.bat
+++ ZXDev/Obj/IM2.bat
@@ -4,8 +4,8 @@
 @SET CodeAddr=26000
 @SET DataAddr=60000
 
-@IF EXIST %Mod%.c @MOVE /Y %Mod%.c IM2
-@CD IM2
+@IF EXIST %Mod%.c @MOVE /Y %Mod%.c %Mod%
+@CD %Mod%
 %Bin%\sdcc %Mod%.c -mz80 --code-loc %CodeAddr% --data-loc %DataAddr% --no-std-crt0 --opt-code-size --funsigned-char --disable-warning 126 -I "." -I %Lib% -L %Lib%/z80 Basic.lib XDev.lib
 @IF errorlevel 1 PAUSE
 
