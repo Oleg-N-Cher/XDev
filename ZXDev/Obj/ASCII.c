82f5aff2c01ab3845e13fec8fdde98bc193fa4fc
Oleg N. Cher <allot@bk.ru>
2013-09-11 00:14:14 +0300

    ZXDev: Update example 'ASCII' - actualize.

--- ZXDev/Obj/ASCII.c
+++ ZXDev/Obj/ASCII.c
@@ -1,24 +1,27 @@
-/*  Ofront 1.2 -xtspkae */
+/*  Ofront 1.2 -xtspkaem */
 #include "SYSTEM.h"
 #include "Console.h"
 
 
-static INTEGER ASCII_n;
+static INTEGER ASCII_n, ASCII__for__1;
 
 
 
 
 
-export void *ASCII__init(void)
+export main(int argc, char **argv)
 {
-	__DEFMOD;
-	__IMPORT(Console);
-	__REGMOD("ASCII", 0);
+	__INIT(argc, argv);
+	__IMPORT(Console__init);
+	__REGMAIN("ASCII", 0);
 /* BEGIN */
 	ASCII_n = 32;
-	while (ASCII_n <= 127) {
+	ASCII__for__1 = 127;
+	ASCII__for__1 = (ASCII__for__1 - ASCII_n) + 1;
+	do {
 		Console_WriteCh((CHAR)ASCII_n);
 		ASCII_n += 1;
-	}
-	__ENDMOD;
+		ASCII__for__1 -= 1;
+	} while (!(ASCII__for__1 == 0));
+	__FINI;
 }
