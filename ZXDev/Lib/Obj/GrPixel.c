38811b6eb3098f43186ea5444174ef715aaaef68
Oleg N. Cher <allot@bk.ru>
2013-09-10 23:58:43 +0300

    ZXDev/Lib: Add 'Obj/GrPixel.c & .h'.

---
+++ ZXDev/Lib/Obj/GrPixel.c
@@ -0,0 +1,26 @@
+/*  Ofront 1.2 -xtspkae */
+#include "SYSTEM.h"
+
+
+
+
+export void GrPixel_Line (INTEGER x1, INTEGER y1, INTEGER x2, INTEGER y2);
+export void GrPixel_PutPixel (INTEGER x, INTEGER y);
+
+
+void GrPixel_PutPixel (INTEGER x, INTEGER y)
+{
+}
+
+void GrPixel_Line (INTEGER x1, INTEGER y1, INTEGER x2, INTEGER y2)
+{
+}
+
+
+export void *GrPixel__init(void)
+{
+	__DEFMOD;
+	__REGMOD("GrPixel", 0);
+/* BEGIN */
+	__ENDMOD;
+}
