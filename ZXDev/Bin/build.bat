ac2f4c1fb1a3a4e725b3c252f3bafa39c7fa9077
Oleg N. Cher <allot@bk.ru>
2013-09-14 01:22:46 +0300

    ZXDev: Adapt library 'TrDos' for ZXDev. Add example 'TrDosTest'. Thanks to Amxgris/Red Triangle.

--- ZXDev/Bin/build.bat
+++ ZXDev/Bin/build.bat
@@ -3,7 +3,7 @@
 @SET Bin=..\Bin
 @SET Lib=..\Lib
 
-%Bin%\sdcc %1.c -mz80 --code-loc %CodeAddr% --data-loc %DataAddr% --opt-code-size --disable-warning 85 -I "." -I %Lib% XDev.lib Graph.lib Basic.lib Laser.lib Mega.lib Best40.lib
+%Bin%\sdcc %1.c -mz80 --code-loc %CodeAddr% --data-loc %DataAddr% --opt-code-size --disable-warning 85 -I "." -I %Lib% XDev.lib Graph.lib Basic.lib Laser.lib Mega.lib Best40.lib trdos.lib
 @IF errorlevel 1 PAUSE
 
 %Bin%\hex2bin %1.ihx
---
+++ ZXDev/Lib/TrDos.h
@@ -0,0 +1,24 @@
+#ifndef TrDos__h
+#define TrDos__h
+
+#include "TrDos/trdos.h"
+
+#define TrDos_FDSP FDSP
+
+#define TrDos_Init trdosInit
+#define TrDos_DiskTune trdosDiskTune
+#define TrDos_DriveSel trdosDriveSel
+#define TrDos_FindFile(name, name__len, nameLen) trdosFindFile(name, nameLen)
+#define TrDos_LoadFile(name, name__len, destPtr, fileDataLen) trdosLoadFile(name, destPtr, fileDataLen)
+#define TrDos_ReadFileDSP trdosReadFileDSP
+#define TrDos_ReadSectors trdosReadSectors
+#define TrDos_ReadSectors2 trdosReadSectors
+
+#define TrDos_WriteFile(name, name__len, dataPtr, dataLen) trdosWriteFile(name, dataPtr, dataLen)
+#define TrDos_EraseFile(name, name__len) trdosEraseFile(name)
+#define TrDos_WriteFileDSP trdosWriteFileDSP
+#define TrDos_WriteSectors trdosWriteSectors
+
+#define TrDos__init trdosInit
+
+#endif
