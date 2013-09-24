abb42efceb74dc8e3a940757361b126116ee236d
Oleg N. Cher <allot@bk.ru>
2013-09-24 14:24:08 +0300

    DosDev: Implemented translation through DOSBox. Thanks to Oleg Komlev (Saferoll) for his research - http://zx.oberon2.ru/forum/viewtopic.php?f=54&t=115&p=689#p633

--- DosDev/Bin/build.bat
+++ DosDev/Bin/build.bat
@@ -1,5 +1,14 @@
+@FOR /F "tokens=*" %%f IN (..\Bin\config.cfg) DO %%f
 @SET PATH=..\BIN\TC201\BIN
 @SET Lib=..\Lib
+@SET This=%CD:~-3%
+@SET DOSBoxOpt=%DOSBoxOpt:~1,-1%
+ 
+IF /%DOSBox%==/  GOTO :nodosbox
+%DOSBox% %DOSBoxOpt% -c "mount c .."  -c "c:"  -c "cd %This%" -c "path %PATH%" -c "tcc -1 -O -d -f87 -Z -I..\BIN\TC201\INCLUDE -L..\BIN\TC201\LIB -IOBJ -I%Lib% %Lib%\DOSDEV.LIB %1.c" -c "@IF errorlevel 1 PAUSE" -c "@IF EXIST %1.exe MOVE %1.exe .." -c "@CLS" -c "@..\%1.exe" -c PAUSE -c EXIT
+@EXIT
+ 
+:nodosbox
 tcc -1 -O -d -f87 -Z -I..\BIN\TC201\INCLUDE -L..\BIN\TC201\LIB -IOBJ -I%Lib% %Lib%\DOSDEV.LIB %1.c
 @IF errorlevel 1 PAUSE
 @IF EXIST %1.exe MOVE %1.exe ..
