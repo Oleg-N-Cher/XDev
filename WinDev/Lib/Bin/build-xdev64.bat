f3ec76fb5338029bba2f7082a32ae80ca6997a3f
Oleg N. Cher <allot@bk.ru>
2013-11-24 16:50:07 +0200

    WinDev: + some 64 bit additions.

---
+++ WinDev/Lib/Bin/build-xdev64.bat
@@ -0,0 +1,12 @@
+@CD ..
+@SET RootBin=..\..\Bin
+@SET tcc=..\Bin\tcc64\tcc.exe
+
+%RootBin%\smartlib %1.c
+@FOR %%i IN (%1_0??.c) DO (
+  %tcc% -c %%i -I "." -I Obj
+  @IF errorlevel 1 PAUSE
+)
+@FOR %%i IN (%1_0??.o) DO ..\Bin\ar64 -rc WinDev64.a %%i
+@CALL Bin\clear
+@CD Obj
