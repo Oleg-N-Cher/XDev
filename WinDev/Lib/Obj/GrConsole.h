c6a504aa644c650bb893322d2fc017bf33634e46
Oleg N. Cher <allot@bk.ru>
2013-11-27 18:56:24 +0200

    WinDev/Lib/GrConsole: + procedure WriteInt.

--- WinDev/Lib/Obj/GrConsole.h
+++ WinDev/Lib/Obj/GrConsole.h
@@ -14,6 +14,7 @@ import void GrConsole_Clear (INTEGER color);
 import void GrConsole_SetColors (GrColors_Colors colors);
 import void GrConsole_SetFont (SYSTEM_BYTE *font, LONGINT font__len);
 import void GrConsole_WriteCh (CHAR ch);
+import void GrConsole_WriteInt (INTEGER x);
 import void GrConsole_WriteStr (CHAR *str, LONGINT str__len);
 import void *GrConsole__init(void);
 
