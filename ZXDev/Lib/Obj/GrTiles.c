e4b82f75d8afda2c7f3f924e6c423d712befc4c3
Oleg N. Cher <allot@bk.ru>
2013-12-04 10:46:58 +0200

    ZXDev/Lib/GrTiles: + stub for DrawTranspMonoTile.

--- ZXDev/Lib/Obj/GrTiles.c
+++ ZXDev/Lib/Obj/GrTiles.c
@@ -8,6 +8,8 @@ export void GrTiles_DrawMonoTile (INTEGER x, INTEGER y, INTEGER tile, SHORTINT c
 export void GrTiles_DrawMonoTile8x8 (INTEGER x, INTEGER y, INTEGER tile, SHORTINT colors);
 export void GrTiles_DrawTile (INTEGER x, INTEGER y, INTEGER tile);
 export void GrTiles_DrawTile8x8 (INTEGER x, INTEGER y, INTEGER tile);
+export void GrTiles_DrawTranspMonoTile (INTEGER x, INTEGER y, INTEGER tile, SHORTINT colors);
+export void GrTiles_DrawTranspMonoTile8x8 (SHORTINT x, SHORTINT y, INTEGER *tile, SHORTINT colors);
 export void GrTiles_SetColors (INTEGER attr);
 
 
@@ -23,11 +25,19 @@ void GrTiles_DrawMonoTile8x8 (INTEGER x, INTEGER y, INTEGER tile, SHORTINT color
 {
 }
 
+void GrTiles_DrawTranspMonoTile8x8 (SHORTINT x, SHORTINT y, INTEGER *tile, SHORTINT colors)
+{
+}
+
 void GrTiles_DrawTile (INTEGER x, INTEGER y, INTEGER tile)
 {
 }
 
 void GrTiles_DrawMonoTile (INTEGER x, INTEGER y, INTEGER tile, SHORTINT colors)
+{
+}
+
+void GrTiles_DrawTranspMonoTile (INTEGER x, INTEGER y, INTEGER tile, SHORTINT colors)
 {
 }
 
--- ZXDev/Lib/Obj/GrTiles.h
+++ ZXDev/Lib/Obj/GrTiles.h
@@ -12,6 +12,8 @@ import void GrTiles_DrawMonoTile (INTEGER x, INTEGER y, INTEGER tile, SHORTINT c
 import void GrTiles_DrawMonoTile8x8 (INTEGER x, INTEGER y, INTEGER tile, SHORTINT colors);
 import void GrTiles_DrawTile (INTEGER x, INTEGER y, INTEGER tile);
 import void GrTiles_DrawTile8x8 (INTEGER x, INTEGER y, INTEGER tile);
+import void GrTiles_DrawTranspMonoTile (INTEGER x, INTEGER y, INTEGER tile, SHORTINT colors);
+import void GrTiles_DrawTranspMonoTile8x8 (SHORTINT x, SHORTINT y, INTEGER *tile, SHORTINT colors);
 import void GrTiles_SetColors (INTEGER attr);
 import void *GrTiles__init(void);
 
