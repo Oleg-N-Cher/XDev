500c79a2d300986752942cd5e8b60e52d9029c32
Oleg N. Cher <allot@bk.ru>
2013-12-08 14:05:37 +0200

    WinDev/Lib: + module 'Input' & example 'TestInput'.

---
+++ WinDev/Obj/TestInput/GrConfig.h
@@ -0,0 +1,17 @@
+#ifndef __GrConfig__h
+#define __GrConfig__h
+
+
+#define GrConfig_Init \
+  GrScr_AppTitle	= "XDev/SDL Graphic"; \
+  GrScr_ActualWidth	= 800; \
+  GrScr_ActualHeight	= 192; \
+  GrScr_VirtualWidth	= 200; \
+  GrScr_VirtualHeight	=  32; \
+  GrScr_DepthBits	=   0; \
+  GrScr_NormalColor	=   0xAA
+
+#define ChkOutOfScreen	1
+
+
+#endif __GrConfig__h
