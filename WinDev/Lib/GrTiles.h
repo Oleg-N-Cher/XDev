ce6acb7b666178b2780fa01bc9c49439dde058ff
Oleg N. Cher <allot@bk.ru>
2013-12-13 00:15:28 +0200

    WinDev/Lib/GrTiles: + PROCEDURE 'DrawTranspMonoTile'.

--- WinDev/Lib/GrTiles.h
+++ WinDev/Lib/GrTiles.h
@@ -14,12 +14,20 @@ typedef
 typedef
 	SYSTEM_BYTE GrTiles_Tile8x8[8];
 
+typedef
+	SYSTEM_BYTE *GrTiles_TranspTile;
+
+typedef
+	SYSTEM_BYTE GrTiles_TranspTile8x8[16];
+
 
 import void GrTiles_DrawMonoTile8x8 (INTEGER x, INTEGER y, SYSTEM_BYTE *tile, LONGINT tile__len, GrColors_Colors colors);
+import void GrTiles_DrawTranspMonoTile8x8 (INTEGER x, INTEGER y, SYSTEM_BYTE *tile, LONGINT tile__len, GrColors_Colors colors);
 import void GrTiles__init (void);
 
 
 #define GrTiles_DrawMonoTile	GrTiles_DrawMonoTile8x8
+#define GrTiles_DrawTranspMonoTile	GrTiles_DrawTranspMonoTile8x8
 
 
 #endif
--- WinDev/Lib/Obj/GrTiles.c
+++ WinDev/Lib/Obj/GrTiles.c
@@ -11,11 +11,19 @@ typedef
 typedef
 	SYSTEM_BYTE GrTiles_Tile8x8[8];
 
+typedef
+	SYSTEM_BYTE *GrTiles_TranspTile;
+
+typedef
+	SYSTEM_BYTE GrTiles_TranspTile8x8[16];
+
 
 export void (*GrTiles_DrawMonoTile)(INTEGER, INTEGER, SYSTEM_BYTE*, LONGINT , GrColors_Colors);
+export void (*GrTiles_DrawTranspMonoTile)(INTEGER, INTEGER, SYSTEM_BYTE*, LONGINT , GrColors_Colors);
 
 
 export void GrTiles_DrawMonoTile8x8 (INTEGER x, INTEGER y, SYSTEM_BYTE *tile, LONGINT tile__len, GrColors_Colors colors);
+export void GrTiles_DrawTranspMonoTile8x8 (INTEGER x, INTEGER y, SYSTEM_BYTE *tile, LONGINT tile__len, GrColors_Colors colors);
 
 
 void GrTiles_DrawMonoTile8x8 (INTEGER x, INTEGER y, SYSTEM_BYTE *tile, LONGINT tile__len, GrColors_Colors colors)
@@ -60,6 +68,52 @@ void GrTiles_DrawMonoTile8x8 (INTEGER x, INTEGER y, SYSTEM_BYTE *tile, LONGINT t
 	GrPixel_Ink(inkTemp);
 }
 
+void GrTiles_DrawTranspMonoTile8x8 (INTEGER x, INTEGER y, SYSTEM_BYTE *tile, LONGINT tile__len, GrColors_Colors colors)
+{
+	CHAR mask, transp;
+	SHORTINT byte, bit;
+	INTEGER inkTemp;
+	SHORTINT _for__6, _for__5;
+	x = __ASHL(x, 3);
+	y = __ASHL(y, 3);
+	inkTemp = GrPixel_ink;
+	if (GrScr_MustLock && !SdlLib_LockSurface(GrScr_Screen)) {
+		return;
+	}
+	byte = 0;
+	_for__6 = 7;
+	_for__6 = (_for__6 - byte) + 1;
+	do {
+		mask = __VAL(CHAR, tile[__X(byte, tile__len)]);
+		transp = __VAL(CHAR, tile[__X(byte + 8, tile__len)]);
+		bit = 0;
+		_for__5 = 7;
+		_for__5 = (_for__5 - bit) + 1;
+		do {
+			if (transp < 0x80) {
+				if (mask >= 0x80) {
+					GrPixel_Ink(colors.ink);
+					(*GrPixel_PutPixelNoLock)(x + (int)bit, y);
+				} else {
+					GrPixel_Ink(colors.paper);
+					(*GrPixel_PutPixelNoLock)(x + (int)bit, y);
+				}
+			}
+			mask = __LSHL(mask, 1, CHAR);
+			transp = __LSHL(transp, 1, CHAR);
+			bit += 1;
+			_for__5 -= 1;
+		} while (!(_for__5 == 0));
+		y += 1;
+		byte += 1;
+		_for__6 -= 1;
+	} while (!(_for__6 == 0));
+	if (GrScr_MustLock) {
+		SdlLib_UnlockSurface(GrScr_Screen);
+	}
+	GrPixel_Ink(inkTemp);
+}
+
 
 export void *GrTiles__init(void)
 {
@@ -71,5 +125,6 @@ export void *GrTiles__init(void)
 	__REGMOD("GrTiles", 0);
 /* BEGIN */
 	GrTiles_DrawMonoTile = GrTiles_DrawMonoTile8x8;
+	GrTiles_DrawTranspMonoTile = GrTiles_DrawTranspMonoTile8x8;
 	__ENDMOD;
 }
