d11e0ef84ec98135064fc8f2478a4f4bdcc76331
Oleg N. Cher <allot@bk.ru>
2014-02-11 14:58:04 +0200

    WinDev/Bin: * 'build*.bat' scripts (optimized).

--- WinDev/Bin/build.bat
+++ WinDev/Bin/build.bat
@@ -4,15 +4,15 @@
 
 @IF EXIST %1 GOTO config
 
-%tcc% %1.c -I "." -I %Lib% %Lib%\WinDev.a %Lib%\Sdl\SdlLib.a %Bin%\tcc\lib\SDL.def -o %1.exe
-GOTO exit
+%tcc% %1.c -I "." -I %Lib% %Lib%\WinDev.a %Lib%\Sdl\SdlLib.a %Bin%\tcc\lib\SDL.def -o ..\%1.exe
+@GOTO exit
 
 :config
-%tcc% %1.c -I %1 -I %Lib% %Lib%\WinDev.a %Lib%\Sdl\SdlLib.a %Bin%\tcc\lib\SDL.def -o %1.exe
+%tcc% %1.c -I %1 -I %Lib% %Lib%\WinDev.a %Lib%\Sdl\SdlLib.a %Bin%\tcc\lib\SDL.def -o ..\%1.exe
 
 :exit
 @IF errorlevel 1 PAUSE
-@IF EXIST %1.exe MOVE %1.exe ..
+@CD ..
 @CLS
-@..\%1.exe
+@%1.exe
 @PAUSE
