edb5bdaaab8fd8e32eaddbff6243fad3db5c57b6
Oleg N. Cher <allot@bk.ru>
2014-02-11 14:58:04 +0200

    WinDev/Bin: * 'build*.bat' scripts (optimized).

--- WinDev/Bin/build64.bat
+++ WinDev/Bin/build64.bat
@@ -4,15 +4,15 @@
 
 @IF EXIST %1 GOTO config
 
-%tcc% %1.c -I "." -I %Lib% %Lib%\WinDev64.a -o %1.exe
-GOTO exit
+%tcc% %1.c -I "." -I %Lib% %Lib%\WinDev64.a -o ..\x64\%1.exe
+@GOTO exit
 
 :config
-%tcc% %1.c -I %1 -I %Lib% %Lib%\WinDev64.a -o %1.exe
+%tcc% %1.c -I %1 -I %Lib% %Lib%\WinDev64.a -o ..\x64\%1.exe
 
 :exit
 @IF errorlevel 1 PAUSE
-@IF EXIST %1.exe MOVE %1.exe ..\x64
+@CD ..\x64 
 @CLS
-@..\x64\%1.exe
+@%1.exe
 @PAUSE
