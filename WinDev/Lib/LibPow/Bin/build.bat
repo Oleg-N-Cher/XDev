629a0b6453f4b73602198a359c6664ac7275928b
Oleg N. Cher <allot@bk.ru>
2014-04-04 18:19:46 +0300

    * WinDev/Lib/LibPow: + smartlink feature.

--- WinDev/Lib/LibPow/Bin/build.bat
+++ WinDev/Lib/LibPow/Bin/build.bat
@@ -1,14 +1,18 @@
+@SET RootBin=..\..\..\..\Bin
 @SET Bin=..\..\..\Bin
 @SET Lib=..\..\..\Lib
 @SET tcc=%Bin%\tcc\tcc.exe
 
-%tcc% -c %1.c -I "." -I %Lib% -I %Lib%\Obj
-@IF errorlevel 1 PAUSE
+@IF EXIST ..\Pow.a DEL ..\Pow.a
 
-%Bin%\ar -rc ..\PowLib.a %1.o
+%RootBin%\smartlib %1.c
+@FOR %%i IN (%1_0??.c) DO (
+  %tcc% -c %%i -I "." -I %Lib% -I %Lib%\Obj
+  @IF errorlevel 1 PAUSE
+)
+@FOR %%i IN (%1_0??.o) DO %Bin%\ar -rc ..\Pow.a %%i
 
 @COPY /B /Y ..\Sym\PowStrings.sym ..\..\Sym
 @COPY /B /Y ..\Obj\PowStrings.h ..\..
-PAUSE
 
-REM @..\Bin\clear
+@..\Bin\clear
---
+++ WinDev/Lib/LibPow/Bin/clear.bat
@@ -0,0 +1,4 @@
+@DEL *.o
+@DEL *_0??.c
+@ATTRIB -H *.sbm
+@DEL *.sbm
