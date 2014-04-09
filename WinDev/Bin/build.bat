ea93ecf32c8f97b23a6bce69f4e9a2449cc3f02f
Oleg N. Cher <allot@bk.ru>
2014-04-09 03:07:07 +0300

    WinDev: * scripts.

--- WinDev/Bin/build.bat
+++ WinDev/Bin/build.bat
@@ -4,11 +4,11 @@
 
 @IF EXIST %1 GOTO config
 
-%tcc% %1.c -I "." -I %Lib% %Lib%\WinDev.a %Lib%\Sdl\SdlLib.a %Bin%\tcc\lib\SDL.def -o ..\%1.exe
+%tcc% %1.c -I "." -I %Lib% %Lib%\WinDev.a %Lib%\LibSdl\Sdl.a %Bin%\tcc\lib\SDL.def -o ..\%1.exe
 @GOTO exit
 
 :config
-%tcc% %1.c -I %1 -I %Lib% %Lib%\WinDev.a %Lib%\Sdl\SdlLib.a %Bin%\tcc\lib\SDL.def -o ..\%1.exe
+%tcc% %1.c -I %1 -I %Lib% %Lib%\WinDev.a %Lib%\LibSdl\Sdl.a %Bin%\tcc\lib\SDL.def -o ..\%1.exe
 
 :exit
 @IF errorlevel 1 PAUSE
