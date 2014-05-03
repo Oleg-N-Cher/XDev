09792793e6d41f5c0e6ebb411cdcc174ed61ba84
Oleg N. Cher <allot@bk.ru>
2014-05-03 04:20:11 +0300

    + ZXDev/Lib/Strings

---
+++ ZXDev/Lib/Obj/Strings.h
@@ -0,0 +1,15 @@
+/*  Ofront 1.2 -xtspkae */
+
+#ifndef Strings__h
+#define Strings__h
+
+#include "SYSTEM.h"
+
+
+
+
+import BOOLEAN Strings_StrToInt (CHAR *str, LONGINT str__len, INTEGER *result);
+import void *Strings__init(void);
+
+
+#endif
---
+++ ZXDev/Lib/Strings.h
@@ -0,0 +1,13 @@
+/*  Ofront 1.2 -xtspkae */
+
+#ifndef Strings__h
+#define Strings__h
+
+#include "SYSTEM.h"
+
+
+import BOOLEAN Strings_StrToInt (CHAR *str, LONGINT str__len, INTEGER *result);
+#define Strings__init()
+
+
+#endif
