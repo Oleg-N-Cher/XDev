f8e85766ffa1dd9cac84f43c756cd25b0c9632d3
Oleg N. Cher <allot@bk.ru>
2014-05-03 04:20:11 +0300

    + ZXDev/Lib/Strings

--- ZXDev/Lib/Obj/XDev.bat
+++ ZXDev/Lib/Obj/XDev.bat
@@ -4,11 +4,14 @@
 @IF EXIST ..\XDev.lib ..\DEL XDev.lib
 @CALL ..\Bin\build-xdev.bat SYSTEM
 @CALL ..\Bin\build-xdev.bat Platform
+@CALL ..\Bin\build-xdev.bat Strings
 @CALL ..\Bin\build-xdev.bat Console
+::@CALL ..\Bin\build-xdev.bat GrScr
 @CALL ..\Bin\build-xdev.bat GrPixel
 @CALL ..\Bin\build-xdev.bat GrTiles
 @CALL ..\Bin\build-xdev.bat GrFonts
 @CALL ..\Bin\build-xdev.bat Math
+@CALL ..\Bin\build-xdev.bat Timer
 @CALL ..\Bin\build-xdev.bat Input
 
 @MOVE /Y ..\XDev.lib ..\z80\XDev.lib
