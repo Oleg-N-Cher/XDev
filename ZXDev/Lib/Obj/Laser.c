8170f5595645f90659689760869472a0246acb75
Oleg N. Cher <allot@bk.ru>
2014-05-06 03:50:16 +0300

    * ZXDev/Lib/Laser: CARDINAL = LONGINT;

--- ZXDev/Lib/Obj/Laser.c
+++ ZXDev/Lib/Obj/Laser.c
@@ -25,8 +25,8 @@ export void Laser_GTOR (INTEGER col, INTEGER row, INTEGER spN);
 export void Laser_GTXR (INTEGER col, INTEGER row, INTEGER spN);
 export void Laser_INVM (INTEGER spN);
 export void Laser_INVV (INTEGER col, INTEGER row, INTEGER len, INTEGER hgt);
-export void Laser_InitScroll (INTEGER scrollBuf);
-export void Laser_InitSprites (INTEGER sprAddr, INTEGER sprSize);
+export void Laser_InitScroll (LONGINT scrollBuf);
+export void Laser_InitSprites (LONGINT sprAddr, LONGINT sprSize);
 export void Laser_MARV (INTEGER col, INTEGER row, INTEGER len, INTEGER hgt);
 export void Laser_MIRV (INTEGER col, INTEGER row, INTEGER len, INTEGER hgt);
 export void Laser_PMAT (INTEGER col, INTEGER row, INTEGER spD, INTEGER spS);
@@ -73,274 +73,343 @@ export void Laser_WR8M (INTEGER spN);
 export void Laser_WR8V (INTEGER col, INTEGER row, INTEGER len, INTEGER hgt);
 
 
-void Laser_InitSprites (INTEGER sprAddr, INTEGER sprSize)
+/*============================================================================*/
+
+void Laser_InitSprites (LONGINT sprAddr, LONGINT sprSize)
 {
 }
 
-void Laser_InitScroll (INTEGER scrollBuf)
+/*----------------------------------------------------------------------------*/
+void Laser_InitScroll (LONGINT scrollBuf)
 {
 }
 
+/*----------------------------------------------------------------------------*/
 void Laser_INVV (INTEGER col, INTEGER row, INTEGER len, INTEGER hgt)
 {
 }
 
+/*----------------------------------------------------------------------------*/
 void Laser_MIRV (INTEGER col, INTEGER row, INTEGER len, INTEGER hgt)
 {
 }
 
+/*----------------------------------------------------------------------------*/
 void Laser_MARV (INTEGER col, INTEGER row, INTEGER len, INTEGER hgt)
 {
 }
 
+/*----------------------------------------------------------------------------*/
 void Laser_SETV (INTEGER col, INTEGER row, INTEGER len, INTEGER hgt)
 {
 }
 
+/*----------------------------------------------------------------------------*/
 void Laser_CLSV (INTEGER col, INTEGER row, INTEGER len, INTEGER hgt)
 {
 }
 
+/*----------------------------------------------------------------------------*/
 void Laser_WL1V (INTEGER col, INTEGER row, INTEGER len, INTEGER hgt)
 {
 }
 
+/*----------------------------------------------------------------------------*/
 void Laser_WR1V (INTEGER col, INTEGER row, INTEGER len, INTEGER hgt)
 {
 }
 
+/*----------------------------------------------------------------------------*/
 void Laser_WL4V (INTEGER col, INTEGER row, INTEGER len, INTEGER hgt)
 {
 }
 
+/*----------------------------------------------------------------------------*/
 void Laser_WR4V (INTEGER col, INTEGER row, INTEGER len, INTEGER hgt)
 {
 }
 
+/*----------------------------------------------------------------------------*/
 void Laser_WL8V (INTEGER col, INTEGER row, INTEGER len, INTEGER hgt)
 {
 }
 
+/*----------------------------------------------------------------------------*/
 void Laser_WR8V (INTEGER col, INTEGER row, INTEGER len, INTEGER hgt)
 {
 }
 
+/*----------------------------------------------------------------------------*/
 void Laser_SL1V (INTEGER col, INTEGER row, INTEGER len, INTEGER hgt)
 {
 }
 
+/*----------------------------------------------------------------------------*/
 void Laser_SR1V (INTEGER col, INTEGER row, INTEGER len, INTEGER hgt)
 {
 }
 
+/*----------------------------------------------------------------------------*/
 void Laser_SL4V (INTEGER col, INTEGER row, INTEGER len, INTEGER hgt)
 {
 }
 
+/*----------------------------------------------------------------------------*/
 void Laser_SR4V (INTEGER col, INTEGER row, INTEGER len, INTEGER hgt)
 {
 }
 
+/*----------------------------------------------------------------------------*/
 void Laser_SL8V (INTEGER col, INTEGER row, INTEGER len, INTEGER hgt)
 {
 }
 
+/*----------------------------------------------------------------------------*/
 void Laser_SR8V (INTEGER col, INTEGER row, INTEGER len, INTEGER hgt)
 {
 }
 
+/*----------------------------------------------------------------------------*/
 void Laser_WCRV (INTEGER col, INTEGER row, INTEGER len, INTEGER hgt, INTEGER npx)
 {
 }
 
+/*----------------------------------------------------------------------------*/
 void Laser_SCRV (INTEGER col, INTEGER row, INTEGER len, INTEGER hgt, INTEGER npx)
 {
 }
 
+/*----------------------------------------------------------------------------*/
 void Laser_AWLV (INTEGER col, INTEGER row, INTEGER len, INTEGER hgt)
 {
 }
 
+/*----------------------------------------------------------------------------*/
 void Laser_ASLV (INTEGER col, INTEGER row, INTEGER len, INTEGER hgt)
 {
 }
 
+/*----------------------------------------------------------------------------*/
 void Laser_AWRV (INTEGER col, INTEGER row, INTEGER len, INTEGER hgt)
 {
 }
 
+/*----------------------------------------------------------------------------*/
 void Laser_ASRV (INTEGER col, INTEGER row, INTEGER len, INTEGER hgt)
 {
 }
 
+/*----------------------------------------------------------------------------*/
 void Laser_ATUV (INTEGER col, INTEGER row, INTEGER len, INTEGER hgt)
 {
 }
 
+/*----------------------------------------------------------------------------*/
 void Laser_ATDV (INTEGER col, INTEGER row, INTEGER len, INTEGER hgt)
 {
 }
 
+/*----------------------------------------------------------------------------*/
 void Laser_CLSM (INTEGER spN)
 {
 }
 
+/*----------------------------------------------------------------------------*/
 void Laser_INVM (INTEGER spN)
 {
 }
 
+/*----------------------------------------------------------------------------*/
 void Laser_PTBL (INTEGER col, INTEGER row, INTEGER spN)
 {
 }
 
+/*----------------------------------------------------------------------------*/
 void Laser_PTOR (INTEGER col, INTEGER row, INTEGER spN)
 {
 }
 
+/*----------------------------------------------------------------------------*/
 void Laser_PTXR (INTEGER col, INTEGER row, INTEGER spN)
 {
 }
 
+/*----------------------------------------------------------------------------*/
 void Laser_PTND (INTEGER col, INTEGER row, INTEGER spN)
 {
 }
 
+/*----------------------------------------------------------------------------*/
 void Laser_WL1M (INTEGER spN)
 {
 }
 
+/*----------------------------------------------------------------------------*/
 void Laser_WR1M (INTEGER spN)
 {
 }
 
+/*----------------------------------------------------------------------------*/
 void Laser_WL4M (INTEGER spN)
 {
 }
 
+/*----------------------------------------------------------------------------*/
 void Laser_WR4M (INTEGER spN)
 {
 }
 
+/*----------------------------------------------------------------------------*/
 void Laser_WL8M (INTEGER spN)
 {
 }
 
+/*----------------------------------------------------------------------------*/
 void Laser_WR8M (INTEGER spN)
 {
 }
 
+/*----------------------------------------------------------------------------*/
 void Laser_SL1M (INTEGER spN)
 {
 }
 
+/*----------------------------------------------------------------------------*/
 void Laser_SR1M (INTEGER spN)
 {
 }
 
+/*----------------------------------------------------------------------------*/
 void Laser_SL4M (INTEGER spN)
 {
 }
 
+/*----------------------------------------------------------------------------*/
 void Laser_SR4M (INTEGER spN)
 {
 }
 
+/*----------------------------------------------------------------------------*/
 void Laser_SL8M (INTEGER spN)
 {
 }
 
+/*----------------------------------------------------------------------------*/
 void Laser_SR8M (INTEGER spN)
 {
 }
 
+/*----------------------------------------------------------------------------*/
 void Laser_WCRM (INTEGER spN, INTEGER npx)
 {
 }
 
+/*----------------------------------------------------------------------------*/
 void Laser_SCRM (INTEGER spN, INTEGER npx)
 {
 }
 
+/*----------------------------------------------------------------------------*/
 void Laser_ATOF (void)
 {
 }
 
+/*----------------------------------------------------------------------------*/
 void Laser_ATON (void)
 {
 }
 
+/*----------------------------------------------------------------------------*/
 void Laser_GTBL (INTEGER col, INTEGER row, INTEGER spN)
 {
 }
 
+/*----------------------------------------------------------------------------*/
 void Laser_GTOR (INTEGER col, INTEGER row, INTEGER spN)
 {
 }
 
+/*----------------------------------------------------------------------------*/
 void Laser_GTXR (INTEGER col, INTEGER row, INTEGER spN)
 {
 }
 
+/*----------------------------------------------------------------------------*/
 void Laser_GTND (INTEGER col, INTEGER row, INTEGER spN)
 {
 }
 
+/*----------------------------------------------------------------------------*/
 void Laser_PMBL (INTEGER col, INTEGER row, INTEGER spD, INTEGER spS)
 {
 }
 
+/*----------------------------------------------------------------------------*/
 void Laser_PMOR (INTEGER col, INTEGER row, INTEGER spD, INTEGER spS)
 {
 }
 
+/*----------------------------------------------------------------------------*/
 void Laser_PMXR (INTEGER col, INTEGER row, INTEGER spD, INTEGER spS)
 {
 }
 
+/*----------------------------------------------------------------------------*/
 void Laser_PMND (INTEGER col, INTEGER row, INTEGER spD, INTEGER spS)
 {
 }
 
+/*----------------------------------------------------------------------------*/
 void Laser_PMAT (INTEGER col, INTEGER row, INTEGER spD, INTEGER spS)
 {
 }
 
+/*----------------------------------------------------------------------------*/
 void Laser_GMBL (INTEGER col, INTEGER row, INTEGER spD, INTEGER spS)
 {
 }
 
+/*----------------------------------------------------------------------------*/
 void Laser_GMOR (INTEGER col, INTEGER row, INTEGER spD, INTEGER spS)
 {
 }
 
+/*----------------------------------------------------------------------------*/
 void Laser_GMXR (INTEGER col, INTEGER row, INTEGER spD, INTEGER spS)
 {
 }
 
+/*----------------------------------------------------------------------------*/
 void Laser_GMND (INTEGER col, INTEGER row, INTEGER spD, INTEGER spS)
 {
 }
 
+/*----------------------------------------------------------------------------*/
 void Laser_GMAT (INTEGER col, INTEGER row, INTEGER spD, INTEGER spS)
 {
 }
 
+/*----------------------------------------------------------------------------*/
 void Laser_PWBL (INTEGER col, INTEGER row, INTEGER spN, INTEGER spCol, INTEGER spRow, INTEGER len, INTEGER hgt)
 {
 }
 
+/*----------------------------------------------------------------------------*/
 void Laser_PWOR (INTEGER col, INTEGER row, INTEGER spN, INTEGER spCol, INTEGER spRow, INTEGER len, INTEGER hgt)
 {
 }
 
+/*----------------------------------------------------------------------------*/
 void Laser_PWXR (INTEGER col, INTEGER row, INTEGER spN, INTEGER spCol, INTEGER spRow, INTEGER len, INTEGER hgt)
 {
 }
 
+/*----------------------------------------------------------------------------*/
 void Laser_PWND (INTEGER col, INTEGER row, INTEGER spN, INTEGER spCol, INTEGER spRow, INTEGER len, INTEGER hgt)
 {
 }
 
+/*----------------------------------------------------------------------------*/
 
 export void *Laser__init(void)
 {
