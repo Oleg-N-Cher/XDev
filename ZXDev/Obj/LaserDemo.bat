a281d51e8ae0d57da156f6fce3a820d261482460
Oleg N. Cher <allot@bk.ru>
2014-05-16 14:25:58 +0300

    ZXDev: LaserDemo.trd => LaserDemo.tap

--- ZXDev/Obj/LaserDemo.bat
+++ ZXDev/Obj/LaserDemo.bat
@@ -1,25 +1,28 @@
-@REM Compile LaserDemo.c
-@REM ===================
-
+:: Build LaserDemo.c
+:: =================
+@SET CodeAddr=45056
+@SET DataAddr=63488
 @SET Bin=..\Bin
 @SET Lib=..\Lib
 
-%bin%\sdcc -mz80 --code-loc 26000 --data-loc 0xF800 --no-std-crt0 --opt-code-size --funsigned-char --disable-warning 126 -I "." -I %Lib% -L %Lib%/z80 LaserDemo.c Basic.lib Laser.lib LaserSprite2B.lib
+%Bin%\sdcc -mz80 --code-loc %CodeAddr% --data-loc %DataAddr% --no-std-crt0 --opt-code-size --funsigned-char --disable-warning 126 -I "." -I %Lib% -L %Lib%/z80 LaserDemo.c Basic.lib Laser.lib LaserSprite2B.lib
 @IF errorlevel 1 PAUSE
 
-@REM Convert Intel hex format to binary
-@REM ==================================
-%bin%\hex2bin LaserDemo.ihx
+:: Convert Intel hex format to binary
+:: ==================================
+%Bin%\hex2bin LaserDemo.ihx
 
-@REM Link the target to TR-DOS format
-@REM ================================
-@DEL /Q LaserDemo.trd
-%bin%\bin2trd.exe LaserDemo.trl
-@ATTRIB -R laserdemo.trd
-@MOVE /Y laserdemo.trd ..\LaserDemo.trd
+:: Link the target to TR-DOS format
+:: ================================
+::@DEL /Q LaserDemo.trd
+::%Bin%\bin2trd.exe LaserDemo.trl
+::@ATTRIB -R laserdemo.trd
+::@MOVE /Y laserdemo.trd ..\LaserDemo.trd
+:: Link the target to TAP format
+%Bin%\hex2bin LaserDemo.ihx
+%Bin%\bin2data.exe -rem -org %CodeAddr% LaserDemo.bin ..\LaserDemo.tap LaserDemo
 
-@REM ======================================================================
+:: ========================================================================
 @ECHO Now you can run the image "LaserDemo.trd" in your favourite emulator
-@REM ======================================================================
-
-START ..\LaserDemo.trd
+:: ========================================================================
+START ..\LaserDemo.tap
