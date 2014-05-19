68cc02c3cac1bb4ce0f0c2524660d80d3b040d9e
Oleg N. Cher <allot@bk.ru>
2014-05-19 20:13:40 +0300

    * ZXDev/Lib/Laser: * build script.

--- ZXDev/Lib/Obj/Laser.bat
+++ ZXDev/Lib/Obj/Laser.bat
@@ -8,9 +8,9 @@
 @IF EXIST Laser.lib DEL Laser.lib
 
 %RootBin%\smartlib Laser.c
-@FOR %%i IN (Laser_0??.c) DO (
+@FOR %%i IN (Laser_???.c) DO (
   %Bin%\sdcc -c %%i -mz80 --opt-code-size --disable-warning 59 --disable-warning 85 -I "." -I Obj
   @IF errorlevel 1 PAUSE
 )
-@FOR %%i IN (Laser_0??.rel) DO %Bin%\sdar -rc Laser.lib %%i
+@FOR %%i IN (Laser_???.rel) DO %Bin%\sdar -rc Laser.lib %%i
 @MOVE /Y Laser.lib z80/Laser.lib
