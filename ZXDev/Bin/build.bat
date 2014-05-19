80935321b747590355a3fa9ec81df61fa3257483
Oleg N. Cher <allot@bk.ru>
2014-05-19 19:01:49 +0300

    * ZXDev/Bin: update scripts.

--- ZXDev/Bin/build.bat
+++ ZXDev/Bin/build.bat
@@ -3,9 +3,16 @@
 @SET Bin=..\Bin
 @SET Lib=..\Lib
 
-%Bin%\sdcc %1.c -mz80 --code-loc %CodeAddr% --data-loc %DataAddr% --opt-code-size --disable-warning 85 -I "." -I %Lib% XDev.lib Graph.lib Basic.lib Laser.lib Mega.lib Best40.lib trdos.lib
-@IF errorlevel 1 PAUSE
+@IF EXIST %1 GOTO config
 
+%Bin%\sdcc %1.c -mz80 --code-loc %CodeAddr% --data-loc %DataAddr% --opt-code-size --oldralloc --disable-warning 85 -I "." -I %Lib% XDev.lib Graph.lib Basic.lib Laser.lib Mega.lib Best40.lib trdos.lib
+@GOTO link
+
+:config
+%Bin%\sdcc %1.c -mz80 --code-loc %CodeAddr% --data-loc %DataAddr% --opt-code-size --oldralloc --disable-warning 85 -I %1 -I %Lib% XDev.lib Graph.lib Basic.lib Laser.lib Mega.lib Best40.lib trdos.lib
+
+:link
+@IF errorlevel 1 PAUSE
 %Bin%\hex2bin %1.ihx
 %Bin%\bin2data.exe -rem -org %CodeAddr% %1.bin ..\%1.tap %1
 @START ..\%1.tap
