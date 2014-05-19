22fbf074d4a5c54a1c697a13c25a28c83c8c99f6
Oleg N. Cher <allot@bk.ru>
2014-05-19 18:50:49 +0300

    / ZXDev/Lib/GrTiles; / example DemoTiles; + GrScr

--- ZXDev/Lib/Obj/GrTiles.c
+++ ZXDev/Lib/Obj/GrTiles.c
@@ -10,37 +10,40 @@ export void GrTiles_DrawTile (INTEGER x, INTEGER y, INTEGER tile);
 export void GrTiles_DrawTile8x8 (INTEGER x, INTEGER y, INTEGER tile);
 export void GrTiles_DrawTranspMonoTile (INTEGER x, INTEGER y, INTEGER tile, SHORTINT colors);
 export void GrTiles_DrawTranspMonoTile8x8 (SHORTINT x, SHORTINT y, INTEGER *tile, SHORTINT colors);
-export void GrTiles_SetColors (INTEGER attr);
 
 
-void GrTiles_SetColors (INTEGER attr)
-{
-}
+/*============================================================================*/
 
 void GrTiles_DrawTile8x8 (INTEGER x, INTEGER y, INTEGER tile)
 {
 }
 
+/*----------------------------------------------------------------------------*/
 void GrTiles_DrawMonoTile8x8 (INTEGER x, INTEGER y, INTEGER tile, SHORTINT colors)
 {
 }
 
+/*----------------------------------------------------------------------------*/
 void GrTiles_DrawTranspMonoTile8x8 (SHORTINT x, SHORTINT y, INTEGER *tile, SHORTINT colors)
 {
 }
 
+/*----------------------------------------------------------------------------*/
 void GrTiles_DrawTile (INTEGER x, INTEGER y, INTEGER tile)
 {
 }
 
+/*----------------------------------------------------------------------------*/
 void GrTiles_DrawMonoTile (INTEGER x, INTEGER y, INTEGER tile, SHORTINT colors)
 {
 }
 
+/*----------------------------------------------------------------------------*/
 void GrTiles_DrawTranspMonoTile (INTEGER x, INTEGER y, INTEGER tile, SHORTINT colors)
 {
 }
 
+/*----------------------------------------------------------------------------*/
 
 export void *GrTiles__init(void)
 {
--- ZXDev/Obj/DemoTiles.c
+++ ZXDev/Obj/DemoTiles.c
@@ -1,6 +1,7 @@
 /*  Ofront 1.2 -xtspkaem */
 #include "SYSTEM.h"
 #include "Basic.h"
+#include "GrScr.h"
 #include "GrTiles.h"
 
 
@@ -11,12 +12,15 @@ static SHORTINT DemoTiles__for__6, DemoTiles__for__5, DemoTiles__for__4, DemoTil
 
 
 
+/*============================================================================*/
+
 
 export main(int argc, char **argv)
 {
 	__INIT(argc, argv);
-	__IMPORT(Basic);
-	__IMPORT(GrTiles);
+	__IMPORT(Basic__init);
+	__IMPORT(GrScr__init);
+	__IMPORT(GrTiles__init);
 	__REGMAIN("DemoTiles", 0);
 /* BEGIN */
 	Basic_Init();
@@ -40,39 +44,35 @@ export main(int argc, char **argv)
 		DemoTiles_i += 1;
 		DemoTiles__for__5 -= 1;
 	} while (!(DemoTiles__for__5 == 0));
-	GrTiles_SetColors(7);
 	DemoTiles_i = 8;
 	DemoTiles__for__4 = 23;
 	DemoTiles__for__4 = (DemoTiles__for__4 - DemoTiles_i) + 1;
 	do {
-		GrTiles_DrawMonoTile(DemoTiles_i, 1, DemoTiles_square);
+		GrTiles_DrawMonoTile(DemoTiles_i, 1, DemoTiles_square, 7);
 		DemoTiles_i += 1;
 		DemoTiles__for__4 -= 1;
 	} while (!(DemoTiles__for__4 == 0));
-	GrTiles_SetColors(13);
 	DemoTiles_i = 10;
 	DemoTiles__for__3 = 21;
 	DemoTiles__for__3 = (DemoTiles__for__3 - DemoTiles_i) + 1;
 	do {
-		GrTiles_DrawMonoTile(DemoTiles_i, 3, DemoTiles_square);
+		GrTiles_DrawMonoTile(DemoTiles_i, 3, DemoTiles_square, 13);
 		DemoTiles_i += 1;
 		DemoTiles__for__3 -= 1;
 	} while (!(DemoTiles__for__3 == 0));
-	GrTiles_SetColors(56);
 	DemoTiles_i = 12;
 	DemoTiles__for__2 = 19;
 	DemoTiles__for__2 = (DemoTiles__for__2 - DemoTiles_i) + 1;
 	do {
-		GrTiles_DrawMonoTile(DemoTiles_i, 5, DemoTiles_square);
+		GrTiles_DrawMonoTile(DemoTiles_i, 5, DemoTiles_square, 56);
 		DemoTiles_i += 1;
 		DemoTiles__for__2 -= 1;
 	} while (!(DemoTiles__for__2 == 0));
-	GrTiles_SetColors(28);
 	DemoTiles_i = 14;
 	DemoTiles__for__1 = 17;
 	DemoTiles__for__1 = (DemoTiles__for__1 - DemoTiles_i) + 1;
 	do {
-		GrTiles_DrawMonoTile(DemoTiles_i, 7, DemoTiles_square);
+		GrTiles_DrawMonoTile(DemoTiles_i, 7, DemoTiles_square, 28);
 		DemoTiles_i += 1;
 		DemoTiles__for__1 -= 1;
 	} while (!(DemoTiles__for__1 == 0));
