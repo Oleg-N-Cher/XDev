f4ec85999e09493225bec7d8a7b7b6e9d354df2f
Oleg N. Cher <allot@bk.ru>
2014-06-15 19:33:34 +0300

    + ZXDev/Lib/Ignore.

---
+++ ZXDev/Lib/Obj/Ignore.h
@@ -0,0 +1,20 @@
+/*  Ofront 1.2 -xtspkae */
+
+#ifndef Ignore__h
+#define Ignore__h
+
+#include "SYSTEM.h"
+
+
+
+
+import void Ignore_Bool (BOOLEAN b);
+import void Ignore_Char (CHAR ch);
+import void Ignore_Int (LONGINT i);
+import void Ignore_Ptr (SYSTEM_PTR ptr);
+import void Ignore_Real (LONGREAL r);
+import void Ignore_Set (SET s);
+import void *Ignore__init(void);
+
+
+#endif
