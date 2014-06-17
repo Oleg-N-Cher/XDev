1b35806973a42745623ede264743359a0db368b4
Oleg N. Cher <allot@bk.ru>
2014-06-17 15:01:02 +0300

    + ZXDev/Lib/Asm; + AsmTest.

---
+++ ZXDev/Lib/Asm.h
@@ -0,0 +1,62 @@
+#ifndef Asm__h
+#define Asm__h
+
+#define Asm_Byte(b) __asm .db b __endasm
+#define Asm_Byte1(b) __asm .db b __endasm
+#define Asm_Byte2(b1,b2) __asm .db b1,b2 __endasm
+#define Asm_Byte3(b1,b2,b3) __asm .db b1,b2,b3 __endasm
+#define Asm_Byte4(b1,b2,b3,b4) __asm .db b1,b2,b3,b4 __endasm
+#define Asm_Byte5(b1,b2,b3,b4,b5) __asm .db b1,b2,b3,b4,b5 __endasm
+#define Asm_Byte6(b1,b2,b3,b4,b5,b6) __asm .db b1,b2,b3,b4,b5,b6 __endasm
+#define Asm_Byte7(b1,b2,b3,b4,b5,b6,b7) __asm .db b1,b2,b3,b4,b5,b6,b7 __endasm
+#define Asm_Byte8(b1,b2,b3,b4,b5,b6,b7,b8) __asm .db b1,b2,b3,b4,b5,b6,b7,b8 __endasm
+#define Asm_Byte9(b1,b2,b3,b4,b5,b6,b7,b8,b9) __asm .db b1,b2,b3,b4,b5,b6,b7,b8,b9 __endasm
+#define Asm_Byte10(b1,b2,b3,b4,b5,b6,b7,b8,b9,b10) __asm .db b1,b2,b3,b4,b5,b6,b7,b8,b9,b10 __endasm
+#define Asm_Byte11(b1,b2,b3,b4,b5,b6,b7,b8,b9,b10,b11) __asm .db b1,b2,b3,b4,b5,b6,b7,b8,b9,b10,b11 __endasm
+#define Asm_Byte12(b1,b2,b3,b4,b5,b6,b7,b8,b9,b10,b11,b12) __asm .db b1,b2,b3,b4,b5,b6,b7,b8,b9,b10,b11,b12 __endasm
+#define Asm_Byte13(b1,b2,b3,b4,b5,b6,b7,b8,b9,b10,b11,b12,b13) __asm .db b1,b2,b3,b4,b5,b6,b7,b8,b9,b10,b11,b12,b13 __endasm
+#define Asm_Byte14(b1,b2,b3,b4,b5,b6,b7,b8,b9,b10,b11,b12,b13,b14) __asm .db b1,b2,b3,b4,b5,b6,b7,b8,b9,b10,b11,b12,b13,b14 __endasm
+#define Asm_Byte15(b1,b2,b3,b4,b5,b6,b7,b8,b9,b10,b11,b12,b13,b14,b15) __asm .db b1,b2,b3,b4,b5,b6,b7,b8,b9,b10,b11,b12,b13,b14,b15 __endasm
+#define Asm_Byte16(b1,b2,b3,b4,b5,b6,b7,b8,b9,b10,b11,b12,b13,b14,b15,b16) __asm .db b1,b2,b3,b4,b5,b6,b7,b8,b9,b10,b11,b12,b13,b14,b15,b16 __endasm
+
+#define Asm_Char(b) __asm .db b __endasm
+#define Asm_Char1(b) __asm .db b __endasm
+#define Asm_Char2(b1,b2) __asm .db b1,b2 __endasm
+#define Asm_Char3(b1,b2,b3) __asm .db b1,b2,b3 __endasm
+#define Asm_Char4(b1,b2,b3,b4) __asm .db b1,b2,b3,b4 __endasm
+#define Asm_Char5(b1,b2,b3,b4,b5) __asm .db b1,b2,b3,b4,b5 __endasm
+#define Asm_Char6(b1,b2,b3,b4,b5,b6) __asm .db b1,b2,b3,b4,b5,b6 __endasm
+#define Asm_Char7(b1,b2,b3,b4,b5,b6,b7) __asm .db b1,b2,b3,b4,b5,b6,b7 __endasm
+#define Asm_Char8(b1,b2,b3,b4,b5,b6,b7,b8) __asm .db b1,b2,b3,b4,b5,b6,b7,b8 __endasm
+#define Asm_Char9(b1,b2,b3,b4,b5,b6,b7,b8,b9) __asm .db b1,b2,b3,b4,b5,b6,b7,b8,b9 __endasm
+#define Asm_Char10(b1,b2,b3,b4,b5,b6,b7,b8,b9,b10) __asm .db b1,b2,b3,b4,b5,b6,b7,b8,b9,b10 __endasm
+#define Asm_Char11(b1,b2,b3,b4,b5,b6,b7,b8,b9,b10,b11) __asm .db b1,b2,b3,b4,b5,b6,b7,b8,b9,b10,b11 __endasm
+#define Asm_Char12(b1,b2,b3,b4,b5,b6,b7,b8,b9,b10,b11,b12) __asm .db b1,b2,b3,b4,b5,b6,b7,b8,b9,b10,b11,b12 __endasm
+#define Asm_Char13(b1,b2,b3,b4,b5,b6,b7,b8,b9,b10,b11,b12,b13) __asm .db b1,b2,b3,b4,b5,b6,b7,b8,b9,b10,b11,b12,b13 __endasm
+#define Asm_Char14(b1,b2,b3,b4,b5,b6,b7,b8,b9,b10,b11,b12,b13,b14) __asm .db b1,b2,b3,b4,b5,b6,b7,b8,b9,b10,b11,b12,b13,b14 __endasm
+#define Asm_Char15(b1,b2,b3,b4,b5,b6,b7,b8,b9,b10,b11,b12,b13,b14,b15) __asm .db b1,b2,b3,b4,b5,b6,b7,b8,b9,b10,b11,b12,b13,b14,b15 __endasm
+#define Asm_Char16(b1,b2,b3,b4,b5,b6,b7,b8,b9,b10,b11,b12,b13,b14,b15,b16) __asm .db b1,b2,b3,b4,b5,b6,b7,b8,b9,b10,b11,b12,b13,b14,b15,b16 __endasm
+
+#define Asm_Word(w) __asm .dw w __endasm
+#define Asm_Word1(w) __asm .dw w __endasm
+#define Asm_Word2(w1,w2) __asm .dw w1,w2 __endasm
+#define Asm_Word3(w1,w2,w3) __asm .dw w1,w2,w3 __endasm
+#define Asm_Word4(w1,w2,w3,w4) __asm .dw w1,w2,w3,w4 __endasm
+#define Asm_Word5(w1,w2,w3,w4,w5) __asm .dw w1,w2,w3,w4,w5 __endasm
+#define Asm_Word6(w1,w2,w3,w4,w5,w6) __asm .dw w1,w2,w3,w4,w5,w6 __endasm
+#define Asm_Word7(w1,w2,w3,w4,w5,w6,w7) __asm .dw w1,w2,w3,w4,w5,w6,w7 __endasm
+#define Asm_Word8(w1,w2,w3,w4,w5,w6,w7,w8) __asm .dw w1,w2,w3,w4,w5,w6,w7,w8 __endasm
+#define Asm_Word9(w1,w2,w3,w4,w5,w6,w7,w8,w9) __asm .dw w1,w2,w3,w4,w5,w6,w7,w8,w9 __endasm
+#define Asm_Word10(w1,w2,w3,w4,w5,w6,w7,w8,w9,w10) __asm .dw w1,w2,w3,w4,w5,w6,w7,w8,w9,w10 __endasm
+#define Asm_Word11(w1,w2,w3,w4,w5,w6,w7,w8,w9,w10,w11) __asm .dw w1,w2,w3,w4,w5,w6,w7,w8,w9,w10,w11 __endasm
+#define Asm_Word12(w1,w2,w3,w4,w5,w6,w7,w8,w9,w10,w11,w12) __asm .dw w1,w2,w3,w4,w5,w6,w7,w8,w9,w10,w11,w12 __endasm
+#define Asm_Word13(w1,w2,w3,w4,w5,w6,w7,w8,w9,w10,w11,w12,w13) __asm .dw w1,w2,w3,w4,w5,w6,w7,w8,w9,w10,w11,w12,w13 __endasm
+#define Asm_Word14(w1,w2,w3,w4,w5,w6,w7,w8,w9,w10,w11,w12,w13,w14) __asm .dw w1,w2,w3,w4,w5,w6,w7,w8,w9,w10,w11,w12,w13,w14 __endasm
+#define Asm_Word15(w1,w2,w3,w4,w5,w6,w7,w8,w9,w10,w11,w12,w13,w14,w15) __asm .dw w1,w2,w3,w4,w5,w6,w7,w8,w9,w10,w11,w12,w13,w14,w15 __endasm
+#define Asm_Word16(w1,w2,w3,w4,w5,w6,w7,w8,w9,w10,w11,w12,w13,w14,w15,w16) __asm .dw w1,w2,w3,w4,w5,w6,w7,w8,w9,w10,w11,w12,w13,w14,w15,w16 __endasm
+
+#define Asm_Code(a,b) __asm__ (a)
+
+#define Asm__init()
+
+#endif
---
+++ ZXDev/Lib/Obj/Asm.c
@@ -0,0 +1,332 @@
+/*  Ofront 1.2 -xtspkae */
+#include "SYSTEM.h"
+
+
+
+
+export void Asm_Byte (INTEGER b);
+export void Asm_Byte1 (INTEGER b);
+export void Asm_Byte10 (INTEGER b1, INTEGER b2, INTEGER b3, INTEGER b4, INTEGER b5, INTEGER b6, INTEGER b7, INTEGER b8, INTEGER b9, INTEGER b10);
+export void Asm_Byte11 (INTEGER b1, INTEGER b2, INTEGER b3, INTEGER b4, INTEGER b5, INTEGER b6, INTEGER b7, INTEGER b8, INTEGER b9, INTEGER b10, INTEGER b11);
+export void Asm_Byte12 (INTEGER b1, INTEGER b2, INTEGER b3, INTEGER b4, INTEGER b5, INTEGER b6, INTEGER b7, INTEGER b8, INTEGER b9, INTEGER b10, INTEGER b11, INTEGER b12);
+export void Asm_Byte13 (INTEGER b1, INTEGER b2, INTEGER b3, INTEGER b4, INTEGER b5, INTEGER b6, INTEGER b7, INTEGER b8, INTEGER b9, INTEGER b10, INTEGER b11, INTEGER b12, INTEGER b13);
+export void Asm_Byte14 (INTEGER b1, INTEGER b2, INTEGER b3, INTEGER b4, INTEGER b5, INTEGER b6, INTEGER b7, INTEGER b8, INTEGER b9, INTEGER b10, INTEGER b11, INTEGER b12, INTEGER b13, INTEGER b14);
+export void Asm_Byte15 (INTEGER b1, INTEGER b2, INTEGER b3, INTEGER b4, INTEGER b5, INTEGER b6, INTEGER b7, INTEGER b8, INTEGER b9, INTEGER b10, INTEGER b11, INTEGER b12, INTEGER b13, INTEGER b14, INTEGER b15);
+export void Asm_Byte16 (INTEGER b1, INTEGER b2, INTEGER b3, INTEGER b4, INTEGER b5, INTEGER b6, INTEGER b7, INTEGER b8, INTEGER b9, INTEGER b10, INTEGER b11, INTEGER b12, INTEGER b13, INTEGER b14, INTEGER b15, INTEGER b16);
+export void Asm_Byte2 (INTEGER b1, INTEGER b2);
+export void Asm_Byte3 (INTEGER b1, INTEGER b2, INTEGER b3);
+export void Asm_Byte4 (INTEGER b1, INTEGER b2, INTEGER b3, INTEGER b4);
+export void Asm_Byte5 (INTEGER b1, INTEGER b2, INTEGER b3, INTEGER b4, INTEGER b5);
+export void Asm_Byte6 (INTEGER b1, INTEGER b2, INTEGER b3, INTEGER b4, INTEGER b5, INTEGER b6);
+export void Asm_Byte7 (INTEGER b1, INTEGER b2, INTEGER b3, INTEGER b4, INTEGER b5, INTEGER b6, INTEGER b7);
+export void Asm_Byte8 (INTEGER b1, INTEGER b2, INTEGER b3, INTEGER b4, INTEGER b5, INTEGER b6, INTEGER b7, INTEGER b8);
+export void Asm_Byte9 (INTEGER b1, INTEGER b2, INTEGER b3, INTEGER b4, INTEGER b5, INTEGER b6, INTEGER b7, INTEGER b8, INTEGER b9);
+export void Asm_Char (SYSTEM_BYTE b);
+export void Asm_Char1 (SYSTEM_BYTE b);
+export void Asm_Char10 (SYSTEM_BYTE b1, SYSTEM_BYTE b2, SYSTEM_BYTE b3, SYSTEM_BYTE b4, SYSTEM_BYTE b5, SYSTEM_BYTE b6, SYSTEM_BYTE b7, SYSTEM_BYTE b8, SYSTEM_BYTE b9, SYSTEM_BYTE b10);
+export void Asm_Char11 (SYSTEM_BYTE b1, SYSTEM_BYTE b2, SYSTEM_BYTE b3, SYSTEM_BYTE b4, SYSTEM_BYTE b5, SYSTEM_BYTE b6, SYSTEM_BYTE b7, SYSTEM_BYTE b8, SYSTEM_BYTE b9, SYSTEM_BYTE b10, SYSTEM_BYTE b11);
+export void Asm_Char12 (SYSTEM_BYTE b1, SYSTEM_BYTE b2, SYSTEM_BYTE b3, SYSTEM_BYTE b4, SYSTEM_BYTE b5, SYSTEM_BYTE b6, SYSTEM_BYTE b7, SYSTEM_BYTE b8, SYSTEM_BYTE b9, SYSTEM_BYTE b10, SYSTEM_BYTE b11, SYSTEM_BYTE b12);
+export void Asm_Char13 (SYSTEM_BYTE b1, SYSTEM_BYTE b2, SYSTEM_BYTE b3, SYSTEM_BYTE b4, SYSTEM_BYTE b5, SYSTEM_BYTE b6, SYSTEM_BYTE b7, SYSTEM_BYTE b8, SYSTEM_BYTE b9, SYSTEM_BYTE b10, SYSTEM_BYTE b11, SYSTEM_BYTE b12, SYSTEM_BYTE b13);
+export void Asm_Char14 (SYSTEM_BYTE b1, SYSTEM_BYTE b2, SYSTEM_BYTE b3, SYSTEM_BYTE b4, SYSTEM_BYTE b5, SYSTEM_BYTE b6, SYSTEM_BYTE b7, SYSTEM_BYTE b8, SYSTEM_BYTE b9, SYSTEM_BYTE b10, SYSTEM_BYTE b11, SYSTEM_BYTE b12, SYSTEM_BYTE b13, SYSTEM_BYTE b14);
+export void Asm_Char15 (SYSTEM_BYTE b1, SYSTEM_BYTE b2, SYSTEM_BYTE b3, SYSTEM_BYTE b4, SYSTEM_BYTE b5, SYSTEM_BYTE b6, SYSTEM_BYTE b7, SYSTEM_BYTE b8, SYSTEM_BYTE b9, SYSTEM_BYTE b10, SYSTEM_BYTE b11, SYSTEM_BYTE b12, SYSTEM_BYTE b13, SYSTEM_BYTE b14, SYSTEM_BYTE b15);
+export void Asm_Char16 (SYSTEM_BYTE b1, SYSTEM_BYTE b2, SYSTEM_BYTE b3, SYSTEM_BYTE b4, SYSTEM_BYTE b5, SYSTEM_BYTE b6, SYSTEM_BYTE b7, SYSTEM_BYTE b8, SYSTEM_BYTE b9, SYSTEM_BYTE b10, SYSTEM_BYTE b11, SYSTEM_BYTE b12, SYSTEM_BYTE b13, SYSTEM_BYTE b14, SYSTEM_BYTE b15, SYSTEM_BYTE b16);
+export void Asm_Char2 (SYSTEM_BYTE b1, SYSTEM_BYTE b2);
+export void Asm_Char3 (SYSTEM_BYTE b1, SYSTEM_BYTE b2, SYSTEM_BYTE b3);
+export void Asm_Char4 (SYSTEM_BYTE b1, SYSTEM_BYTE b2, SYSTEM_BYTE b3, SYSTEM_BYTE b4);
+export void Asm_Char5 (SYSTEM_BYTE b1, SYSTEM_BYTE b2, SYSTEM_BYTE b3, SYSTEM_BYTE b4, SYSTEM_BYTE b5);
+export void Asm_Char6 (SYSTEM_BYTE b1, SYSTEM_BYTE b2, SYSTEM_BYTE b3, SYSTEM_BYTE b4, SYSTEM_BYTE b5, SYSTEM_BYTE b6);
+export void Asm_Char7 (SYSTEM_BYTE b1, SYSTEM_BYTE b2, SYSTEM_BYTE b3, SYSTEM_BYTE b4, SYSTEM_BYTE b5, SYSTEM_BYTE b6, SYSTEM_BYTE b7);
+export void Asm_Char8 (SYSTEM_BYTE b1, SYSTEM_BYTE b2, SYSTEM_BYTE b3, SYSTEM_BYTE b4, SYSTEM_BYTE b5, SYSTEM_BYTE b6, SYSTEM_BYTE b7, SYSTEM_BYTE b8);
+export void Asm_Char9 (SYSTEM_BYTE b1, SYSTEM_BYTE b2, SYSTEM_BYTE b3, SYSTEM_BYTE b4, SYSTEM_BYTE b5, SYSTEM_BYTE b6, SYSTEM_BYTE b7, SYSTEM_BYTE b8, SYSTEM_BYTE b9);
+export void Asm_Code (CHAR *str, LONGINT str__len);
+export void Asm_Word (INTEGER w);
+export void Asm_Word1 (INTEGER w);
+export void Asm_Word10 (INTEGER w1, INTEGER w2, INTEGER w3, INTEGER w4, INTEGER w5, INTEGER w6, INTEGER w7, INTEGER w8, INTEGER w9, INTEGER w10);
+export void Asm_Word11 (INTEGER w1, INTEGER w2, INTEGER w3, INTEGER w4, INTEGER w5, INTEGER w6, INTEGER w7, INTEGER w8, INTEGER w9, INTEGER w10, INTEGER w11);
+export void Asm_Word12 (INTEGER w1, INTEGER w2, INTEGER w3, INTEGER w4, INTEGER w5, INTEGER w6, INTEGER w7, INTEGER w8, INTEGER w9, INTEGER w10, INTEGER w11, INTEGER w12);
+export void Asm_Word13 (INTEGER w1, INTEGER w2, INTEGER w3, INTEGER w4, INTEGER w5, INTEGER w6, INTEGER w7, INTEGER w8, INTEGER w9, INTEGER w10, INTEGER w11, INTEGER w12, INTEGER w13);
+export void Asm_Word14 (INTEGER w1, INTEGER w2, INTEGER w3, INTEGER w4, INTEGER w5, INTEGER w6, INTEGER w7, INTEGER w8, INTEGER w9, INTEGER w10, INTEGER w11, INTEGER w12, INTEGER w13, INTEGER w14);
+export void Asm_Word15 (INTEGER w1, INTEGER w2, INTEGER w3, INTEGER w4, INTEGER w5, INTEGER w6, INTEGER w7, INTEGER w8, INTEGER w9, INTEGER w10, INTEGER w11, INTEGER w12, INTEGER w13, INTEGER w14, INTEGER w15);
+export void Asm_Word16 (INTEGER w1, INTEGER w2, INTEGER w3, INTEGER w4, INTEGER w5, INTEGER w6, INTEGER w7, INTEGER w8, INTEGER w9, INTEGER w10, INTEGER w11, INTEGER w12, INTEGER w13, INTEGER w14, INTEGER w15, INTEGER w16);
+export void Asm_Word2 (INTEGER w1, INTEGER w2);
+export void Asm_Word3 (INTEGER w1, INTEGER w2, INTEGER w3);
+export void Asm_Word4 (INTEGER w1, INTEGER w2, INTEGER w3, INTEGER w4);
+export void Asm_Word5 (INTEGER w1, INTEGER w2, INTEGER w3, INTEGER w4, INTEGER w5);
+export void Asm_Word6 (INTEGER w1, INTEGER w2, INTEGER w3, INTEGER w4, INTEGER w5, INTEGER w6);
+export void Asm_Word7 (INTEGER w1, INTEGER w2, INTEGER w3, INTEGER w4, INTEGER w5, INTEGER w6, INTEGER w7);
+export void Asm_Word8 (INTEGER w1, INTEGER w2, INTEGER w3, INTEGER w4, INTEGER w5, INTEGER w6, INTEGER w7, INTEGER w8);
+export void Asm_Word9 (INTEGER w1, INTEGER w2, INTEGER w3, INTEGER w4, INTEGER w5, INTEGER w6, INTEGER w7, INTEGER w8, INTEGER w9);
+
+
+/*============================================================================*/
+
+void Asm_Byte (INTEGER b)
+{
+}
+
+/*----------------------------------------------------------------------------*/
+void Asm_Byte1 (INTEGER b)
+{
+}
+
+/*----------------------------------------------------------------------------*/
+void Asm_Byte2 (INTEGER b1, INTEGER b2)
+{
+}
+
+/*----------------------------------------------------------------------------*/
+void Asm_Byte3 (INTEGER b1, INTEGER b2, INTEGER b3)
+{
+}
+
+/*----------------------------------------------------------------------------*/
+void Asm_Byte4 (INTEGER b1, INTEGER b2, INTEGER b3, INTEGER b4)
+{
+}
+
+/*----------------------------------------------------------------------------*/
+void Asm_Byte5 (INTEGER b1, INTEGER b2, INTEGER b3, INTEGER b4, INTEGER b5)
+{
+}
+
+/*----------------------------------------------------------------------------*/
+void Asm_Byte6 (INTEGER b1, INTEGER b2, INTEGER b3, INTEGER b4, INTEGER b5, INTEGER b6)
+{
+}
+
+/*----------------------------------------------------------------------------*/
+void Asm_Byte7 (INTEGER b1, INTEGER b2, INTEGER b3, INTEGER b4, INTEGER b5, INTEGER b6, INTEGER b7)
+{
+}
+
+/*----------------------------------------------------------------------------*/
+void Asm_Byte8 (INTEGER b1, INTEGER b2, INTEGER b3, INTEGER b4, INTEGER b5, INTEGER b6, INTEGER b7, INTEGER b8)
+{
+}
+
+/*----------------------------------------------------------------------------*/
+void Asm_Byte9 (INTEGER b1, INTEGER b2, INTEGER b3, INTEGER b4, INTEGER b5, INTEGER b6, INTEGER b7, INTEGER b8, INTEGER b9)
+{
+}
+
+/*----------------------------------------------------------------------------*/
+void Asm_Byte10 (INTEGER b1, INTEGER b2, INTEGER b3, INTEGER b4, INTEGER b5, INTEGER b6, INTEGER b7, INTEGER b8, INTEGER b9, INTEGER b10)
+{
+}
+
+/*----------------------------------------------------------------------------*/
+void Asm_Byte11 (INTEGER b1, INTEGER b2, INTEGER b3, INTEGER b4, INTEGER b5, INTEGER b6, INTEGER b7, INTEGER b8, INTEGER b9, INTEGER b10, INTEGER b11)
+{
+}
+
+/*----------------------------------------------------------------------------*/
+void Asm_Byte12 (INTEGER b1, INTEGER b2, INTEGER b3, INTEGER b4, INTEGER b5, INTEGER b6, INTEGER b7, INTEGER b8, INTEGER b9, INTEGER b10, INTEGER b11, INTEGER b12)
+{
+}
+
+/*----------------------------------------------------------------------------*/
+void Asm_Byte13 (INTEGER b1, INTEGER b2, INTEGER b3, INTEGER b4, INTEGER b5, INTEGER b6, INTEGER b7, INTEGER b8, INTEGER b9, INTEGER b10, INTEGER b11, INTEGER b12, INTEGER b13)
+{
+}
+
+/*----------------------------------------------------------------------------*/
+void Asm_Byte14 (INTEGER b1, INTEGER b2, INTEGER b3, INTEGER b4, INTEGER b5, INTEGER b6, INTEGER b7, INTEGER b8, INTEGER b9, INTEGER b10, INTEGER b11, INTEGER b12, INTEGER b13, INTEGER b14)
+{
+}
+
+/*----------------------------------------------------------------------------*/
+void Asm_Byte15 (INTEGER b1, INTEGER b2, INTEGER b3, INTEGER b4, INTEGER b5, INTEGER b6, INTEGER b7, INTEGER b8, INTEGER b9, INTEGER b10, INTEGER b11, INTEGER b12, INTEGER b13, INTEGER b14, INTEGER b15)
+{
+}
+
+/*----------------------------------------------------------------------------*/
+void Asm_Byte16 (INTEGER b1, INTEGER b2, INTEGER b3, INTEGER b4, INTEGER b5, INTEGER b6, INTEGER b7, INTEGER b8, INTEGER b9, INTEGER b10, INTEGER b11, INTEGER b12, INTEGER b13, INTEGER b14, INTEGER b15, INTEGER b16)
+{
+}
+
+/*----------------------------------------------------------------------------*/
+void Asm_Char (SYSTEM_BYTE b)
+{
+}
+
+/*----------------------------------------------------------------------------*/
+void Asm_Char1 (SYSTEM_BYTE b)
+{
+}
+
+/*----------------------------------------------------------------------------*/
+void Asm_Char2 (SYSTEM_BYTE b1, SYSTEM_BYTE b2)
+{
+}
+
+/*----------------------------------------------------------------------------*/
+void Asm_Char3 (SYSTEM_BYTE b1, SYSTEM_BYTE b2, SYSTEM_BYTE b3)
+{
+}
+
+/*----------------------------------------------------------------------------*/
+void Asm_Char4 (SYSTEM_BYTE b1, SYSTEM_BYTE b2, SYSTEM_BYTE b3, SYSTEM_BYTE b4)
+{
+}
+
+/*----------------------------------------------------------------------------*/
+void Asm_Char5 (SYSTEM_BYTE b1, SYSTEM_BYTE b2, SYSTEM_BYTE b3, SYSTEM_BYTE b4, SYSTEM_BYTE b5)
+{
+}
+
+/*----------------------------------------------------------------------------*/
+void Asm_Char6 (SYSTEM_BYTE b1, SYSTEM_BYTE b2, SYSTEM_BYTE b3, SYSTEM_BYTE b4, SYSTEM_BYTE b5, SYSTEM_BYTE b6)
+{
+}
+
+/*----------------------------------------------------------------------------*/
+void Asm_Char7 (SYSTEM_BYTE b1, SYSTEM_BYTE b2, SYSTEM_BYTE b3, SYSTEM_BYTE b4, SYSTEM_BYTE b5, SYSTEM_BYTE b6, SYSTEM_BYTE b7)
+{
+}
+
+/*----------------------------------------------------------------------------*/
+void Asm_Char8 (SYSTEM_BYTE b1, SYSTEM_BYTE b2, SYSTEM_BYTE b3, SYSTEM_BYTE b4, SYSTEM_BYTE b5, SYSTEM_BYTE b6, SYSTEM_BYTE b7, SYSTEM_BYTE b8)
+{
+}
+
+/*----------------------------------------------------------------------------*/
+void Asm_Char9 (SYSTEM_BYTE b1, SYSTEM_BYTE b2, SYSTEM_BYTE b3, SYSTEM_BYTE b4, SYSTEM_BYTE b5, SYSTEM_BYTE b6, SYSTEM_BYTE b7, SYSTEM_BYTE b8, SYSTEM_BYTE b9)
+{
+}
+
+/*----------------------------------------------------------------------------*/
+void Asm_Char10 (SYSTEM_BYTE b1, SYSTEM_BYTE b2, SYSTEM_BYTE b3, SYSTEM_BYTE b4, SYSTEM_BYTE b5, SYSTEM_BYTE b6, SYSTEM_BYTE b7, SYSTEM_BYTE b8, SYSTEM_BYTE b9, SYSTEM_BYTE b10)
+{
+}
+
+/*----------------------------------------------------------------------------*/
+void Asm_Char11 (SYSTEM_BYTE b1, SYSTEM_BYTE b2, SYSTEM_BYTE b3, SYSTEM_BYTE b4, SYSTEM_BYTE b5, SYSTEM_BYTE b6, SYSTEM_BYTE b7, SYSTEM_BYTE b8, SYSTEM_BYTE b9, SYSTEM_BYTE b10, SYSTEM_BYTE b11)
+{
+}
+
+/*----------------------------------------------------------------------------*/
+void Asm_Char12 (SYSTEM_BYTE b1, SYSTEM_BYTE b2, SYSTEM_BYTE b3, SYSTEM_BYTE b4, SYSTEM_BYTE b5, SYSTEM_BYTE b6, SYSTEM_BYTE b7, SYSTEM_BYTE b8, SYSTEM_BYTE b9, SYSTEM_BYTE b10, SYSTEM_BYTE b11, SYSTEM_BYTE b12)
+{
+}
+
+/*----------------------------------------------------------------------------*/
+void Asm_Char13 (SYSTEM_BYTE b1, SYSTEM_BYTE b2, SYSTEM_BYTE b3, SYSTEM_BYTE b4, SYSTEM_BYTE b5, SYSTEM_BYTE b6, SYSTEM_BYTE b7, SYSTEM_BYTE b8, SYSTEM_BYTE b9, SYSTEM_BYTE b10, SYSTEM_BYTE b11, SYSTEM_BYTE b12, SYSTEM_BYTE b13)
+{
+}
+
+/*----------------------------------------------------------------------------*/
+void Asm_Char14 (SYSTEM_BYTE b1, SYSTEM_BYTE b2, SYSTEM_BYTE b3, SYSTEM_BYTE b4, SYSTEM_BYTE b5, SYSTEM_BYTE b6, SYSTEM_BYTE b7, SYSTEM_BYTE b8, SYSTEM_BYTE b9, SYSTEM_BYTE b10, SYSTEM_BYTE b11, SYSTEM_BYTE b12, SYSTEM_BYTE b13, SYSTEM_BYTE b14)
+{
+}
+
+/*----------------------------------------------------------------------------*/
+void Asm_Char15 (SYSTEM_BYTE b1, SYSTEM_BYTE b2, SYSTEM_BYTE b3, SYSTEM_BYTE b4, SYSTEM_BYTE b5, SYSTEM_BYTE b6, SYSTEM_BYTE b7, SYSTEM_BYTE b8, SYSTEM_BYTE b9, SYSTEM_BYTE b10, SYSTEM_BYTE b11, SYSTEM_BYTE b12, SYSTEM_BYTE b13, SYSTEM_BYTE b14, SYSTEM_BYTE b15)
+{
+}
+
+/*----------------------------------------------------------------------------*/
+void Asm_Char16 (SYSTEM_BYTE b1, SYSTEM_BYTE b2, SYSTEM_BYTE b3, SYSTEM_BYTE b4, SYSTEM_BYTE b5, SYSTEM_BYTE b6, SYSTEM_BYTE b7, SYSTEM_BYTE b8, SYSTEM_BYTE b9, SYSTEM_BYTE b10, SYSTEM_BYTE b11, SYSTEM_BYTE b12, SYSTEM_BYTE b13, SYSTEM_BYTE b14, SYSTEM_BYTE b15, SYSTEM_BYTE b16)
+{
+}
+
+/*----------------------------------------------------------------------------*/
+void Asm_Word (INTEGER w)
+{
+}
+
+/*----------------------------------------------------------------------------*/
+void Asm_Word1 (INTEGER w)
+{
+}
+
+/*----------------------------------------------------------------------------*/
+void Asm_Word2 (INTEGER w1, INTEGER w2)
+{
+}
+
+/*----------------------------------------------------------------------------*/
+void Asm_Word3 (INTEGER w1, INTEGER w2, INTEGER w3)
+{
+}
+
+/*----------------------------------------------------------------------------*/
+void Asm_Word4 (INTEGER w1, INTEGER w2, INTEGER w3, INTEGER w4)
+{
+}
+
+/*----------------------------------------------------------------------------*/
+void Asm_Word5 (INTEGER w1, INTEGER w2, INTEGER w3, INTEGER w4, INTEGER w5)
+{
+}
+
+/*----------------------------------------------------------------------------*/
+void Asm_Word6 (INTEGER w1, INTEGER w2, INTEGER w3, INTEGER w4, INTEGER w5, INTEGER w6)
+{
+}
+
+/*----------------------------------------------------------------------------*/
+void Asm_Word7 (INTEGER w1, INTEGER w2, INTEGER w3, INTEGER w4, INTEGER w5, INTEGER w6, INTEGER w7)
+{
+}
+
+/*----------------------------------------------------------------------------*/
+void Asm_Word8 (INTEGER w1, INTEGER w2, INTEGER w3, INTEGER w4, INTEGER w5, INTEGER w6, INTEGER w7, INTEGER w8)
+{
+}
+
+/*----------------------------------------------------------------------------*/
+void Asm_Word9 (INTEGER w1, INTEGER w2, INTEGER w3, INTEGER w4, INTEGER w5, INTEGER w6, INTEGER w7, INTEGER w8, INTEGER w9)
+{
+}
+
+/*----------------------------------------------------------------------------*/
+void Asm_Word10 (INTEGER w1, INTEGER w2, INTEGER w3, INTEGER w4, INTEGER w5, INTEGER w6, INTEGER w7, INTEGER w8, INTEGER w9, INTEGER w10)
+{
+}
+
+/*----------------------------------------------------------------------------*/
+void Asm_Word11 (INTEGER w1, INTEGER w2, INTEGER w3, INTEGER w4, INTEGER w5, INTEGER w6, INTEGER w7, INTEGER w8, INTEGER w9, INTEGER w10, INTEGER w11)
+{
+}
+
+/*----------------------------------------------------------------------------*/
+void Asm_Word12 (INTEGER w1, INTEGER w2, INTEGER w3, INTEGER w4, INTEGER w5, INTEGER w6, INTEGER w7, INTEGER w8, INTEGER w9, INTEGER w10, INTEGER w11, INTEGER w12)
+{
+}
+
+/*----------------------------------------------------------------------------*/
+void Asm_Word13 (INTEGER w1, INTEGER w2, INTEGER w3, INTEGER w4, INTEGER w5, INTEGER w6, INTEGER w7, INTEGER w8, INTEGER w9, INTEGER w10, INTEGER w11, INTEGER w12, INTEGER w13)
+{
+}
+
+/*----------------------------------------------------------------------------*/
+void Asm_Word14 (INTEGER w1, INTEGER w2, INTEGER w3, INTEGER w4, INTEGER w5, INTEGER w6, INTEGER w7, INTEGER w8, INTEGER w9, INTEGER w10, INTEGER w11, INTEGER w12, INTEGER w13, INTEGER w14)
+{
+}
+
+/*----------------------------------------------------------------------------*/
+void Asm_Word15 (INTEGER w1, INTEGER w2, INTEGER w3, INTEGER w4, INTEGER w5, INTEGER w6, INTEGER w7, INTEGER w8, INTEGER w9, INTEGER w10, INTEGER w11, INTEGER w12, INTEGER w13, INTEGER w14, INTEGER w15)
+{
+}
+
+/*----------------------------------------------------------------------------*/
+void Asm_Word16 (INTEGER w1, INTEGER w2, INTEGER w3, INTEGER w4, INTEGER w5, INTEGER w6, INTEGER w7, INTEGER w8, INTEGER w9, INTEGER w10, INTEGER w11, INTEGER w12, INTEGER w13, INTEGER w14, INTEGER w15, INTEGER w16)
+{
+}
+
+/*----------------------------------------------------------------------------*/
+void Asm_Code (CHAR *str, LONGINT str__len)
+{
+	__DUP(str, str__len, CHAR);
+	__DEL(str);
+}
+
+/*----------------------------------------------------------------------------*/
+
+export void *Asm__init(void)
+{
+	__DEFMOD;
+	__REGMOD("Asm", 0);
+/* BEGIN */
+	__ENDMOD;
+}
