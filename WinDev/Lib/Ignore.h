bdde21e6f42ba7d4f0b0b704d2335ea605ee5d9d
Oleg N. Cher <allot@bk.ru>
2014-07-29 00:55:43 +0300

    + WinDev/Lib/Ignore

---
+++ WinDev/Lib/Ignore.h
@@ -0,0 +1,16 @@
+/*  Ofront 1.2 -xtspkae */
+
+#ifndef Ignore__h
+#define Ignore__h
+
+
+#define Ignore_Bool(x) x
+#define Ignore_Char(x) x
+#define Ignore_Int(x) x
+#define Ignore_Ptr(x) x
+#define Ignore_Real(x) x
+#define Ignore_Set(x) x
+#define Ignore__init()
+
+
+#endif
---
+++ WinDev/Lib/Obj/Ignore.bat
@@ -0,0 +1,2 @@
+@IF EXIST Ignore.h DEL Ignore.h
+@IF EXIST Ignore.c DEL Ignore.c
