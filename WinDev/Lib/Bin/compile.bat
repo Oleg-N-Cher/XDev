e48aac16587ecd0805f52a6796a40e282782df56
Oleg N. Cher <allot@bk.ru>
2014-08-02 00:08:27 +0300

    * WinDev: update build scripts.

--- WinDev/Lib/Bin/compile.bat
+++ WinDev/Lib/Bin/compile.bat
@@ -1,3 +1,19 @@
 @CD ..
+@IF EXIST %1.c GOTO clib
+
+:olib
+@COPY Obj\%2.h
+..\Bin\tcc\tcc -c Obj\%1.c -I "." -I Obj
+@IF errorlevel 1 GOTO noinit
+@GOTO exit
+
+:noinit
+..\..\Bin\smartlib Obj\%1.c -noinit -nocut
+..\Bin\tcc\tcc -c Obj\%1.c -I "." -I Obj
+@GOTO exit
+
+:clib
 ..\Bin\tcc\tcc -c %1.c -I "." -I Obj
+
+:exit
 @IF errorlevel 1 PAUSE
---
+++ WinDev/Lib/Bin/smart.bat
@@ -0,0 +1,24 @@
+@REM args:
+@REM   LibName ModName [PartName] [-noinit] [-nocut]
+
+@SET RootBin=..\..\Bin
+@SET Bin=..\Bin
+@SET tcc=%Bin%\tcc\tcc.exe
+
+@IF EXIST %2.c GOTO clib
+
+:olib
+@COPY Obj\%2.h
+%RootBin%\smartlib Obj\%2.c %3 %4 %5
+@GOTO compile
+
+:clib
+%RootBin%\smartlib %2.c %3 %4 %5
+
+:compile
+@FOR %%i IN (%2_???.c) DO (
+  %tcc% -c %%i -I "." -I Obj
+  @IF errorlevel 1 PAUSE
+)
+@FOR %%i IN (%2_???.o) DO %Bin%\ar -rc %1 %%i
+@Bin\clear
