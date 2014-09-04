3e0685b46c8a2ad528b124f0580dee18767ef1eb
Oleg N. Cher <allot@bk.ru>
2014-09-04 03:36:51 +0300

    d:/WIN32APP/Dev/Git/ WinDev/Lib/Acl.

--- WinDev/Lib/Obj/AclLib.h
+++ WinDev/Lib/Obj/AclLib.h
@@ -12,37 +12,180 @@ typedef
 	struct AclLib_TWinControl {
 		char _prvt0[788];
 		SYSTEM_PTR Brush;
-		char _prvt1[4];
+		char _prvt1[12];
 		INTEGER Color, TextColor;
 		char _prvt2[4];
-		SET Style;
-		char _prvt3[8];
+		SET Style, ExStyle;
+		char _prvt3[4];
 		INTEGER Left, Top, Width, Height;
-		char _prvt4[15];
+		AclLib_PWinControl Parent;
+		BOOLEAN AutoStop;
+		char _prvt4[7];
+		BOOLEAN Enabled, IsModal;
+		char _prvt5[7];
 		BOOLEAN Visible;
-		char _prvt5[4];
+		char _prvt6[6];
 	} AclLib_TWinControl;
 
 import void AclLib_TWinControl_Create (AclLib_TWinControl *w, LONGINT *w__typ, AclLib_PWinControl AParent);
+import void AclLib_TWinControl_CreateWnd (AclLib_TWinControl *w, LONGINT *w__typ);
 import void AclLib_TWinControl_SetCaption (AclLib_TWinControl *w, LONGINT *w__typ, CHAR *value, LONGINT value__len);
 import void AclLib_TWinControl_SetColor (AclLib_TWinControl *w, LONGINT *w__typ, INTEGER value);
 import void AclLib_TWinControl_SetHeight (AclLib_TWinControl *w, LONGINT *w__typ, INTEGER value);
 import void AclLib_TWinControl_SetLeft (AclLib_TWinControl *w, LONGINT *w__typ, INTEGER value);
 import void AclLib_TWinControl_SetTextColor (AclLib_TWinControl *w, LONGINT *w__typ, INTEGER value);
 import void AclLib_TWinControl_SetTop (AclLib_TWinControl *w, LONGINT *w__typ, INTEGER value);
+import void AclLib_TWinControl_SetVisible (AclLib_TWinControl *w, LONGINT *w__typ, BOOLEAN value);
 import void AclLib_TWinControl_SetWidth (AclLib_TWinControl *w, LONGINT *w__typ, INTEGER value);
+import INTEGER AclLib_TWinControl_ShowModal (AclLib_TWinControl *w, LONGINT *w__typ);
 #define __AclLib_TWinControl_Create(w, w__typ, AParent) __SEND(w__typ, AclLib_TWinControl_Create, 0, void(*)(AclLib_TWinControl*, LONGINT *, AclLib_PWinControl), (w, w__typ, AParent))
-#define __AclLib_TWinControl_SetCaption(w, w__typ, value, value__len) __SEND(w__typ, AclLib_TWinControl_SetCaption, 5, void(*)(AclLib_TWinControl*, LONGINT *, CHAR*, LONGINT ), (w, w__typ, value, value__len))
-#define __AclLib_TWinControl_SetColor(w, w__typ, value) __SEND(w__typ, AclLib_TWinControl_SetColor, 6, void(*)(AclLib_TWinControl*, LONGINT *, INTEGER), (w, w__typ, value))
-#define __AclLib_TWinControl_SetHeight(w, w__typ, value) __SEND(w__typ, AclLib_TWinControl_SetHeight, 7, void(*)(AclLib_TWinControl*, LONGINT *, INTEGER), (w, w__typ, value))
-#define __AclLib_TWinControl_SetLeft(w, w__typ, value) __SEND(w__typ, AclLib_TWinControl_SetLeft, 8, void(*)(AclLib_TWinControl*, LONGINT *, INTEGER), (w, w__typ, value))
-#define __AclLib_TWinControl_SetTextColor(w, w__typ, value) __SEND(w__typ, AclLib_TWinControl_SetTextColor, 9, void(*)(AclLib_TWinControl*, LONGINT *, INTEGER), (w, w__typ, value))
-#define __AclLib_TWinControl_SetTop(w, w__typ, value) __SEND(w__typ, AclLib_TWinControl_SetTop, 10, void(*)(AclLib_TWinControl*, LONGINT *, INTEGER), (w, w__typ, value))
-#define __AclLib_TWinControl_SetWidth(w, w__typ, value) __SEND(w__typ, AclLib_TWinControl_SetWidth, 11, void(*)(AclLib_TWinControl*, LONGINT *, INTEGER), (w, w__typ, value))
+#define __AclLib_TWinControl_CreateWnd(w, w__typ) __SEND(w__typ, AclLib_TWinControl_CreateWnd, 3, void(*)(AclLib_TWinControl*, LONGINT *), (w, w__typ))
+#define __AclLib_TWinControl_SetCaption(w, w__typ, value, value__len) __SEND(w__typ, AclLib_TWinControl_SetCaption, 14, void(*)(AclLib_TWinControl*, LONGINT *, CHAR*, LONGINT ), (w, w__typ, value, value__len))
+#define __AclLib_TWinControl_SetColor(w, w__typ, value) __SEND(w__typ, AclLib_TWinControl_SetColor, 15, void(*)(AclLib_TWinControl*, LONGINT *, INTEGER), (w, w__typ, value))
+#define __AclLib_TWinControl_SetHeight(w, w__typ, value) __SEND(w__typ, AclLib_TWinControl_SetHeight, 17, void(*)(AclLib_TWinControl*, LONGINT *, INTEGER), (w, w__typ, value))
+#define __AclLib_TWinControl_SetLeft(w, w__typ, value) __SEND(w__typ, AclLib_TWinControl_SetLeft, 18, void(*)(AclLib_TWinControl*, LONGINT *, INTEGER), (w, w__typ, value))
+#define __AclLib_TWinControl_SetTextColor(w, w__typ, value) __SEND(w__typ, AclLib_TWinControl_SetTextColor, 19, void(*)(AclLib_TWinControl*, LONGINT *, INTEGER), (w, w__typ, value))
+#define __AclLib_TWinControl_SetTop(w, w__typ, value) __SEND(w__typ, AclLib_TWinControl_SetTop, 20, void(*)(AclLib_TWinControl*, LONGINT *, INTEGER), (w, w__typ, value))
+#define __AclLib_TWinControl_SetVisible(w, w__typ, value) __SEND(w__typ, AclLib_TWinControl_SetVisible, 21, void(*)(AclLib_TWinControl*, LONGINT *, BOOLEAN), (w, w__typ, value))
+#define __AclLib_TWinControl_SetWidth(w, w__typ, value) __SEND(w__typ, AclLib_TWinControl_SetWidth, 22, void(*)(AclLib_TWinControl*, LONGINT *, INTEGER), (w, w__typ, value))
+#define __AclLib_TWinControl_ShowModal(w, w__typ) __SEND(w__typ, AclLib_TWinControl_ShowModal, 24, INTEGER(*)(AclLib_TWinControl*, LONGINT *), (w, w__typ))
+
+typedef
+	struct AclLib_TStdControl { /* AclLib_TWinControl */
+		char _prvt0[788];
+		SYSTEM_PTR Brush;
+		char _prvt1[12];
+		INTEGER Color, TextColor;
+		char _prvt2[4];
+		SET Style, ExStyle;
+		char _prvt3[4];
+		INTEGER Left, Top, Width, Height;
+		AclLib_PWinControl Parent;
+		BOOLEAN AutoStop;
+		char _prvt4[7];
+		BOOLEAN Enabled, IsModal;
+		char _prvt5[7];
+		BOOLEAN Visible;
+		char _prvt6[6];
+	} AclLib_TStdControl;
+
+typedef
+	struct AclLib_TButton { /* AclLib_TStdControl */
+		char _prvt0[788];
+		SYSTEM_PTR Brush;
+		char _prvt1[12];
+		INTEGER Color, TextColor;
+		char _prvt2[4];
+		SET Style, ExStyle;
+		char _prvt3[4];
+		INTEGER Left, Top, Width, Height;
+		AclLib_PWinControl Parent;
+		BOOLEAN AutoStop;
+		char _prvt4[7];
+		BOOLEAN Enabled, IsModal;
+		char _prvt5[7];
+		BOOLEAN Visible;
+		char _prvt6[6];
+	} AclLib_TButton;
+
+typedef
+	struct AclLib_TCheckBox { /* AclLib_TStdControl */
+		char _prvt0[788];
+		SYSTEM_PTR Brush;
+		char _prvt1[12];
+		INTEGER Color, TextColor;
+		char _prvt2[4];
+		SET Style, ExStyle;
+		char _prvt3[4];
+		INTEGER Left, Top, Width, Height;
+		AclLib_PWinControl Parent;
+		BOOLEAN AutoStop;
+		char _prvt4[7];
+		BOOLEAN Enabled, IsModal;
+		char _prvt5[7];
+		BOOLEAN Visible;
+		char _prvt6[6];
+	} AclLib_TCheckBox;
+
+typedef
+	struct AclLib_TCreateParams {
+		INTEGER _prvt0;
+		char _prvt1[388];
+	} AclLib_TCreateParams;
+
+typedef
+	struct AclLib_TLabel { /* AclLib_TStdControl */
+		char _prvt0[788];
+		SYSTEM_PTR Brush;
+		char _prvt1[12];
+		INTEGER Color, TextColor;
+		char _prvt2[4];
+		SET Style, ExStyle;
+		char _prvt3[4];
+		INTEGER Left, Top, Width, Height;
+		AclLib_PWinControl Parent;
+		BOOLEAN AutoStop;
+		char _prvt4[7];
+		BOOLEAN Enabled, IsModal;
+		char _prvt5[7];
+		BOOLEAN Visible;
+		char _prvt6[6];
+	} AclLib_TLabel;
+
+typedef
+	struct AclLib_TListBox { /* AclLib_TStdControl */
+		char _prvt0[788];
+		SYSTEM_PTR Brush;
+		char _prvt1[12];
+		INTEGER Color, TextColor;
+		char _prvt2[4];
+		SET Style, ExStyle;
+		char _prvt3[4];
+		INTEGER Left, Top, Width, Height;
+		AclLib_PWinControl Parent;
+		BOOLEAN AutoStop;
+		char _prvt4[7];
+		BOOLEAN Enabled, IsModal;
+		char _prvt5[7];
+		BOOLEAN Visible;
+		char _prvt6[6];
+	} AclLib_TListBox;
+
+typedef
+	struct AclLib_TObject {
+		char _prvt0[1];
+	} AclLib_TObject;
+
+typedef
+	struct AclLib_TProgressBar { /* AclLib_TStdControl */
+		char _prvt0[788];
+		SYSTEM_PTR Brush;
+		char _prvt1[12];
+		INTEGER Color, TextColor;
+		char _prvt2[4];
+		SET Style, ExStyle;
+		char _prvt3[4];
+		INTEGER Left, Top, Width, Height;
+		AclLib_PWinControl Parent;
+		BOOLEAN AutoStop;
+		char _prvt4[7];
+		BOOLEAN Enabled, IsModal;
+		char _prvt5[7];
+		BOOLEAN Visible;
+		char _prvt6[6];
+	} AclLib_TProgressBar;
 
 
 
+import LONGINT *AclLib_TObject__typ;
+import LONGINT *AclLib_TCreateParams__typ;
 import LONGINT *AclLib_TWinControl__typ;
+import LONGINT *AclLib_TStdControl__typ;
+import LONGINT *AclLib_TLabel__typ;
+import LONGINT *AclLib_TListBox__typ;
+import LONGINT *AclLib_TButton__typ;
+import LONGINT *AclLib_TCheckBox__typ;
+import LONGINT *AclLib_TProgressBar__typ;
 
 import void *AclLib__init(void);
 
--- WinDev/Obj64/AclDemo.c
+++ WinDev/Obj64/AclDemo.c
@@ -1,6 +1,7 @@
 /*  Ofront 1.2 -xtspkaem */
 #include "SYSTEM.h"
 #include "AclLib.h"
+#include "Ignore.h"
 #include "AclUtils.h"
 #include "WinApi.h"
 
@@ -8,15 +9,32 @@ typedef
 	struct AclDemo_TWnd1 { /* AclLib_TWinControl */
 		char _prvt0[788];
 		SYSTEM_PTR Brush;
+		char _prvt1[8];
 		INTEGER Color, TextColor;
-		char _prvt1[4];
-		SET Style;
-		char _prvt2[8];
+		char _prvt2[4];
+		SET Style, ExStyle;
+		char _prvt3[4];
 		INTEGER Left, Top, Width, Height;
-		char _prvt3[15];
+		AclLib_PWinControl Parent;
+		BOOLEAN AutoStop;
+		char _prvt4[7];
+		BOOLEAN Enabled, IsModal;
+		char _prvt5[7];
 		BOOLEAN Visible;
-		char _prvt4[4];
+		char _prvt6[4];
+		AclLib_TLabel L1;
+		AclLib_TListBox Lb1;
+		AclLib_TCheckBox Ch1, Ch2, Ch3, Ch4;
+		AclLib_TButton Btn1, Btn2;
+		AclLib_TProgressBar Prg1;
 	} AclDemo_TWnd1;
+
+static void AclDemo_TWnd1_Btn1Click (AclDemo_TWnd1 *w, LONGINT *w__typ, AclLib_TObject sender);
+static void AclDemo_TWnd1_Btn2Click (AclDemo_TWnd1 *w, LONGINT *w__typ, AclLib_TObject sender);
+static void AclDemo_TWnd1_WndCreate (AclDemo_TWnd1 *w, LONGINT *w__typ, AclLib_TObject sender);
+#define __AclDemo_TWnd1_Btn1Click(w, w__typ, sender) __SEND(w__typ, AclDemo_TWnd1_Btn1Click, 25, void(*)(AclDemo_TWnd1*, LONGINT *, AclLib_TObject), (w, w__typ, sender))
+#define __AclDemo_TWnd1_Btn2Click(w, w__typ, sender) __SEND(w__typ, AclDemo_TWnd1_Btn2Click, 26, void(*)(AclDemo_TWnd1*, LONGINT *, AclLib_TObject), (w, w__typ, sender))
+#define __AclDemo_TWnd1_WndCreate(w, w__typ, sender) __SEND(w__typ, AclDemo_TWnd1_WndCreate, 27, void(*)(AclDemo_TWnd1*, LONGINT *, AclLib_TObject), (w, w__typ, sender))
 
 
 static AclDemo_TWnd1 AclDemo_Wnd1;
@@ -29,21 +47,42 @@ export LONGINT *AclDemo_TWnd1__typ;
 
 /*============================================================================*/
 
-static void EnumPtrs(void (*P)(void*))
+static void AclDemo_TWnd1_WndCreate (AclDemo_TWnd1 *w, LONGINT *w__typ, AclLib_TObject sender)
 {
-	__ENUMR(&AclDemo_Wnd1, AclDemo_TWnd1__typ, 856, 1, P);
+	(*w).AutoStop = 1;
 }
 
-__TDESC(AclDemo_TWnd1__desc, 9, 6) = {__TDFLDS("TWnd1", 856), {0, 4, 788, 792, 836, 844, -56}};
+static void AclDemo_TWnd1_Btn1Click (AclDemo_TWnd1 *w, LONGINT *w__typ, AclLib_TObject sender)
+{
+}
+
+static void AclDemo_TWnd1_Btn2Click (AclDemo_TWnd1 *w, LONGINT *w__typ, AclLib_TObject sender)
+{
+}
+
+static void EnumPtrs(void (*P)(void*))
+{
+	__ENUMR(&AclDemo_Wnd1, AclDemo_TWnd1__typ, 8720, 1, P);
+}
+
+__TDESC(AclDemo_TWnd1__desc, 29, 70) = {__TDFLDS("TWnd1", 8720), {0, 4, 788, 792, 800, 844, 852, 872, 876, 1660, 1664, 1672, 1716, 1724, 1744, 1748, 
+	2532, 2536, 2544, 2588, 2596, 2616, 2620, 3404, 3408, 3416, 3460, 3468, 3488, 3492, 4276, 4280, 
+	4288, 4332, 4340, 4360, 4364, 5148, 5152, 5160, 5204, 5212, 5232, 5236, 6020, 6024, 6032, 6076, 
+	6084, 6104, 6108, 6892, 6896, 6904, 6948, 6956, 6976, 6980, 7764, 7768, 7776, 7820, 7828, 7848, 
+	7852, 8636, 8640, 8648, 8692, 8700, -568}};
 
 export main(int argc, char **argv)
 {
 	__INIT(argc, argv);
 	__IMPORT(AclLib__init);
+	__IMPORT(Ignore__init);
 	__IMPORT(AclUtils__init);
 	__IMPORT(WinApi__init);
 	__REGMAIN("AclDemo", EnumPtrs);
 	__INITYP(AclDemo_TWnd1, AclLib_TWinControl, 1);
+	__INITBP(AclDemo_TWnd1, AclDemo_TWnd1_Btn1Click, 25);
+	__INITBP(AclDemo_TWnd1, AclDemo_TWnd1_Btn2Click, 26);
+	__INITBP(AclDemo_TWnd1, AclDemo_TWnd1_WndCreate, 27);
 /* BEGIN */
 	__MOVE("Sorted ListBox Sample", AclDemo_s1, 22);
 	__MOVE("SORTED LISTBOX SAMPLE", AclDemo_s2, 22);
@@ -55,5 +94,9 @@ export main(int argc, char **argv)
 	__AclLib_TWinControl_SetLeft((void*)&AclDemo_Wnd1, AclDemo_TWnd1__typ, __ASHR(WinApi_GetSystemMetrics(0) - AclDemo_Wnd1.Width, 1));
 	__AclLib_TWinControl_SetTop((void*)&AclDemo_Wnd1, AclDemo_TWnd1__typ, __ASHR(WinApi_GetSystemMetrics(1) - AclDemo_Wnd1.Height, 1));
 	__AclLib_TWinControl_SetColor((void*)&AclDemo_Wnd1, AclDemo_TWnd1__typ, AclUtils_RGB('3', 0x99, 0xff));
+	__AclLib_TWinControl_SetCaption((void*)&AclDemo_Wnd1, AclDemo_TWnd1__typ, (void*)&"Win32 API Programming Sample", (LONGINT)29);
+	__AclLib_TWinControl_CreateWnd((void*)&AclDemo_Wnd1, AclDemo_TWnd1__typ);
+	__AclLib_TWinControl_SetVisible((void*)&AclDemo_Wnd1, AclDemo_TWnd1__typ, 1);
+	Ignore_Int(__AclLib_TWinControl_ShowModal((void*)&AclDemo_Wnd1, AclDemo_TWnd1__typ));
 	__FINI;
 }
