60701ab46f38ed2007245a4d49371e33997ef1d0
Oleg N. Cher <allot@bk.ru>
2014-09-27 20:53:13 +0300

    / ZXDev: / build scripts.

--- ZXDev/Lib/Bin/compile.bat
+++ ZXDev/Lib/Bin/compile.bat
@@ -2,14 +2,14 @@
 @IF EXIST %1.c GOTO c_lib
 
 :o_lib_obj
-..\Bin\sdcc Obj\%1.c -mz80 --opt-code-size --no-std-crt0 --disable-warning 59 --disable-warning 85 -I "." -I include -I Obj -L z80 XDev.lib
+..\Bin\sdcc Obj\%1.c -mz80 --opt-code-size --disable-warning 59 --disable-warning 85 -I "." -I include -I Obj -L z80 XDev.lib
 @IF NOT errorlevel 1 GOTO exit
 ..\..\Bin\smartlib Obj\%1.c -nocut -noinit
-..\Bin\sdcc Obj\%1.c -mz80 --opt-code-size --no-std-crt0 --disable-warning 59 --disable-warning 85 -I "." -I include -I Obj -L z80 XDev.lib
+..\Bin\sdcc Obj\%1.c -mz80 --opt-code-size --disable-warning 59 --disable-warning 85 -I "." -I include -I Obj -L z80 XDev.lib
 @GOTO exit
 
 :c_lib
-..\Bin\sdcc %1.c -mz80 --opt-code-size --no-std-crt0 --disable-warning 59 --disable-warning 85 -I "." -I include -I Obj -L z80 XDev.lib
+..\Bin\sdcc %1.c -mz80 --opt-code-size --disable-warning 59 --disable-warning 85 -I "." -I include -I Obj -L z80 XDev.lib
 
 :exit
 
