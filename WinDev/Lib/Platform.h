c5aa919bb0e462e35ae69d1dc520f3b269a09c8e
Oleg N. Cher <allot@bk.ru>
2014-09-30 20:00:27 +0300

    * WinDev/Lib/Platform.

--- WinDev/Lib/Platform.h
+++ WinDev/Lib/Platform.h
@@ -1,5 +1,3 @@
-/*  Ofront 1.2 -xtspkae */
-
 #ifndef Platform__h
 #define Platform__h
 
@@ -9,13 +7,10 @@ typedef
 	CHAR *Platform_STRING;
 
 
-
-
-import SET Platform_BITS (INTEGER i);
 import void Platform_DISPOSE (SYSTEM_PTR *mem);
-import INTEGER Platform_ORD (SET s);
 #define Platform_Unsigned(i) ((unsigned)(i))
 #define Platform__init()
 
 
 #endif
+
