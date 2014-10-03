235e13540731c846967f823553e488605bf95342
Oleg N. Cher <allot@bk.ru>
2014-10-03 12:32:16 +0300

    + WinDev/Lib/Eco.

--- WinDev/Lib/Bin/buildEco64.bat
+++ WinDev/Lib/Bin/buildEco64.bat
@@ -2,7 +2,7 @@
 @REM =====================
 
 @IF NOT EXIST .tcc64 GOTO exit
-@SET Lib=..\x86-64\Eco64.a
+@SET Lib=..\Eco64.a
 @IF EXIST %Lib% DEL %Lib%
 @CALL ..\Bin\smart64 %Lib% EcoBigSets
 @CALL ..\Bin\smart64 %Lib% EcoListen
