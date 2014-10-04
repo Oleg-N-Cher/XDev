d8a2378e844e5210d3c19546e65807c3be0511bc
Oleg N. Cher <allot@bk.ru>
2014-10-05 01:07:30 +0300

    * WinDev/Lib/GrConsole.

--- WinDev/Lib/C/GrConsole.h
+++ WinDev/Lib/C/GrConsole.h
@@ -1,17 +1,14 @@
-/*  Ofront 1.2 -xtspkae */
-
 #ifndef GrConsole__h
 #define GrConsole__h
 
 #include "SYSTEM.h"
 #include "GrColors.h"
-#include "GrConfig.h"
 
 
 import void GrConsole_At (INTEGER x, INTEGER y);
 import void GrConsole_Clear (INTEGER color);
 import void GrConsole_SetColors (GrColors_Colors colors);
-import void GrConsole_SetFont (SYSTEM_BYTE *font, LONGINT font__len);
+import void GrConsole_SetFont (BYTE *font, LONGINT font__len);
 import void GrConsole_WriteCh (CHAR ch);
 import void GrConsole_WriteInt (INTEGER x);
 import void GrConsole_WriteLn (void);
