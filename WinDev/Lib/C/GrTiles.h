f6d7293a77dabdbb7c4a22f7899258ac361eaf40
Oleg N. Cher <allot@bk.ru>
2014-10-06 23:34:00 +0300

    * WinDev/Lib/GrTiles.

--- WinDev/Lib/C/GrTiles.h
+++ WinDev/Lib/C/GrTiles.h
@@ -1,24 +1,30 @@
-/*  Ofront 1.2 -xtspkae */
-
 #ifndef GrTiles__h
 #define GrTiles__h
 
 #include "SYSTEM.h"
 #include "GrColors.h"
-#include "GrConfig.h"
 
 
 typedef
 	BYTE *GrTiles_Tile;
 
 typedef
-	BYTE GrTiles_Tile8x8[8];
+	BYTE *GrTiles_MonoTile;
 
 typedef
-	BYTE *GrTiles_TranspTile;
+	BYTE GrTiles_MonoTile8x8[8];
 
 typedef
-	BYTE GrTiles_TranspTile8x8[16];
+	struct GrTiles_Tile8x8 {
+		GrTiles_MonoTile8x8 mono;
+		GrColors_Colors colors;
+	} GrTiles_Tile8x8;
+
+typedef
+	BYTE *GrTiles_TranspMonoTile;
+
+typedef
+	BYTE GrTiles_TranspMonoTile8x8[16];
 
 
 import void GrTiles_DrawMonoTile8x8 (INTEGER x, INTEGER y, BYTE *tile, LONGINT tile__len, GrColors_Colors colors);
@@ -28,6 +34,9 @@ import void GrTiles__init (void);
 
 #define GrTiles_DrawMonoTile	GrTiles_DrawMonoTile8x8
 #define GrTiles_DrawTranspMonoTile	GrTiles_DrawTranspMonoTile8x8
+#define GrTiles_DrawTile	GrTiles_DrawTile8x8
+
+#define GrTiles_Tile8x8__typ	NIL
 
 
 #endif
