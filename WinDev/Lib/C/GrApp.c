e911363ba1758f6bf2790f7cfea27722733a2916
Oleg N. Cher <allot@bk.ru>
2014-10-20 00:53:57 +0300

    WinDev/Lib/XDev: graphic colors moved to GrApp.Mod

--- WinDev/Lib/C/GrApp.c
+++ WinDev/Lib/C/GrApp.c
@@ -9,6 +9,11 @@ export SHORTINT GrApp_DepthBits, GrApp_NormalColor;
 export CHAR* GrApp_AppTitle;
 
 
+export INTEGER GrApp_Black, GrApp_Blue, GrApp_Green, GrApp_Cyan, GrApp_Red,
+  GrApp_Magenta, GrApp_Olive, GrApp_White, GrApp_Gray, GrApp_LightBlue,
+  GrApp_LightGreen, GrApp_LightCyan, GrApp_LightRed, GrApp_LightMagenta,
+  GrApp_Yellow, GrApp_LightWhite, GrApp_Orange;
+
 export void GrApp_Close (void);
 export void GrApp_Update (void);
 
@@ -22,6 +27,27 @@ void GrApp_Update (void)
 {
 	SdlLib_Flip(GrApp_Screen);
 }
+
+void GrApp_InitColors (void)
+{
+	GrApp_Black = SdlLib_MapRGB(GrApp_Screen->format, 0, 0, 0);
+	GrApp_Blue = SdlLib_MapRGB(GrApp_Screen->format, 0, 0, GrApp_NormalColor);
+	GrApp_Green = SdlLib_MapRGB(GrApp_Screen->format, 0, GrApp_NormalColor, 0);
+	GrApp_Cyan = SdlLib_MapRGB(GrApp_Screen->format, 0, GrApp_NormalColor, GrApp_NormalColor);
+	GrApp_Red = SdlLib_MapRGB(GrApp_Screen->format, GrApp_NormalColor, 0, 0);
+	GrApp_Magenta = SdlLib_MapRGB(GrApp_Screen->format, GrApp_NormalColor, 0, GrApp_NormalColor);
+	GrApp_Olive = SdlLib_MapRGB(GrApp_Screen->format, GrApp_NormalColor, GrApp_NormalColor, 0);
+	GrApp_White = SdlLib_MapRGB(GrApp_Screen->format, GrApp_NormalColor, GrApp_NormalColor, GrApp_NormalColor);
+	GrApp_Gray = SdlLib_MapRGB(GrApp_Screen->format, GrApp_NormalColor/2, GrApp_NormalColor/2, GrApp_NormalColor/2);
+	GrApp_LightBlue = SdlLib_MapRGB(GrApp_Screen->format, 0, 0, 255);
+	GrApp_LightGreen = SdlLib_MapRGB(GrApp_Screen->format, 0, 255, 0);
+	GrApp_LightCyan = SdlLib_MapRGB(GrApp_Screen->format, 0, 255, 255);
+	GrApp_LightRed = SdlLib_MapRGB(GrApp_Screen->format, 255, 0, 0);
+	GrApp_LightMagenta = SdlLib_MapRGB(GrApp_Screen->format, 255, 0, 255);
+	GrApp_Yellow = SdlLib_MapRGB(GrApp_Screen->format, 255, 255, 0);
+	GrApp_LightWhite = SdlLib_MapRGB(GrApp_Screen->format, 255, 255, 255);
+	GrApp_Orange = SdlLib_MapRGB(GrApp_Screen->format, 255, GrApp_NormalColor, 0);
+} //GrApp_InitColors
 
 export void GrApp_Init (void)
 {
@@ -43,5 +69,6 @@ export void GrApp_Init (void)
 	GrApp_MustLock = SdlLib_MustLock(GrApp_Screen);
 	GrApp_ActualMaxX = GrApp_ActualWidth - 1;
 	GrApp_ActualMaxY = GrApp_ActualHeight - 1;
+	GrApp_InitColors();
 	__ENDMOD;
 }
