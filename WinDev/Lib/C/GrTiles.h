c933811a5993d1c575029b014c7dc97d67cd2950
Oleg N. Cher <allot@bk.ru>
2014-10-20 00:53:57 +0300

    WinDev/Lib/XDev: graphic colors moved to GrApp.Mod

--- WinDev/Lib/C/GrTiles.h
+++ WinDev/Lib/C/GrTiles.h
@@ -2,7 +2,7 @@
 #define GrTiles__h
 
 #include "SYSTEM.h"
-#include "GrColors.h"
+#include "GrApp.h"
 
 
 typedef
@@ -17,7 +17,7 @@ typedef
 typedef
 	struct GrTiles_Tile8x8 {
 		GrTiles_MonoTile8x8 mono;
-		GrColors_Colors colors;
+		GrApp_Colors colors;
 	} GrTiles_Tile8x8;
 
 typedef
@@ -27,8 +27,8 @@ typedef
 	BYTE GrTiles_TranspMonoTile8x8[16];
 
 
-import void GrTiles_DrawMonoTile8x8 (INTEGER x, INTEGER y, BYTE *tile, LONGINT tile__len, GrColors_Colors colors);
-import void GrTiles_DrawTranspMonoTile8x8 (INTEGER x, INTEGER y, BYTE *tile, LONGINT tile__len, GrColors_Colors colors);
+import void GrTiles_DrawMonoTile8x8 (INTEGER x, INTEGER y, BYTE *tile, LONGINT tile__len, GrApp_Colors colors);
+import void GrTiles_DrawTranspMonoTile8x8 (INTEGER x, INTEGER y, BYTE *tile, LONGINT tile__len, GrApp_Colors colors);
 import void GrTiles__init (void);
 
 
