9148669f1520f377e4c50ce468cf9e6a706af305
Oleg N. Cher <allot@bk.ru>
2014-10-20 22:18:58 +0300

    * WinDev/Bin: update build scripts.

--- WinDev/Bin/build.bat
+++ WinDev/Bin/build.bat
@@ -4,11 +4,11 @@
 
 @IF EXIST %1 GOTO config
 
-%tcc% %1.c -I "." -I %Lib% -I %Lib%\Obj %Lib%\x86\Oak.a %Lib%\x86\XDev.a %Lib%\x86\Kol.a %Lib%\LibSdl\Sdl.a %Bin%\tcc\lib\SDL.def -o ..\%1.exe
+%tcc% %1.c -I "." -I %Lib%\C -I %Lib%\Obj %Lib%\Oak.a %Lib%\XDev.a %Lib%\Kol.a %Bin%\tcc\lib\SDL.def -o ..\%1.exe
 @GOTO exit
 
 :config
-%tcc% %1.c -I %1 -I %Lib% -I %Lib%\Obj %Lib%\x86\Oak.a %Lib%\x86\XDev.a %Lib%\x86\Kol.a %Lib%\LibSdl\Sdl.a %Bin%\tcc\lib\SDL.def -o ..\%1.exe
+%tcc% %1.c -I %1 -I %Lib%\C -I %Lib%\Obj %Lib%\Oak.a %Lib%\XDev.a %Lib%\Kol.a %Bin%\tcc\lib\SDL.def -o ..\%1.exe
 
 :exit
 @IF errorlevel 1 PAUSE
