396f14ecd4c9592b7e8f3823b1058dcb8e81f247
Oleg N. Cher <allot@bk.ru>
2014-10-20 22:40:25 +0300

    * WinDev/Lib/Kol: + Obj64.

---
+++ WinDev/Lib/Obj64/KolTypes.h
@@ -0,0 +1,35 @@
+/*  Ofront 1.2 -xtspkae */
+
+#ifndef KolTypes__h
+#define KolTypes__h
+
+#include "SYSTEM.h"
+
+typedef
+	CHAR *KolTypes_AnsiString;
+
+typedef
+	CHAR *KolTypes_KOLString;
+
+typedef
+	struct {
+		LONGINT len[1];
+		CHAR data[1];
+	} *KolTypes_PKOLChar;
+
+typedef
+	struct {
+		LONGINT len[1];
+		CHAR data[1];
+	} *KolTypes_PKOLString;
+
+
+
+
+import void KolTypes_IntToString (LONGINT x, CHAR *s, LONGINT s__len);
+import void KolTypes_StringToInt (CHAR *s, LONGINT s__len, INTEGER *x, INTEGER *res);
+import void *KolTypes__init(void);
+
+#define KolTypes__init()	/*-noinit*/
+
+#endif
