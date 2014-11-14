def9181ce9544684547b5ff36f6db16b6177f97b
Oleg N. Cher <allot@bk.ru>
2014-11-14 05:06:52 +0200

    + ZXDev/Lib/Sound (thanks to Reobne!).

---
+++ ZXDev/Lib/Sound.h
@@ -0,0 +1,16 @@
+/*  Ofront 1.2 -xtspkae */
+
+#ifndef Sound__h
+#define Sound__h
+
+#include "SYSTEM.h"
+
+
+
+
+import void Sound_Play (INTEGER fx);
+import void Sound_RunMe50Hz (void);
+import void *Sound__init(void);
+
+
+#endif
