e49b205bf36c6b599a1ec78f65cb4c7fe11c61a7
Oleg N. Cher <allot@bk.ru>
2014-11-19 04:46:23 +0200

    * ZXDev/Lib/Input.

--- ZXDev/Lib/C/Input.c
+++ ZXDev/Lib/C/Input.c
@@ -13,21 +13,10 @@ __asm
   CALL 0x28E
   LD   L,#0
   INC  E
-  JR   Z,RETURN$
+  RET  Z
   INC  L
-RETURN$:
 __endasm;
 }
-/*
-  LD   IY,#0x5C3A
-  RES  5,1(IY)
-  CALL 0x386E
-  LD   L,#0
-  BIT  5,1(IY)
-  JR   NZ,RETURN$
-  INC  L
-RETURN$:
-*/
 
 /*--------------------------------- Cut here ---------------------------------*/
 /*
@@ -37,24 +26,12 @@ CHAR Input_Read (void)
 {
 __asm
   LD   IY,#0x5C3A
-  ;EI
-  ;RES  5,1(IY)
-LOOP$:
-  CALL 0x28E
-  ;BIT  5,1(IY)
-  INC  E
-  JR   Z,LOOP$
-  LD   L,-50(IY)
-  ;DI
-__endasm;
-}
-/*
-  LD   IY,#0x5C3A
-  EI
   RES  5,1(IY)
 LOOP$:
+  CALL 0x2BF
   BIT  5,1(IY)
   JR   Z,LOOP$
   LD   L,-50(IY)
-  DI
-*/
+__endasm;
+}
+
