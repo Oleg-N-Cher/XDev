f3a3fa97c5120ed34584a745539ca6a1a1f5d62d
Oleg N. Cher <allot@bk.ru>
2014-11-24 05:14:55 +0200

    / ZXDev/Lib/Input: + key repeat feature.

--- ZXDev/Lib/C/Input.h
+++ ZXDev/Lib/C/Input.h
@@ -1,16 +1,32 @@
-/*  Ofront 1.2 -xtspkae */
-
 #ifndef Input__h
 #define Input__h
 
-#include "SYSTEM.h"
+#include "XDevCfg.h"
 
 
+extern unsigned char Input_Available (void);
 
+extern CHAR Input_Read_NoKeyRepeat (void);
+extern CHAR Input_Read_KeyRepeat (void);
+extern CHAR Input_Read_KeyRepeatBuf (void);
+#  ifdef Input_KeyNoRepeat
+#    define Input_Read Input_Read_KeyNoRepeat
+#  endif
+#  ifdef Input_KeyRepeat
+#    define Input_Read Input_Read_KeyRepeat
+#  endif
+#  ifdef Input_KeyRepeatBuf
+#    define Input_Read Input_Read_KeyRepeatBuf
+#  endif
 
-import SHORTINT Input_Available (void);
-import CHAR Input_Read (void);
-#define Input__init()
+extern void Input_RunMe50Hz (void);
+
+#  ifdef Input_KeyRepeatCount
+#    define Input__init() (*(unsigned char*) (0x5C09) = (Input_KeyRepeatCount))
+#else
+#    define Input__init()
+#  endif
 
 
 #endif
+
