8623c972dddee93bf42822de0ade3f85bd72a91c
Oleg N. Cher <allot@bk.ru>
2014-11-24 05:14:55 +0200

    / ZXDev/Lib/Input: + key repeat feature.

--- ZXDev/Lib/C/Input.c
+++ ZXDev/Lib/C/Input.c
@@ -1,13 +1,12 @@
-/*  Ofront 1.2 -xtspkae */
 #include "SYSTEM.h"
 
 
 export SHORTINT Input_Available (void);
-export CHAR Input_Read (void);
+export CHAR Input_Read_NoKeyRepeat (void);
+export CHAR Input_Read_KeyRepeat (void);
 
 /*================================== Header ==================================*/
-SHORTINT Input_Available (void)
-{
+SHORTINT Input_Available (void) {
 __asm
   LD   IY,#0x5C3A
   CALL 0x28E
@@ -19,19 +18,47 @@ __endasm;
 }
 
 /*--------------------------------- Cut here ---------------------------------*/
-/*
-http://zxpress.ru/book_articles.php?id=1395
-*/
-CHAR Input_Read (void)
-{
+CHAR Input_Read_KeyNoRepeat (void) {
+__asm // http://zxpress.ru/book_articles.php?id=1395
+  LD   IY,#0x5C3A
+  RES  5,1(IY)
+LOOPnoRpt$:
+  CALL 0x2BF
+  BIT  5,1(IY)
+  JR   Z,LOOPnoRpt$
+  LD   L,-50(IY)
+__endasm;
+} //Input_Read_KeyNoRepeat
+
+/*--------------------------------- Cut here ---------------------------------*/
+CHAR Input_Read_KeyRepeat (void) {
 __asm
   LD   IY,#0x5C3A
   RES  5,1(IY)
-LOOP$:
-  CALL 0x2BF
+LOOPrpt$:
   BIT  5,1(IY)
-  JR   Z,LOOP$
+  JR   Z,LOOPrpt$
   LD   L,-50(IY)
 __endasm;
-}
+} //Input_Read_KeyRepeat
+
+/*--------------------------------- Cut here ---------------------------------*/
+CHAR Input_Read_KeyRepeatBuf (void) {
+__asm
+  LD   IY,#0x5C3A
+LOOPrptbuf$:
+  BIT  5,1(IY)
+  JR   Z,LOOPrptbuf$
+  RES  5,1(IY)
+  LD   L,-50(IY)
+__endasm;
+} //Input_Read_KeyRepeatBuf
+
+/*--------------------------------- Cut here ---------------------------------*/
+void Input_RunMe50Hz (void) {
+__asm
+  LD   IY,#0x5C3A
+  RST  0x38
+__endasm;
+} //Input_RunMe50Hz
 
