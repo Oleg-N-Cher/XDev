385a6436bfb43f93e3ed501fdb37c183fe7b0446
Oleg N. Cher <allot@bk.ru>
2014-11-26 18:27:41 +0200

    / ZXDev: + bufferized key checking (IM 2 mode).

--- ZXDev/Lib/C/Input.c
+++ ZXDev/Lib/C/Input.c
@@ -1,12 +1,17 @@
 #include "SYSTEM.h"
 
+#define KeyBufSize 8
+extern char Input_keyBuf [KeyBufSize];
 
-export SHORTINT Input_Available (void);
-export CHAR Input_Read_NoKeyRepeat (void);
-export CHAR Input_Read_KeyRepeat (void);
+export SHORTINT Input_Available_NoRepeat (void);
+export SHORTINT Input_Available_RepeatBuf (void);
+export CHAR Input_Read_NoRepeat (void);
+export CHAR Input_Read_Repeat (void);
+export CHAR Input_Read_RepeatBuf (void);
+export void Input_RunMe50Hz (void);
 
 /*================================== Header ==================================*/
-SHORTINT Input_Available (void) {
+SHORTINT Input_Available_NoRepeat (void) {
 __asm
   LD   IY,#0x5C3A
   CALL 0x28E
@@ -15,10 +20,17 @@ __asm
   RET  Z
   INC  L
 __endasm;
+} //Input_Available_NoRepeat
+
+/*--------------------------------- Cut here ---------------------------------*/
+export SHORTINT Input_Available_RepeatBuf (void) {
+__asm
+  LD   HL,(_Input_keysAvailable+1)
+__endasm;
 }
 
 /*--------------------------------- Cut here ---------------------------------*/
-CHAR Input_Read_KeyNoRepeat (void) {
+CHAR Input_Read_NoRepeat (void) {
 __asm // http://zxpress.ru/book_articles.php?id=1395
   LD   IY,#0x5C3A
   RES  5,1(IY)
@@ -28,10 +40,10 @@ LOOPnoRpt$:
   JR   Z,LOOPnoRpt$
   LD   L,-50(IY)
 __endasm;
-} //Input_Read_KeyNoRepeat
+} //Input_Read_NoRepeat
 
 /*--------------------------------- Cut here ---------------------------------*/
-CHAR Input_Read_KeyRepeat (void) {
+CHAR Input_Read_Repeat (void) {
 __asm
   LD   IY,#0x5C3A
   RES  5,1(IY)
@@ -40,25 +52,65 @@ LOOPrpt$:
   JR   Z,LOOPrpt$
   LD   L,-50(IY)
 __endasm;
-} //Input_Read_KeyRepeat
+} //Input_Read_Repeat
 
 /*--------------------------------- Cut here ---------------------------------*/
-CHAR Input_Read_KeyRepeatBuf (void) {
+char Input_keyBuf [KeyBufSize];
+
+/* Закономерно считаем, что более актуальны клавиши, нажатые позднее,
+   поэтому при переполнении буфера будем удалять из него нажатые ранее.
+   Эта стратегия кажется более удачной, чем игнорирование всех клавиш,
+   нажатых после переполнения буфера (как в контроллере клавиатуры IBM PC). */
+
+CHAR Input_Read_RepeatBuf (void) {
 __asm
   LD   IY,#0x5C3A
 LOOPrptbuf$:
-  BIT  5,1(IY)
+  LD   HL,#_Input_keysAvailable+1
+  LD   A,(HL)
+  OR   A
   JR   Z,LOOPrptbuf$
-  RES  5,1(IY)
-  LD   L,-50(IY)
+  DEC  (HL)
+.globl _Input_keyOut
+_Input_keyOut:           ; Read a key:
+  LD   HL,#_Input_keyBuf ; key := keyBuf[keyOut];
+  LD   C,(HL)
+  INC  HL                ; keyOut := (keyOut+1) MOD KeyBufSize;
+  LD   A,L
+  SUB  #<_Input_keyBuf+KeyBufSize
+  JR   NZ,Save_keyOut$
+  LD   HL,#_Input_keyBuf
+Save_keyOut$:
+  LD   (_Input_keyOut+1),HL
+  LD   L,C               ; RETURN key
 __endasm;
-} //Input_Read_KeyRepeatBuf
+} //Input_Read_RepeatBuf
 
-/*--------------------------------- Cut here ---------------------------------*/
-void Input_RunMe50Hz (void) {
+void Input_RunMe50Hz (void) __naked {
 __asm
   LD   IY,#0x5C3A
   RST  0x38
+  BIT  5,1(IY)
+  RET  Z
+  RES  5,1(IY)
+_Input_keyIn:
+  LD   HL,#_Input_keyBuf ; Add a key:
+  LD   A,-50(IY)         ; keyBuf[keyIn] := CHR(keyCode);
+  LD   (HL),A
+  INC  HL
+  LD   A,L
+  SUB  #<_Input_keyBuf+KeyBufSize
+  JR   NZ,Save_keyIn$
+  LD   HL,#_Input_keyBuf
+Save_keyIn$:
+  LD   (_Input_keyIn+1),HL
+.globl _Input_keysAvailable
+_Input_keysAvailable:
+  LD   A,#0              ; Check overflow:
+  CP   #8                ; IF keysAvailable > 8 THEN keysAvailable := 8 END;
+  ADC  #0
+  LD   (_Input_keysAvailable+1),A
+  RET  C
+  JR   _Input_keyOut
 __endasm;
 } //Input_RunMe50Hz
-
--- ZXDev/Lib/C/Input.h
+++ ZXDev/Lib/C/Input.h
@@ -4,19 +4,29 @@
 #include "XDevCfg.h"
 
 
-extern unsigned char Input_Available (void);
-
-extern CHAR Input_Read_NoKeyRepeat (void);
-extern CHAR Input_Read_KeyRepeat (void);
-extern CHAR Input_Read_KeyRepeatBuf (void);
+extern unsigned char Input_Available_NoRepeat (void);
+extern unsigned char Input_Available_RepeatBuf (void);
 #  ifdef Input_KeyNoRepeat
-#    define Input_Read Input_Read_KeyNoRepeat
+#    define Input_Available Input_Available_NoRepeat
 #  endif
 #  ifdef Input_KeyRepeat
-#    define Input_Read Input_Read_KeyRepeat
+#    define Input_Available Input_Available_NoRepeat
 #  endif
 #  ifdef Input_KeyRepeatBuf
-#    define Input_Read Input_Read_KeyRepeatBuf
+#    define Input_Available Input_Available_RepeatBuf
+#  endif
+
+extern CHAR Input_Read_NoRepeat (void);
+extern CHAR Input_Read_Repeat (void);
+extern CHAR Input_Read_RepeatBuf (void);
+#  ifdef Input_KeyNoRepeat
+#    define Input_Read Input_Read_NoRepeat
+#  endif
+#  ifdef Input_KeyRepeat
+#    define Input_Read Input_Read_Repeat
+#  endif
+#  ifdef Input_KeyRepeatBuf
+#    define Input_Read Input_Read_RepeatBuf
 #  endif
 
 extern void Input_RunMe50Hz (void);
