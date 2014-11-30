0d75bedae534c631d35bb3a791f8623d1c50f65f
Oleg N. Cher <allot@bk.ru>
2014-11-30 10:13:27 +0200

    * ZXDev: - ConsoleCfg; - SYSTEM_Cfg; + XDevCfg.

--- ZXDev/Obj/ASCII/ConsoleCfg.h
+++
@@ -1,8 +0,0 @@
-/* Configuration file of the library Console */
-
-/* Use ROM output (RST 10H, slow, 32x22) or COMPACT/FAST (fast, 32x24) */
-
-//#define OUTPUT_COMPACT
-//#define OUTPUT_FAST
-#define OUTPUT_ROM
-
