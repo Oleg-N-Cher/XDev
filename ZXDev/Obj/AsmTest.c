d7346bdbf9893828e88d1d85a7c92da91e66bdf0
Oleg N. Cher <allot@bk.ru>
2014-12-05 10:35:22 +0200

    * example ZXDev/AsmTest.

--- ZXDev/Obj/AsmTest.c
+++ ZXDev/Obj/AsmTest.c
@@ -12,8 +12,12 @@ export void AsmTest_Border (SHORTINT color);
 
 void AsmTest_Border (SHORTINT color)
 {
-	Asm_Code((CHAR*)"LD   A,4(IX)", (LONGINT)13);
-	Asm_Code((CHAR*)"CALL 0x229B ", (LONGINT)13);
+	Asm_Code((CHAR*)"POP  DE    ", (LONGINT)12);
+	Asm_Code((CHAR*)"POP  HL    ", (LONGINT)12);
+	Asm_Code((CHAR*)"PUSH HL    ", (LONGINT)12);
+	Asm_Code((CHAR*)"PUSH DE    ", (LONGINT)12);
+	Asm_Code((CHAR*)"LD   A,L   ", (LONGINT)12);
+	Asm_Code((CHAR*)"JP   0x229B", (LONGINT)12);
 }
 
 /*----------------------------------------------------------------------------*/
