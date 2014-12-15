ade1f5e3fda4e0b798152a3dac57414c3513544e
Oleg N. Cher <allot@bk.ru>
2014-12-15 05:05:02 +0200

    * ZXDev: rebuild example 'MiraTest'.

--- ZXDev/Obj/MiraTest.c
+++ ZXDev/Obj/MiraTest.c
@@ -6,8 +6,7 @@
 
 
 static SHORTINT MiraTest_c;
-static INTEGER MiraTest_i, MiraTest__for__3;
-static SHORTINT MiraTest__for__2, MiraTest__for__1;
+static INTEGER MiraTest_i;
 
 
 
@@ -26,30 +25,21 @@ export main(int argc, char **argv)
 	GRAPH0_CLS2();
 	GRAPH0_GCLS();
 	MiraTest_i = 0;
-	MiraTest__for__3 = 512;
-	MiraTest__for__3 = (MiraTest__for__3 - MiraTest_i) + 1;
-	do {
+	while (MiraTest_i <= 512) {
 		Basic_INK(Basic_RND(0, 6));
 		Basic_PLOT(Basic_RND(0, 255), Basic_RND(0, 175));
 		MiraTest_i += 1;
-		MiraTest__for__3 -= 1;
-	} while (!(MiraTest__for__3 == 0));
+	}
 	MiraTest_c = 1;
-	MiraTest__for__2 = 23;
-	MiraTest__for__2 = (MiraTest__for__2 - MiraTest_c) + 1;
-	do {
+	while (MiraTest_c <= 23) {
 		GRAPH0_SETATTRS(16 - __DIV(MiraTest_c, 3), MiraTest_c - 3, __ASHL(__DIV(MiraTest_c, 3), 1), 36);
 		MiraTest_c += 1;
-		MiraTest__for__2 -= 1;
-	} while (!(MiraTest__for__2 == 0));
+	}
 	MiraTest_c = 21;
-	MiraTest__for__1 = 23;
-	MiraTest__for__1 = (MiraTest__for__1 - MiraTest_c) + 1;
-	do {
+	while (MiraTest_c <= 23) {
 		GRAPH0_SETATTRS(15, MiraTest_c, 2, 18);
 		MiraTest_c += 1;
-		MiraTest__for__1 -= 1;
-	} while (!(MiraTest__for__1 == 0));
+	}
 	Basic_PAUSE(0);
 	__FINI;
 }
