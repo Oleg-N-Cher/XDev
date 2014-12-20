e142adb96bee60679b42d2acdd5d8ee3817b0d4b
Oleg N. Cher <allot@bk.ru>
2014-12-20 04:42:21 +0200

    + ZXDev: + example 'IM0'.

---
+++ ZXDev/Obj/IM0.bat
@@ -0,0 +1,17 @@
+@SET Mod=IM0
+@SET Bin=..\..\Bin
+@SET Lib=..\..\Lib
+@SET CodeAddr=26000
+@SET DataAddr=60000
+
+@IF EXIST %Mod%.c @MOVE /Y %Mod%.c %Mod%
+@CD %Mod%
+%Bin%\sdcc %Mod%.c -mz80 --code-loc %CodeAddr% --data-loc %DataAddr% --reserve-regs-iy --opt-code-size --funsigned-char --disable-warning 126 -I "." -I %Lib% -I %Lib%\C -I %Lib%\Obj -L %Lib% --disable-warning 59 --disable-warning 85 Basic.lib XDev.lib
+@IF errorlevel 1 PAUSE
+
+@REM Convert Intel hex format to binary
+@REM ==================================
+%Bin%\hex2bin %Mod%.ihx
+%Bin%\bin2data.exe -rem -org %CodeAddr% %Mod%.bin ..\..\%Mod%.tap %Mod%
+@CD ..
+@START ..\%Mod%.tap
