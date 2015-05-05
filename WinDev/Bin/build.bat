81b3b27c9a692501077d522adac572a4587b39bc
Oleg N. Cher <allot@bk.ru>
2015-05-05 20:33:15 +0300

    + WinDev/Bin: + WinSock support.

--- WinDev/Bin/build.bat
+++ WinDev/Bin/build.bat
@@ -7,12 +7,12 @@
 @SET tcc=%Bin%\tcc\tcc
 @IF EXIST %1 GOTO tcccfg
 
-%tcc% %1.c -I "." -I %Lib% -I %Lib%\C -I %Lib%\Obj %Lib%\Oak.a %Lib%\XDev.a %Lib%\Kol.a %Bin%\tcc\lib\SDL.def %Bin%\tcc\lib\SDL_mixer.def -o ..\%1.exe
+%tcc% %1.c -I "." -I %Lib% -I %Lib%\C -I %Lib%\Obj %Lib%\Oak.a %Lib%\Rose.a %Lib%\XDev.a %Lib%\Kol.a %Bin%\tcc\lib\SDL.def %Bin%\tcc\lib\SDL_mixer.def %Bin%\tcc\lib\wsock32.def -o ..\%1.exe
 @IF errorlevel 1 PAUSE
 @GOTO run
 
 :tcccfg
-%tcc% %1.c -I %1 -I %Lib% -I %Lib%\C -I %Lib%\Obj %Lib%\Oak.a %Lib%\XDev.a %Lib%\Kol.a %Bin%\tcc\lib\SDL.def %Bin%\tcc\lib\SDL_mixer.def -o ..\%1.exe
+%tcc% %1.c -I %1 -I %Lib% -I %Lib%\C -I %Lib%\Obj %Lib%\Oak.a %Lib%\Rose.a %Lib%\XDev.a %Lib%\Kol.a %Bin%\tcc\lib\SDL.def %Bin%\tcc\lib\SDL_mixer.def %Bin%\tcc\lib\wsock32.def -o ..\%1.exe
 @IF errorlevel 1 PAUSE
 @GOTO run
 
