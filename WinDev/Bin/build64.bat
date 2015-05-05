a07ae8b0deb0357333737c4736ae00e97bffe0aa
Oleg N. Cher <allot@bk.ru>
2015-05-05 20:33:15 +0300

    + WinDev/Bin: + WinSock support.

--- WinDev/Bin/build64.bat
+++ WinDev/Bin/build64.bat
@@ -4,11 +4,11 @@
 
 @IF EXIST %1 GOTO config
 
-%tcc% %1.c -I "." -I %Lib% -I %Lib%\C -I %Lib%\Obj64 %Lib%\Oak64.a %Lib%\Kol64.a %Lib%\XDev64.a -o ..\x64\%1.exe
+%tcc% %1.c -I "." -I %Lib% -I %Lib%\C -I %Lib%\Obj64 %Lib%\Oak64.a %Lib%\Kol64.a %Lib%\Rose64.a %Lib%\XDev64.a -o ..\x64\%1.exe
 @GOTO exit
 
 :config
-%tcc% %1.c -I %1 -I %Lib% -I %Lib%\C -I %Lib%\Obj64 %Lib%\Oak64.a %Lib%\Kol64.a %Lib%\XDev64.a -o ..\x64\%1.exe
+%tcc% %1.c -I %1 -I %Lib% -I %Lib%\C -I %Lib%\Obj64 %Lib%\Oak64.a %Lib%\Kol64.a %Lib%\Rose64.a %Lib%\XDev64.a -o ..\x64\%1.exe
 
 :exit
 @IF errorlevel 1 PAUSE
