a6fa9fbf227ec4c289eae413baf56f64b9570a0d
Oleg N. Cher <allot@bk.ru>
2015-08-12 18:13:54 +0300

    * WinDev/TestSdl2: rebuild with DJGPP.

--- WinDev/Obj/TestSdl2.bat
+++ WinDev/Obj/TestSdl2.bat
@@ -1,9 +1,23 @@
+@SET Mod=TestSdl2
 @SET Bin=..\Bin
 @SET Lib=..\Lib
+
+@IF EXIST .djgpp GOTO djgpp
+
+:tcc
 @SET tcc=%Bin%\tcc\tcc
 
-%tcc% TestSdl2.c -D_WINGUI -I %Lib% -I %Lib%\C -I %Lib%\Obj %Lib%\XDev.a %Bin%\tcc\lib\SDL2.def -o ..\TestSdl2.exe
+%tcc% %Mod%.c -D_WINGUI -I %Lib% -I %Lib%\C -I %Lib%\Obj %Lib%\XDev.a %Bin%\tcc\lib\SDL2.def -o ..\%Mod%.exe
+@IF errorlevel 1 PAUSE
+@GOTO run
+
+:djgpp
+@SET DJGPP=d:\Archive\Projects\XDev\WinDev\Bin\djgpp\djgpp.env
+@SET PATH=d:\Archive\Projects\XDev\WinDev\Bin\djgpp\bin;%PATH%
+
+gcc.exe %Mod%.c -D_WINGUI -o ..\%Mod%.exe @..\Bin\djgpp.opt ..\Bin\djgpp\lib\libsdl2.a
 @IF errorlevel 1 PAUSE
 
-@CD ..
-@IF EXIST TestSdl2.exe START TestSdl2.exe
+:run
+@IF EXIST %Mod%.exe MOVE %Mod%.exe ..
+@START ..\%Mod%.exe 
