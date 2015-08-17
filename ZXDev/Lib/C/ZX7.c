ee4ef442143a28f52b9143e0573ee9940e6a1157
Oleg N. Cher <allot@bk.ru>
2015-08-17 23:41:30 +0300

    * ZXDev/Lib/ZX7: + SmartRCS, AgileRCS.

--- ZXDev/Lib/C/ZX7.c
+++ ZXDev/Lib/C/ZX7.c
@@ -1,6 +1,8 @@
 void ZX7_Standard (unsigned int src, unsigned int dst);
 void ZX7_Turbo (unsigned int src, unsigned int dst);
 void ZX7_Mega (unsigned int src, unsigned int dst);
+void ZX7_SmartRCS (unsigned int src, unsigned int dst);
+void ZX7_AgileRCS (unsigned int src, unsigned int dst);
 /*================================== Header ==================================*/
 
 void ZX7_Standard (unsigned int src, unsigned int dst) __naked {
@@ -11,6 +13,7 @@ void ZX7_Standard (unsigned int src, unsigned int dst) __naked {
         push de
         push hl
         push bc         ; restore RET address
+
 ; -----------------------------------------------------------------------------
 ; ZX7 decoder by Einar Saukas, Antonio Villena & Metalbrain
 ; "Standard" version (69 bytes only)
@@ -92,6 +95,7 @@ void ZX7_Turbo (unsigned int src, unsigned int dst) __naked {
         push de
         push hl
         push bc         ; restore RET address
+
 ; -----------------------------------------------------------------------------
 ; ZX7 decoder by Einar Saukas & Urusergi
 ; "Turbo" version (88 bytes, 25% faster)
@@ -184,6 +188,7 @@ void ZX7_Mega (unsigned int src, unsigned int dst) __naked {
         push de
         push hl
         push bc         ; restore RET address
+
 ; -----------------------------------------------------------------------------
 ; ZX7 decoder by Einar Saukas
 ; "Mega" version (244 bytes, 30% faster)
@@ -404,3 +409,268 @@ dzx7m_load_bits7:
 ; -----------------------------------------------------------------------------
   __endasm;
 } //ZX7_Mega
+
+/*--------------------------------- Cut here ---------------------------------*/
+void ZX7_SmartRCS (unsigned int src, unsigned int dst) __naked {
+  __asm
+        pop  bc         ; RET address
+        pop  hl         ; HL=src
+        pop  de         ; DE=dst
+        push de
+        push hl
+        push bc         ; restore RET address
+
+; -----------------------------------------------------------------------------
+; "Smart" integrated RCS+ZX7 decoder by Einar Saukas (110 bytes)
+; -----------------------------------------------------------------------------
+; Parameters:
+;   HL: source address (compressed data)
+;   DE: destination address (decompressing)
+; -----------------------------------------------------------------------------
+
+dzx7_smartrcs:
+        ld      a, #0x80
+dzx7r_copy_byte_loop:
+        call    dzx7r_copy_byte         ; copy literal byte
+dzx7r_main_loop:
+        call    dzx7r_next_bit
+        jr      nc, dzx7r_copy_byte_loop ; next bit indicates either literal or sequence
+
+; determine number of bits used for length (Elias gamma coding)
+        push    de
+        ld      bc, #0
+        ld      d, b
+dzx7r_len_size_loop:
+        inc     d
+        call    dzx7r_next_bit
+        jr      nc, dzx7r_len_size_loop
+
+; determine length
+dzx7r_len_value_loop:
+        call    nc, dzx7r_next_bit
+        rl      c
+        rl      b
+        jr      c, dzx7r_exit           ; check end marker
+        dec     d
+        jr      nz, dzx7r_len_value_loop
+        inc     bc                      ; adjust length
+
+; determine offset
+        ld      e, (hl)                 ; load offset flag (1 bit) + offset value (7 bits)
+        inc     hl
+        .db     #0xcb, #0x33            ; opcode for undocumented instruction "SLL E" aka "SLS E"
+        jr      nc, dzx7r_offset_end    ; if offset flag is set, load 4 extra bits
+        ld      d, #0x10                ; bit marker to load 4 bits
+dzx7r_rld_next_bit:
+        call    dzx7r_next_bit
+        rl      d                       ; insert next bit into D
+        jr      nc, dzx7r_rld_next_bit  ; repeat 4 times, until bit marker is out
+        inc     d                       ; add 128 to DE
+        srl	d			; retrieve fourth bit from D
+dzx7r_offset_end:
+        rr      e                       ; insert fourth bit into E
+
+; copy previous sequence
+        ex      (sp), hl                ; store source, restore destination
+        push    hl                      ; store destination
+        sbc     hl, de                  ; HL = destination - offset - 1
+        pop     de                      ; DE = destination
+dzx7r_copy_bytes:
+        push    hl
+        ex      de, hl
+        call    dzx7r_convert
+        ex      de, hl
+        call    dzx7r_copy_byte
+        pop     hl
+        inc     hl
+        jp      pe, dzx7r_copy_bytes
+        pop     hl                      ; restore source address (compressed data)
+        jr      dzx7r_main_loop
+
+dzx7r_next_bit:
+        add     a, a                    ; check next bit
+        ret     nz                      ; no more bits left?
+        ld      a, (hl)                 ; load another group of 8 bits
+        inc     hl
+        rla
+        ret
+
+dzx7r_copy_byte:
+        push    de
+        call    dzx7r_convert
+        ldi
+dzx7r_exit:
+        pop     de
+        inc     de
+        ret
+
+; Convert an RCS address 010RRccc ccrrrppp to screen address 010RRppp rrrccccc
+; (note: replace both EX AF,AF' with PUSH AF/POP AF if you want to preserve AF')
+dzx7r_convert:
+        ex      af, af
+        ld      a, d                    ; A = 010RRccc
+        cp      #0x58
+        jr      nc, dzx7r_skip
+        xor     e
+        and     #0xf8
+        xor     e                       ; A = 010RRppp
+        push    af
+        xor     d
+        xor     e                       ; A = ccrrrccc
+        rlca
+        rlca                            ; A = rrrccccc
+        pop     de                      ; D = 010RRppp
+        ld      e, a                    ; E = rrrccccc
+dzx7r_skip:
+        ex      af, af
+        ret
+
+; -----------------------------------------------------------------------------
+  __endasm;
+} //ZX7_SmartRCS
+
+/*--------------------------------- Cut here ---------------------------------*/
+void ZX7_AgileRCS (unsigned int src, unsigned int dst) __naked {
+  __asm
+        pop  bc         ; RET address
+        pop  hl         ; HL=src
+        pop  de         ; DE=dst
+        push de
+        push hl
+        push bc         ; restore RET address
+
+; -----------------------------------------------------------------------------
+; "Agile" integrated RCS+ZX7 decoder by Einar Saukas (150 bytes)
+; -----------------------------------------------------------------------------
+; Parameters:
+;   HL: source address (compressed data)
+;   DE: destination address (decompressing)
+; -----------------------------------------------------------------------------
+
+dzx7_agilercs:
+        ld      a, #0x80
+dzx7a_copy_byte_loop1:
+        ex      af, af
+        call    dzx7a_copy_byte         ; copy literal byte
+dzx7a_main_loop1:
+        ex      af, af
+        add     a, a                    ; check next bit
+        call    z, dzx7a_load_bits      ; no more bits left?
+        jr      nc, dzx7a_copy_byte_loop1 ; next bit indicates either literal or sequence
+        jr      dzx7a_len_size_start
+
+dzx7a_copy_byte_loop:
+        ldi                             ; copy literal byte
+dzx7a_main_loop:
+        add     a, a                    ; check next bit
+        call    z, dzx7a_load_bits      ; no more bits left?
+        jr      nc, dzx7a_copy_byte_loop ; next bit indicates either literal or sequence
+
+dzx7a_len_size_start:
+; determine number of bits used for length (Elias gamma coding)
+        push    de
+        ld      bc, #1
+        ld      d, b
+dzx7a_len_size_loop:
+        inc     d
+        add     a, a                    ; check next bit
+        call    z, dzx7a_load_bits      ; no more bits left?
+        jr      nc, dzx7a_len_size_loop
+        jp      dzx7a_len_value_start
+
+; determine length
+dzx7a_len_value_loop:
+        add     a, a                    ; check next bit
+        call    z, dzx7a_load_bits      ; no more bits left?
+        rl      c
+        rl      b
+        jr      c, dzx7a_exit           ; check end marker
+dzx7a_len_value_start:
+        dec     d
+        jr      nz, dzx7a_len_value_loop
+        inc     bc                      ; adjust length
+
+; determine offset
+        ld      e, (hl)                 ; load offset flag (1 bit) + offset value (7 bits)
+        inc     hl
+        .db     #0xcb, #0x33            ; opcode for undocumented instruction "SLL E" aka "SLS E"
+        jr      nc, dzx7a_offset_end    ; if offset flag is set, load 4 extra bits
+        add     a, a                    ; check next bit
+        call    z, dzx7a_load_bits      ; no more bits left?
+        rl      d                       ; insert first bit into D
+        add     a, a                    ; check next bit
+        call    z, dzx7a_load_bits      ; no more bits left?
+        rl      d                       ; insert second bit into D
+        add     a, a                    ; check next bit
+        call    z, dzx7a_load_bits      ; no more bits left?
+        rl      d                       ; insert third bit into D
+        add     a, a                    ; check next bit
+        call    z, dzx7a_load_bits      ; no more bits left?
+        ccf
+        jr      c, dzx7a_offset_end
+        inc     d                       ; equivalent to adding 128 to DE
+dzx7a_offset_end:
+        rr      e                       ; insert inverted fourth bit into E
+
+; copy previous sequence
+        ex      (sp), hl                ; store source, restore destination
+        push    hl                      ; store destination
+        sbc     hl, de                  ; HL = destination - offset - 1
+        pop     de                      ; DE = destination
+        ex      af, af
+        ld      a, h                    ; A = 010RRccc
+        cp      #0x58
+        jr      c, dzx7a_copy_bytes
+        ldir
+        pop     hl                      ; restore source address (compressed data)
+        ex      af, af
+        jp      dzx7a_main_loop
+
+dzx7a_copy_bytes:
+        push    hl
+        ex      de, hl
+        call    dzx7a_convert
+        ex      de, hl
+        call    dzx7a_copy_byte
+        pop     hl
+        inc     hl
+        jp      pe, dzx7a_copy_bytes
+        pop     hl                      ; restore source address (compressed data)
+        jr      dzx7a_main_loop1
+
+dzx7a_load_bits:
+        ld      a, (hl)                 ; load another group of 8 bits
+        inc     hl
+        rla
+        ret
+
+dzx7a_copy_byte:
+        push    de
+        call    dzx7a_convert
+        ldi
+dzx7a_exit:
+        pop     de
+        inc     de
+        ret
+
+; Convert an RCS address 010RRccc ccrrrppp to screen address 010RRppp rrrccccc
+; (Note: replace both EX AF,AF' with PUSH AF/POP AF if you want to preserve AF')
+dzx7a_convert:
+        ld      a, d                    ; A = 010RRccc
+        cp      #0x58
+        ret     nc
+        xor     e
+        and     #0xf8
+        xor     e                       ; A = 010RRppp
+        push    af
+        xor     d
+        xor     e                       ; A = ccrrrccc
+        rlca
+        rlca                            ; A = rrrccccc
+        pop     de                      ; D = 010RRppp
+        ld      e, a                    ; E = rrrccccc
+        ret
+
+; -----------------------------------------------------------------------------
+  __endasm;
+} //ZX7_AgileRCS
--- ZXDev/Lib/C/ZX7.h
+++ ZXDev/Lib/C/ZX7.h
@@ -5,6 +5,8 @@
 extern void ZX7_Standard (unsigned int src, unsigned int dst);
 extern void ZX7_Turbo (unsigned int src, unsigned int dst);
 extern void ZX7_Mega (unsigned int src, unsigned int dst);
+extern void ZX7_SmartRCS (unsigned int src, unsigned int dst);
+extern void ZX7_AgileRCS (unsigned int src, unsigned int dst);
 
 #define ZX7__init()
 
