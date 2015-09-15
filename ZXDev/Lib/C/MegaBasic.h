a1f1bf41e8f38d2de108da8219ddbd9284d79c51
Oleg N. Cher <allot@bk.ru>
2015-09-15 03:05:13 +0300

    * ZXDev/Lib/Mega => MegaBasic

---
+++ ZXDev/Lib/C/MegaBasic.h
@@ -0,0 +1,14 @@
+#ifndef MegaBasic__h
+#define MegaBasic__h
+
+#include "SYSTEM.h"
+
+
+import void MegaBasic_INVERT (void);
+import void MegaBasic_PLAY (BOOLEAN soundType,
+  SHORTCARD stepLen, SHORTCARD startFreq, CARDINAL stepNum, SHORTCARD incFreq);
+
+#define MegaBasic__init()
+
+
+#endif
