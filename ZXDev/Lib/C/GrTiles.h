38dd0b64879017e98d577955429e7dea15ae97a9
Oleg N. Cher <allot@bk.ru>
2015-09-21 22:49:51 +0300

    * ZXDev/Lib/GrTiles: optimize & clean.

--- ZXDev/Lib/C/GrTiles.h
+++ ZXDev/Lib/C/GrTiles.h
@@ -1,18 +1,16 @@
-/*  Ofront 1.2 -xtspkae */
-
 #ifndef GrTiles__h
 #define GrTiles__h
 
-#include "SYSTEM.h"
 
-
-import void GrTiles_DrawTile8x8 (SHORTCARD x, SHORTCARD y, CARDINAL tile);
-import void GrTiles_DrawMonoTile8x8 (SHORTCARD x, SHORTCARD y, CARDINAL tile, SHORTINT colors);
-import void GrTiles_DrawTranspMonoTile8x8 (SHORTCARD x, SHORTCARD y, CARDINAL tile, SHORTINT colors);
-#define GrTiles__init()
+extern void GrTiles_DrawTile8x8 (unsigned char x, unsigned char y, unsigned int tile);
+extern void GrTiles_DrawMonoTile8x8 (unsigned char x, unsigned char y, unsigned int tile, unsigned char colors);
+extern void GrTiles_DrawTranspMonoTile8x8 (unsigned char x, unsigned char y, unsigned int tile, unsigned char colors);
 
 #define GrTiles_DrawTile GrTiles_DrawTile8x8
 #define GrTiles_DrawMonoTile GrTiles_DrawMonoTile8x8
 #define GrTiles_DrawTranspMonoTile GrTiles_DrawMonoTile8x8
+
+#define GrTiles__init()
+
 
 #endif
