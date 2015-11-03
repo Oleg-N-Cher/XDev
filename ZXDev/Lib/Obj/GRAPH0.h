fb1a9a54b4ae74e5f631df79353093de1f39fe57
Oleg N. Cher <allot@bk.ru>
2015-11-03 11:01:03 +0200

    * ZXDev: - SYSTEM unsigned types.

--- ZXDev/Lib/Obj/GRAPH0.h
+++ ZXDev/Lib/Obj/GRAPH0.h
@@ -12,7 +12,7 @@ import void GRAPH0_CLS2 (void);
 import void GRAPH0_GCLS (void);
 import void GRAPH0_SCRDOT (void);
 import void GRAPH0_SCRNAD (void);
-import void GRAPH0_SETATTRS (CARDINAL X, CARDINAL Y, SHORTCARD L, SHORTCARD C);
+import void GRAPH0_SETATTRS (INTEGER X, INTEGER Y, SHORTINT L, SHORTINT C);
 import void *GRAPH0__init(void);
 
 #define GRAPH0__init()	/*noinit*/
