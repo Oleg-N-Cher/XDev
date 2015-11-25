7df66d09e05df8358323c65588ea3cac786f6e8e
Oleg N. Cher <allot@bk.ru>
2015-11-25 13:26:58 +0200

    * WinDev/Lib/Bin

--- WinDev/Lib/Bin/compile.bat
+++ WinDev/Lib/Bin/compile.bat
@@ -1,19 +1,19 @@
 @IF EXIST ..\C\%1.c GOTO clib
 
 :olib
-..\..\Bin\tcc\tcc -c %1.c -I "." -I ..\C
+..\..\Bin\tcc\tcc -c %1.c -I ".." -I ..\C
 @IF errorlevel 1 GOTO noinit
 @GOTO exit
 
 :noinit
 ..\..\..\Bin\smartlib %1.c -noinit -nocut
-..\..\Bin\tcc\tcc -c %1.c -I "." -I ..\C
+..\..\Bin\tcc\tcc -c %1.c -I ".." -I ..\C
 @GOTO exit
 
 :clib
 @IF EXIST %1.h DEL %1.h
 @IF EXIST %1.c DEL %1.c
-..\..\Bin\tcc\tcc -c ..\C\%1.c -I "." -I ..\C
+..\..\Bin\tcc\tcc -c ..\C\%1.c -I ".." -I ..\C
 
 :exit
 @IF errorlevel 1 PAUSE
