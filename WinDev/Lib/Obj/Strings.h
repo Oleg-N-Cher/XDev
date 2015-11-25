d443d938f3b7f8b81918c978959261e98c9df798
Oleg N. Cher <allot@bk.ru>
2015-11-25 11:36:37 +0200

    * WinDev/Lib/Strings

--- WinDev/Lib/Obj/Strings.h
+++ WinDev/Lib/Obj/Strings.h
@@ -8,13 +8,14 @@
 
 
 
-import BOOLEAN Strings_CP1251ToUtf8 (CHAR *s, LONGINT s__len, CHAR *res, LONGINT res__len);
 import INTEGER Strings_Find (CHAR *str, LONGINT str__len, CHAR *pattern, LONGINT pattern__len, INTEGER start);
 import void Strings_IntToStr (INTEGER n, CHAR *str, LONGINT str__len);
 import void Strings_IntToStrForm (INTEGER x, INTEGER form, INTEGER minWidth, CHAR fillCh, BOOLEAN showBase, CHAR *s, LONGINT s__len);
 import void Strings_LIntToStr (LONGINT n, CHAR *str, LONGINT str__len);
 import void Strings_LIntToStrForm (LONGINT x, INTEGER form, INTEGER minWidth, CHAR fillCh, BOOLEAN showBase, CHAR *s, LONGINT s__len);
 import INTEGER Strings_Length (CHAR *str, LONGINT str__len);
+import BOOLEAN Strings_Utf8ToWin1251 (CHAR *s, LONGINT s__len, CHAR *res, LONGINT res__len);
+import BOOLEAN Strings_Win1251ToUtf8 (CHAR *s, LONGINT s__len, CHAR *res, LONGINT res__len);
 import void *Strings__init(void);
 
 #define Strings__init()	/*-noinit*/
