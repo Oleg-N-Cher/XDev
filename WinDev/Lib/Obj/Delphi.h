bc387796c3e4309da66b49831a353655f2d3208f
Oleg N. Cher <allot@bk.ru>
2015-12-10 00:31:04 +0200

    + WinDev/Lib/Delphi.

---
+++ WinDev/Lib/Obj/Delphi.h
@@ -0,0 +1,17 @@
+/*  Ofront 1.2 -xtspkae */
+
+#ifndef Delphi__h
+#define Delphi__h
+
+#include "SYSTEM.h"
+
+
+
+
+import INTEGER Delphi_HiWord (INTEGER i);
+import INTEGER Delphi_LoWord (INTEGER i);
+import void *Delphi__init(void);
+
+#define Delphi__init()	/*-noinit*/
+
+#endif
