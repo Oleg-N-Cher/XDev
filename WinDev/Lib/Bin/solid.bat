ec41103b9d37fa538d28a4517203fc13243370b3
Oleg N. Cher <allot@bk.ru>
2015-12-10 06:53:14 +0200

    * WinDev/Lib/Bin (ready for MinGW).

--- WinDev/Lib/Bin/solid.bat
+++ WinDev/Lib/Bin/solid.bat
@@ -2,22 +2,22 @@
 @REM   LibName ModName
 
 @SET RootBin=..\..\..\Bin
-@SET Bin=..\..\Bin
-@SET tcc=%Bin%\tcc\tcc.exe
+@SET PATH=..\..\Bin\MinGW\bin;%PATH%
+@SET gcc=gcc.exe -ffunction-sections
 
 @IF EXIST ..\C\%2.c GOTO clib
 
 :olib
-%tcc% -c %2.c -I ".." -I ..\C
+%gcc% -c %2.c -I ".." -I ..\C
 @GOTO done
 
 :clib
 @IF EXIST %2.h DEL %2.h
 @IF EXIST %2.c DEL %2.c
-%tcc% -c ..\C\%2.c -I ".." -I ..\C
+%gcc% -c ..\C\%2.c -I ".." -I ..\C
 
 :done
 @IF errorlevel 1 PAUSE
 
-@%Bin%\ar -rc %1 %2.o
+@ar.exe -rc %1 %2.o
 @DEL %2.o
