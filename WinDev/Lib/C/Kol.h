efd75908b4737723e50343ae5a4f5d9c4d70cad8
Oleg N. Cher <allot@bk.ru>
2015-12-24 20:19:06 +0200

    * WinDev/Lib/Kol.

---
+++ WinDev/Lib/C/Kol.h
@@ -0,0 +1,15 @@
+#ifndef Kol__h
+#define Kol__h
+
+#include "SYSTEM.h"
+
+/* DEFINITION KolFiles */
+#include "KolFiles.h"
+
+#define Kol_ExtractFileName KolFiles_ExtractFileName
+#define Kol_ExtractFilePath KolFiles_ExtractFilePath
+
+
+#define Kol__init()
+
+#endif
