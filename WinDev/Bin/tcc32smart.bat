a54534cb0bcf7fc20423280dd87cd3213d069073
Oleg N. Cher <allot@bk.ru>
2016-04-20 18:46:00 +0300

    - WinDev/Bin/tcc & tcc64.

--- WinDev/Bin/tcc32smart.bat
+++
@@ -1,7 +0,0 @@
-@SET bin=..\Bin
-@SET tcc=%bin%\tcc32\tcc.exe
-%tcc% -c %1.c
-@IF ERRORLEVEL 1 @PAUSE
-@%bin%\smartlib %1.c
-@FOR %%i IN (%1_0??.c) DO %tcc% -c %%i
-@FOR %%i IN (%1_0??.o) DO %bin%\ar -rc Ofront32.a %%i
--- WinDev/Bin/tccsmart.bat
+++
@@ -1,9 +0,0 @@
-@SET bin=..\Bin
-@SET tcc=%bin%\tcc\tcc.exe
-@SET smartlib=..\%bin%\smartlib
-
-%tcc% -c %1.c -I "." -I Obj
-@IF ERRORLEVEL 1 @PAUSE
-@%smartlib% %1.c
-@FOR %%i IN (%1_0??.c) DO %tcc% -c %%i -I "." -I Obj
-@FOR %%i IN (%1_0??.o) DO %bin%\ar -rc WinDev.a %%i
