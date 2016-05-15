44248a77389dd616c64e46983d2f8fd8b9c3d328
Oleg N. Cher <allot@bk.ru>
2016-05-16 01:10:29 +0300

    * WinDev/Lib/Bin/*.bat: + "-fdata-sections".

--- WinDev/Lib/Bin/build.bat
+++ WinDev/Lib/Bin/build.bat
@@ -1,6 +1,6 @@
 @SET RootBin=..\..\..\Bin
 @SET PATH=..\..\Bin\MinGW\bin
-@SET gcc=gcc.exe -Os -fno-exceptions -fno-asynchronous-unwind-tables -ffunction-sections
+@SET gcc=gcc.exe -Os -fno-exceptions -fno-asynchronous-unwind-tables -ffunction-sections -fdata-sections
 
 @IF EXIST ..\%1.a DEL ..\%1.a
 %RootBin%\smartlib %1.c
--- WinDev/Lib/Bin/solid.bat
+++ WinDev/Lib/Bin/solid.bat
@@ -3,7 +3,7 @@
 
 @SET RootBin=..\..\..\Bin
 @SET PATH=..\..\Bin\MinGW\bin
-@SET gcc=gcc.exe -Os -fno-exceptions -fno-asynchronous-unwind-tables -ffunction-sections
+@SET gcc=gcc.exe -Os -fno-exceptions -fno-asynchronous-unwind-tables -ffunction-sections -fdata-sections
 
 @IF EXIST ..\C\%2.c GOTO clib
 
