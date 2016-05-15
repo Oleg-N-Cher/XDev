7f411ec5e94a808d3c0c112842b7d4ba526a202c
Oleg N. Cher <allot@bk.ru>
2016-05-16 01:11:20 +0300

    * WinDev/Lib/Bin: * build scripts.

--- WinDev/Lib/Bin/buildEco64.bat
+++ WinDev/Lib/Bin/buildEco64.bat
@@ -5,6 +5,7 @@
 @SET Lib=..\Eco64.a
 @IF EXIST %Lib% DEL %Lib%
 @CALL ..\Bin\smart64 %Lib% EcoBigSets
+@CALL ..\Bin\solid64 %Lib% EcoBuffer
 @CALL ..\Bin\smart64 %Lib% EcoListen
 @CALL ..\Bin\smart64 %Lib% EcoLists
 @CALL ..\Bin\solid64 %Lib% EcoVector
