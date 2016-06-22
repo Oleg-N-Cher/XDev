31a9624344f3daea4c8d279f5059b7f1cbe47192
Oleg N. Cher <allot@bk.ru>
2016-06-22 23:35:14 +0300

    + WinDev/Lib/KolFiles.SameFile

--- WinDev/Lib/Obj64/KolFiles.h
+++ WinDev/Lib/Obj64/KolFiles.h
@@ -13,6 +13,7 @@ import BOOLEAN KolFiles_ExtractFileName (CHAR *path, LONGINT path__len, CHAR *na
 import BOOLEAN KolFiles_ExtractFilePath (CHAR *path, LONGINT path__len, CHAR *res, LONGINT res__len);
 import BOOLEAN KolFiles_FileClose (SYSTEM_PTR handle);
 import SYSTEM_PTR KolFiles_FileCreate (CHAR *fileName, LONGINT fileName__len, SET openFlags);
+import BOOLEAN KolFiles_SameFile (CHAR *fileName1, LONGINT fileName1__len, CHAR *fileName2, LONGINT fileName2__len);
 import void *KolFiles__init(void);
 
 #define KolFiles__init()	/*-noinit*/
