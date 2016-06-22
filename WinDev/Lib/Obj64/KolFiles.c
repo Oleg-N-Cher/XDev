dc6bf39a9612d1450e8692ab86e0d2c991029368
Oleg N. Cher <allot@bk.ru>
2016-06-22 23:35:14 +0300

    + WinDev/Lib/KolFiles.SameFile

--- WinDev/Lib/Obj64/KolFiles.c
+++ WinDev/Lib/Obj64/KolFiles.c
@@ -12,6 +12,7 @@ export BOOLEAN KolFiles_ExtractFileName (CHAR *path, LONGINT path__len, CHAR *na
 export BOOLEAN KolFiles_ExtractFilePath (CHAR *path, LONGINT path__len, CHAR *res, LONGINT res__len);
 export BOOLEAN KolFiles_FileClose (SYSTEM_PTR handle);
 export SYSTEM_PTR KolFiles_FileCreate (CHAR *fileName, LONGINT fileName__len, SET openFlags);
+export BOOLEAN KolFiles_SameFile (CHAR *fileName1, LONGINT fileName1__len, CHAR *fileName2, LONGINT fileName2__len);
 
 #define KolFiles__init()	/*-noinit*/
 
@@ -54,6 +55,26 @@ BOOLEAN KolFiles_ExtractFileName (CHAR *path, LONGINT path__len, CHAR *name, LON
 		return KolStrings_SubStr((void*)path, path__len, p + 1, (int)path__len, (void*)name, name__len);
 	}
 	return KolStrings_SubStr((void*)path, path__len, 0, p, (void*)name, name__len);
+}
+
+/*----------------------------------------------------------------------------*/
+BOOLEAN KolFiles_SameFile (CHAR *fileName1, LONGINT fileName1__len, CHAR *fileName2, LONGINT fileName2__len)
+{
+	WinApi_BY_HANDLE_FILE_INFORMATION fileInfo1, fileInfo2;
+	SYSTEM_PTR handle1 = NIL, handle2 = NIL;
+	BOOLEAN same;
+	INTEGER res;
+	same = 0;
+	handle1 = WinApi_CreateFileA((SYSTEM_PTR)((LONGINT)fileName1), 0x80000000, 0x01, NIL, NIL, 3, 0x0, NIL);
+	if (handle1 != (SYSTEM_PTR)-1) {
+		handle2 = WinApi_CreateFileA((SYSTEM_PTR)((LONGINT)fileName2), 0x80000000, 0x01, NIL, NIL, 3, 0x0, NIL);
+		if (handle2 != (SYSTEM_PTR)-1) {
+			same = (((WinApi_GetFileInformationByHandle(handle1, &fileInfo1, WinApi_BY_HANDLE_FILE_INFORMATION__typ) != 0 && WinApi_GetFileInformationByHandle(handle2, &fileInfo2, WinApi_BY_HANDLE_FILE_INFORMATION__typ) != 0) && fileInfo1.nFileIndexLow == fileInfo2.nFileIndexLow) && fileInfo1.nFileIndexHigh == fileInfo2.nFileIndexHigh) && fileInfo1.dwVolumeSerialNumber == fileInfo2.dwVolumeSerialNumber;
+			res = WinApi_CloseHandle(handle2);
+		}
+		res = WinApi_CloseHandle(handle1);
+	}
+	return same;
 }
 
 /*----------------------------------------------------------------------------*/
