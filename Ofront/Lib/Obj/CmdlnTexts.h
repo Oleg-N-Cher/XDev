6967c5799cd88f6434f615b872844fe9433269d6
Oleg N. Cher <allot@bk.ru>
2016-06-24 02:37:58 +0300

    * Ofront/Lib: rebuild.

--- Ofront/Lib/Obj/CmdlnTexts.h
+++ Ofront/Lib/Obj/CmdlnTexts.h
@@ -30,8 +30,8 @@ typedef
 
 typedef
 	struct CmdlnTexts_RunDesc {
-		LONGINT _prvt0;
-		char _prvt1[15];
+		INTEGER _prvt0;
+		char _prvt1[17];
 	} CmdlnTexts_RunDesc;
 
 typedef
@@ -39,7 +39,7 @@ typedef
 
 typedef
 	struct CmdlnTexts_ElemDesc {
-		char _prvt0[20];
+		char _prvt0[24];
 		LONGINT W, H;
 		CmdlnTexts_Handler handle;
 		char _prvt1[4];
@@ -106,7 +106,7 @@ typedef
 		CmdlnTexts_Buffer buf;
 		CmdlnTexts_FontsFont fnt;
 		SHORTINT col, voff;
-		char _prvt0[26];
+		char _prvt0[24];
 	} CmdlnTexts_Writer;
 
 
