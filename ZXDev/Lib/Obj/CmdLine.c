bc4bb3aeaaa3c05a724449f18790024a1b2bf5a6
Oleg N. Cher <allot@bk.ru>
2016-07-29 00:49:47 +0300

    * ZXDev: rebuild.

--- ZXDev/Lib/Obj/CmdLine.c
+++ ZXDev/Lib/Obj/CmdLine.c
@@ -18,3 +18,12 @@ void CmdLine_GetParam (INTEGER n, CHAR *param, LONGINT param__len)
 	param[0] = 0x00;
 }
 
+/*----------------------------------------------------------------------------*/
+
+export void *CmdLine__init(void)
+{
+	__DEFMOD;
+	__REGMOD("CmdLine", 0);
+/* BEGIN */
+	__ENDMOD;
+}
