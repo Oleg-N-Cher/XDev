8c230535229c92c8efbc9e1401183ea2264abba0
Oleg N. Cher <allot@bk.ru>
2016-08-30 06:42:03 +0300

    * ZXDev: improved build system.

--- ZXDev/Bin/compile.bat
+++ ZXDev/Bin/compile.bat
@@ -1,14 +1,34 @@
-@SET sdcc=..\Bin\sdcc -mz80 --no-std-crt0 -I ..\Lib\C -I ..\Lib\Obj --disable-warning 59 --disable-warning 85 --disable-warning 126
+@SET ZXDev=%XDev%\ZXDev
+@IF "%XDev%"=="" SET ZXDev=..
 
-@IF EXIST %1 GOTO config
+@IF "%MainMod%"=="" SET MainMod=%1
 
-%sdcc% -c %1.c -I ..\Lib
-@GOTO exit
+@SET Options=%Options% -mz80 --no-xinit-opt --opt-code-size --disable-warning 59 --disable-warning 85 --disable-warning 126
+@SET Include=%Include% -I %ZXDev%\Lib\C -I %ZXDev%\Lib\Obj
+@IF "%Clean%"=="" SET Clean=true
 
-:config
+@IF EXIST %1.sym MOVE /Y %1.sym %1.osm
 
-%sdcc% -c %1.c -I %1
+@SET SDCC=%ZXDev%\Bin\sdcc.exe %Options% %Include%
 
-:exit
+@IF EXIST %1 GOTO Config
+
+%SDCC% -c %1.c -I %ZXDev%\Lib
+@GOTO Exit
+
+:Config
+
+%SDCC% -c %1.c -I %1
+
+:Exit
 
 @IF errorlevel 1 PAUSE
+
+@IF NOT "%Clean%"=="true" GOTO Done
+@DEL *.asm *.lst %1.sym
+@IF "%1"=="%MainMod%" DEL %1.h %1.rel %1.osm
+@IF NOT "%1"=="%MainMod%" DEL %1.c
+
+:Done
+
+@IF EXIST %1.osm MOVE /Y %1.osm %1.sym
--- ZXDev/Obj/Circl.bat
+++ ZXDev/Obj/Circl.bat
@@ -1,21 +1,3 @@
-@SET Name=Circl
-@SET CodeAddr=38000
-@SET DataAddr=45000
-@SET bin=..\Bin
-@SET lib=..\Lib
+@SET Target=rem
 
-%bin%\sdcc %Name%.c -mz80 --code-loc %CodeAddr% --data-loc %DataAddr% --opt-code-size -I %lib% -I %lib%\C -I %lib%\Obj -L %lib% --disable-warning 59 --disable-warning 85 --disable-warning 126 Graph.lib Basic.lib
-@IF errorlevel 1 PAUSE
-
-@REM Convert Intel hex format to binary
-@REM ==================================
-%bin%\hex2bin %Name%.ihx
-
-@REM %bin%\stripbin %Name%.bin
-
-@REM Link the target to TAP format
-@REM =============================
-@DEL /Q %Name%.tap
-%bin%\bin2data.exe -rem -org %CodeAddr% %Name%.bin ..\%Name%.tap %Name%
-
-START ..\%Name%.tap
+@..\Bin\build Circl
