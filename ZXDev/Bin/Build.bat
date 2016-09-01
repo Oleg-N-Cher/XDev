849590f95e3addf24a1d416875c8d59571f9b6f2
Oleg N. Cher <allot@bk.ru>
2016-09-01 04:43:54 +0300

    * ZXDev/Bin: * build scripts.

--- ZXDev/Bin/Build.bat
+++ ZXDev/Bin/Build.bat
@@ -1,32 +1,37 @@
-@SET ZXDev=%XDev%\ZXDev
-@IF "%XDev%"=="" SET ZXDev=..
+@ECHO OFF
+SET ZXDev=%XDev%\ZXDev
+IF "%XDev%"=="" SET ZXDev=..
 
-@IF "%MainMod%"=="" SET MainMod=%1
-@IF "%MainMod%"=="%1" GOTO Build
+IF "%MainMod%"=="" SET MainMod=%1
+IF "%MainMod%"=="%1" GOTO Build
 
 :Compile
 
-@IF EXIST %1.sym MOVE /Y %1.sym %1.osm
-@CALL %ZXDev%\Bin\compile.bat %1
-@IF EXIST %1.osm MOVE /Y %1.osm %1.sym
+IF EXIST %1.sym MOVE /Y %1.sym %1.osm
+SET SaveOptions=%Options%
+SET SaveInclude=%Include%
+CALL %ZXDev%\Bin\compile.bat %1
+SET Options=%SaveOptions%
+SET Include=%SaveInclude%
+IF EXIST %1.osm MOVE /Y %1.osm %1.sym
 
 :Build
 
-@IF "%CodeAdr%"=="" SET CodeAdr=45056
-@IF "%DataAdr%"=="" SET DataAdr=63488
-@SET Options=%Options% -mz80 --no-xinit-opt --opt-code-size --code-loc %CodeAdr% --data-loc %DataAdr% --disable-warning 59 --disable-warning 85 --disable-warning 126
-@SET Include=%Include% -I %ZXDev%\Lib\C -I %ZXDev%\Lib\Obj
-@SET Libraries=%Libraries% -L %ZXDev%\Lib XDev.lib Graph.lib Basic.lib Laser.lib MegaBasic.lib Best40.lib trdos.lib libspr.lib NewSupercode.lib MiraMod2.lib PT3x0A.lib Wham.lib ZX7.lib
-@IF "%Target%"=="" SET Target=tap
-@IF "%Clean%"=="" SET Clean=TRUE
-@IF "%Start%"=="" SET Start=TRUE
+IF "%CodeAdr%"=="" SET CodeAdr=45056
+IF "%DataAdr%"=="" SET DataAdr=63488
+SET Options=%Options% -mz80 --no-xinit-opt --opt-code-size --code-loc %CodeAdr% --data-loc %DataAdr% --disable-warning 59 --disable-warning 85 --disable-warning 126
+SET Include=%Include% -I %ZXDev%\Lib\C -I %ZXDev%\Lib\Obj
+SET Libraries=%Libraries% -L %ZXDev%\Lib XDev.lib Graph.lib Basic.lib Laser.lib MegaBasic.lib Best40.lib trdos.lib libspr.lib NewSupercode.lib MiraMod2.lib PT3x0A.lib Wham.lib ZX7.lib
+IF "%Target%"=="" SET Target=TAP
+IF "%Clean%"=="" SET Clean=TRUE
+IF "%Start%"=="" SET Start=TRUE
 
-@SET SDCC=%ZXDev%\Bin\sdcc.exe %Options% %Include% %Modules% %Libraries%
+SET SDCC=%ZXDev%\Bin\sdcc.exe %Options% %Include% %Modules% %Libraries%
 
-@IF EXIST %MainMod% GOTO Config
+IF EXIST %MainMod% GOTO Config
 
 %SDCC% %MainMod%.c -I %ZXDev%\Lib
-@GOTO Link
+GOTO Link
 
 :Config
 
@@ -34,16 +39,16 @@
 
 :Link
 
-@IF errorlevel 1 PAUSE
+IF errorlevel 1 PAUSE
 
 %ZXDev%\Bin\hex2bin.exe %MainMod%.ihx
-@IF "%Target%"=="rem" %ZXDev%\Bin\bin2data.exe -rem -org %CodeAdr% %MainMod%.bin ..\%MainMod%.tap %MainMod%
-@IF "%Target%"=="tap" %ZXDev%\Bin\bin2tap.exe -c 24999 -a %CodeAdr% -r %CodeAdr% -b -o ..\%MainMod%.tap %MainMod%.bin
+IF "%Target%"=="REM" %ZXDev%\Bin\bin2data.exe -rem -org %CodeAdr% %MainMod%.bin ..\%MainMod%.tap %MainMod%
+IF "%Target%"=="TAP" %ZXDev%\Bin\bin2tap.exe -c 24999 -a %CodeAdr% -r %CodeAdr% -b -o ..\%MainMod%.tap %MainMod%.bin
 
-@IF NOT "%Clean%"=="TRUE" GOTO Done
-@DEL *.asm *.bin *.ihx *.lk *.lst *.map *.noi %MainMod%.h %MainMod%.sym %MainMod%.rel
-@IF "%Modules%"=="" DEL %MainMod%.c
+IF NOT "%Clean%"=="TRUE" GOTO Done
+DEL *.asm *.bin *.ihx *.lk *.lst *.map *.noi %MainMod%.h %MainMod%.sym %MainMod%.rel
+IF "%Modules%"=="" DEL %MainMod%.c
 
 :Done
 
-@IF "%Start%"=="TRUE" START ..\%MainMod%.tap
+IF "%Start%"=="TRUE" START ..\%MainMod%.tap
--- ZXDev/Bin/Compile.bat
+++ ZXDev/Bin/Compile.bat
@@ -1,20 +1,21 @@
-@SET ZXDev=%XDev%\ZXDev
-@IF "%XDev%"=="" SET ZXDev=..
+@ECHO OFF
+SET ZXDev=%XDev%\ZXDev
+IF "%XDev%"=="" SET ZXDev=..
 
-@IF "%MainMod%"=="" SET MainMod=%1
+IF "%MainMod%"=="" SET MainMod=%1
 
-@SET Options=%Options% -mz80 --no-xinit-opt --opt-code-size --disable-warning 59 --disable-warning 85 --disable-warning 126
-@SET Include=%Include% -I %ZXDev%\Lib\C -I %ZXDev%\Lib\Obj
-@IF "%Clean%"=="" SET Clean=TRUE
+SET Options=%Options% -mz80 --no-xinit-opt --opt-code-size --disable-warning 59 --disable-warning 85 --disable-warning 126
+SET Include=%Include% -I %ZXDev%\Lib\C -I %ZXDev%\Lib\Obj
+IF "%Clean%"=="" SET Clean=TRUE
 
-@IF EXIST %1.sym MOVE /Y %1.sym %1.osm
+IF EXIST %1.sym MOVE /Y %1.sym %1.osm
 
-@SET SDCC=%ZXDev%\Bin\sdcc.exe %Options% %Include%
+SET SDCC=%ZXDev%\Bin\sdcc.exe %Options% %Include%
 
-@IF EXIST %1 GOTO Config
+IF EXIST %1 GOTO Config
 
 %SDCC% -c %1.c -I %ZXDev%\Lib
-@GOTO Exit
+GOTO Exit
 
 :Config
 
@@ -22,13 +23,13 @@
 
 :Exit
 
-@IF errorlevel 1 PAUSE
+IF errorlevel 1 PAUSE
 
-@IF NOT "%Clean%"=="TRUE" GOTO Done
-@DEL *.asm *.lst %1.sym
-@IF "%1"=="%MainMod%" DEL %1.h %1.rel %1.osm
-@IF NOT "%1"=="%MainMod%" DEL %1.c
+IF NOT "%Clean%"=="TRUE" GOTO Done
+DEL *.asm *.lst %1.sym
+IF "%1"=="%MainMod%" DEL %1.h %1.rel %1.osm
+IF NOT "%1"=="%MainMod%" DEL %1.c
 
 :Done
 
-@IF EXIST %1.osm MOVE /Y %1.osm %1.sym
+IF EXIST %1.osm MOVE /Y %1.osm %1.sym
