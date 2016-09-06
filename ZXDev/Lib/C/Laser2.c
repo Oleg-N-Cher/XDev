4d90d45bb7eaa2855e2531b46762a97f48606d81
Oleg N. Cher <allot@bk.ru>
2016-09-06 17:53:01 +0300

    * ZXDev/Lib/Laser2; + example Laser2Test.

--- ZXDev/Lib/C/Laser2.c
+++ ZXDev/Lib/C/Laser2.c
@@ -8,10 +8,17 @@ extern unsigned int Laser2_SPRT_ADR;  // Sprite file start address
 
 // Functions for screen windows processing
 
-void Laser2_PTBL (signed char col, signed char row, unsigned char spn);
-void Laser2_PTOR (signed char col, signed char row, unsigned char spn);
-void Laser2_PTND (signed char col, signed char row, unsigned char spn);
-void Laser2_PTXR (signed char col, signed char row, unsigned char spn);
+void Laser2_ATOF_INSCR (void);
+void Laser2_ATON_INSCR (void);
+void Laser2_PTBL_INSCR (signed char col, signed char row, unsigned char spn);
+void Laser2_PTOR_INSCR (signed char col, signed char row, unsigned char spn);
+void Laser2_PTND_INSCR (signed char col, signed char row, unsigned char spn);
+void Laser2_PTXR_INSCR (signed char col, signed char row, unsigned char spn);
+
+void Laser2_PTBL_OUTSCR (signed char col, signed char row, unsigned char spn);
+void Laser2_PTOR_OUTSCR (signed char col, signed char row, unsigned char spn);
+void Laser2_PTND_OUTSCR (signed char col, signed char row, unsigned char spn);
+void Laser2_PTXR_OUTSCR (signed char col, signed char row, unsigned char spn);
 
 /* Спрайты хранятся в памяти в следующем формате:
 Байт 1 - номер спрайта.
@@ -38,60 +45,98 @@ static typedef struct Sprite {
 unsigned int Laser2_SPRT_ADR;  // Sprite file start address
 
 /*--------------------------------- Cut here ---------------------------------*/
-void Laser2_PTBL (signed char col, signed char row, unsigned char spn) __naked {
+void Laser2_ATOF_INSCR (void)
+{
 __asm
-             POP   BC
-             POP   HL        ; col, row
-             POP   DE        ; spn
+             LD    HL, #Laser2_ATOF0_IN
+             XOR   A
+             LD    (HL), A     ; "NOP"
+             INC   HL
+             LD    (HL), A     ; "NOP"
+             INC   HL
+             LD    (HL), A     ; "NOP"
+
+             LD    HL, #Laser2_ATOF_IN
+             LD    (HL), #0x09 ; "ADD HL, BC"
+             INC   HL
+             LD    (HL), #0x18 ; "JR  XX"
+             INC   HL
+             LD    (HL), #16   ; "XX"
+__endasm;
+} //Laser2_ATOF_INSCR
+
+/*--------------------------------- Cut here ---------------------------------*/
+void Laser2_ATON_INSCR (void)
+{
+__asm
+             LD    HL, #Laser2_ATOF0_IN
+             LD    (HL), #0x22 ; "LD (Laser2_ATOF_IN+1), HL"
+             INC   HL
+             LD    (HL), #<Laser2_ATOF_IN+1
+             INC   HL
+             LD    (HL), #>Laser2_ATOF_IN+1
+             
+             LD    A, #0x11    ; "LD DE, NN"
+             LD    (#Laser2_ATOF_IN), A
+__endasm;
+} //Laser2_ATON_INSCR
+
+/*--------------------------------- Cut here ---------------------------------*/
+
+void Laser2_PTBL_INSCR (signed char col, signed char row, unsigned char spn) __naked {
+__asm
+             POP   HL
+             POP   BC        ; C = col; B = row
+             POP   DE        ; E = spn
              PUSH  DE
-             PUSH  HL
              PUSH  BC
+             PUSH  HL
              XOR   A         ; NOP
-             JP    Laser2_PUT_SPRITE
+             JP    _Laser2_PUT_SPRITE_INSCR
 __endasm;
-} //Laser2_PTBL
+} //Laser2_PTBL_INSCR
 
 /*--------------------------------- Cut here ---------------------------------*/
-void Laser2_PTOR (signed char col, signed char row, unsigned char spn) __naked {
+void Laser2_PTOR_INSCR (signed char col, signed char row, unsigned char spn) __naked {
 __asm
-             POP   BC
-             POP   HL        ; col, row
-             POP   DE        ; spn
+             POP   HL
+             POP   BC        ; C = col; B = row
+             POP   DE        ; E = spn
              PUSH  DE
-             PUSH  HL
              PUSH  BC
+             PUSH  HL
              LD    A, #0xB6  ; OR (HL)
-             JP    Laser2_PUT_SPRITE
+             JP    _Laser2_PUT_SPRITE_INSCR
 __endasm;
-} //Laser2_PTOR
+} //Laser2_PTOR_INSCR
 
 /*--------------------------------- Cut here ---------------------------------*/
-void Laser2_PTXR (signed char col, signed char row, unsigned char spn) __naked {
+void Laser2_PTXR_INSCR (signed char col, signed char row, unsigned char spn) __naked {
 __asm
-             POP   BC
-             POP   HL        ; col, row
-             POP   DE        ; spn
+             POP   HL
+             POP   BC        ; C = col; B = row
+             POP   DE        ; E = spn
              PUSH  DE
-             PUSH  HL
              PUSH  BC
+             PUSH  HL
              LD    A, #0xAE  ; XOR (HL)
-             JP    Laser2_PUT_SPRITE
+             JP    _Laser2_PUT_SPRITE_INSCR
 __endasm;
-} //Laser2_PTXR
+} //Laser2_PTXR_INSCR
 
 /*--------------------------------- Cut here ---------------------------------*/
-void Laser2_PTND (signed char col, signed char row, unsigned char spn) __naked {
+void Laser2_PTND_INSCR (signed char col, signed char row, unsigned char spn) __naked {
 __asm
-             POP   BC
-             POP   HL        ; col, row
-             POP   DE        ; spn
+             POP   HL
+             POP   BC        ; C = col; B = row
+             POP   DE        ; E = spn
              PUSH  DE
-             PUSH  HL
              PUSH  BC
+             PUSH  HL
              LD    A, #0xA6  ; AND (HL)
-             JP    Laser2_PUT_SPRITE
+             JP    _Laser2_PUT_SPRITE_INSCR
 __endasm;
-} //Laser2_PTND
+} //Laser2_PTND_INSCR
 
 /*--------------------------------- Cut here ---------------------------------*/
 
@@ -107,8 +152,8 @@ HGT*LEN - атрибуты.
   ИТОГО: 9*HGT*LEN+5 байтов.
 */
 
-void Laser2_PUT_SPRITE (void) __naked {
-//  A: mode; L: col; H: row; D: spn
+void Laser2_PUT_SPRITE_OUTSCR (void) __naked {
+//  A: mode; C: col; D: row; E: spn
 __asm
              LD    (SPRT_MODE$), A ; Set draw mode
              LD    (SPRT_XY$+1), HL
@@ -117,12 +162,12 @@ FIND_BY_N$:  LD    A, (HL)         ; N of a sprite
              OR    A
              RET   Z
              INC   HL
-             CP    D               ; spn
+             CP    E               ; spn
              JR    Z, SPRT_FOUND$
              LD    C, (HL)
              INC   HL
              LD    B, (HL)
-             ADD   HL, BC
+             ADD   HL, BC          ; + offset to next sprite
              JR    FIND_BY_N$
 SPRT_FOUND$: INC   HL
              INC   HL
@@ -136,7 +181,7 @@ SAVE_LEN$:   LD    (SPRT_LINE8$+1), A
              LD    B, A            ; |len|
              SUB   (HL)
              NEG
-             LD    (SAVE_REST$+1)  ; rest := len - |len|
+             //LD    (SAVE_REST$+1)  ; rest := len - |len|
              INC   HL
              LD    A, #24          ; 24
              SUB   E               ; - row
@@ -308,3 +353,120 @@ PTBL_5    LD   DE,9
 
 __endasm;
 } //Laser2_PUT_SPRITE
+
+/*--------------------------------- Cut here ---------------------------------*/
+void Laser2_PUT_SPRITE_INSCR (void) __naked {
+//  A: mode; C: col; D: row; E: spn
+__asm
+.globl Laser2_ATOF0_IN
+.globl Laser2_ATOF_IN
+
+                 LD    (SPRT_MODE_IN$), A ; Set draw mode
+
+;Процедура расчёта адреса атрибутов из координат
+; (c) Д.Анисимов, г.Киров, 1996.
+; http://zxpress.ru/book_articles.php?id=633
+;Первый вариант (C = X; B = Y; 12B; 61T):
+                 LD    A, B            ;  4
+                 ADD   A, A            ;  4
+                 ADD   A, A            ;  4
+                 ADD   A, A            ;  4
+                 LD    L, A            ;  4
+                 LD    H, #0x16        ;  7
+                 ADD   HL, HL          ; 11
+                 ADD   HL, HL          ; 11
+                 LD    A, L            ;  4
+                 OR    C               ;  4
+                 LD    L, A            ;  4
+/*
+;Второй вариант (H = X; L = Y; 10B; 74T):
+                 LD    A, H            ;  4
+                 ADD   HL, HL          ; 11
+                 ADD   HL, HL          ; 11
+                 ADD   HL, HL          ; 11
+                 LD    H, #0x16        ;  7
+                 ADD   HL, HL          ; 11
+                 ADD   HL, HL          ; 11
+                 ADD   A, L            ;  4
+                 LD    L, A            ;  4
+*/
+;На выходе обеих процедур в HL - адрес файла атрибутов.
+Laser2_ATOF0_IN:
+                 LD    (Laser2_ATOF_IN+1), HL
+                 
+;Расчёт экранного адреса из адреса атрибутов
+; (c) Д.Анисимов, г.Киров, 1996.
+; http://zxpress.ru/book_articles.php?id=633
+;Вход: HL = адрес атрибутов
+                 LD    A, H            ; 4
+                 ADD   A, A            ; 4
+                 ADD   A, A            ; 4
+                 ADD   A, A            ; 4
+                 AND   #0x7F           ; 7
+                 LD    H, A            ; 4 = 27T
+;Выход: HL = экранный адрес
+                 LD    (SCR_ADR_IN$+1), HL
+                 
+                 LD    HL, (_Laser2_SPRT_ADR)
+FIND_BY_N_IN$:   LD    A, (HL)         ; N of a sprite
+                 OR    A
+                 RET   Z
+                 INC   HL
+                 CP    E               ; spn
+                 JR    Z, SPRT_FOUND_IN$
+                 LD    C, (HL)
+                 INC   HL
+                 LD    B, (HL)
+                 ADD   HL, BC          ; + offset to next sprite
+                 JR    FIND_BY_N_IN$
+SPRT_FOUND_IN$:  INC   HL
+                 INC   HL
+                 LD    A, (HL)         ; len
+                 LD    (SPRT_HGT_A_IN$+1), A
+                 LD    (SPRT_HGT_S_IN$+1), A
+                 INC   HL
+                 LD    B, (HL)         ; height of sprite
+                 INC   HL
+SPRT_HLINE_IN$:  PUSH  BC              ; Begin of loop on charlines
+SPRT_HGT_A_IN$:  LD    BC, #0
+Laser2_ATOF_IN:  LD    DE, #0          ; ATOF$: ADD HL, BC : JR XX [SCR_ADR_IN$]
+                 LDIR                         ; 2
+                 LD    E, L                   ; 1
+                 LD    D, H                   ; 1
+                 LD    HL, (Laser2_ATOF_IN+1) ; 3
+                 LD    BC, #0x20              ; 3
+                 ADD   HL, BC                 ; 1
+                 LD    (Laser2_ATOF_IN+1), HL ; 3
+                 LD    L, E                   ; 1
+                 LD    H, D                   ; 1 => 16
+                 
+SCR_ADR_IN$:     LD    DE, #0
+SPRT_HGT_S_IN$:  LD    BC, #0x0800     ; Height in a character fields (charlines)
+                 //PUSH  DE              ; Draw W lines (one line)
+SPRT_CHAR_IN$:   LD    A, (HL)
+SPRT_MODE_IN$:   NOP                   ; NOP | AND (HL) | OR (HL) | XOR (HL)
+                 LD    (DE), A
+                 INC   HL
+                 INC   D
+                 DJNZ  SPRT_CHAR_IN$
+                 LD    B, #8           ; Draw 8 bytes (one charline)
+                 LD    A, D
+                 SUB   A, B
+                 LD    D, A
+                 INC   E               ; Next screen line
+                 DEC   C
+                 JR    NZ, SPRT_CHAR_IN$
+                 //POP   DE
+                 LD    A, #0x20        ; Next charline
+                 ADD   E
+                 LD    D, A            ; If carry then jump to next third of screen
+                 JR    NC, CONTIN_1_3_IN$
+                 LD    A, #0x08        ; Next third of screen
+                 ADD   A, D
+                 LD    D, A            ; DE = DE + 0x0800
+CONTIN_1_3_IN$:  LD    (SCR_ADR_IN$+1), DE
+                 POP   BC
+                 DJNZ  SPRT_HLINE_IN$  ; End of loop on charlines (the same third)
+                 RET
+__endasm;
+} //Laser2_PUT_SPRITE_INSCR
