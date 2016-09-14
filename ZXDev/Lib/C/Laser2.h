42c800fc86ab556a26c9bdc01daea0699289eaaf
Oleg N. Cher <allot@bk.ru>
2016-09-14 05:19:55 +0300

    + ZXDev/Lib/Laser2.SETV; * minor fixes.

--- ZXDev/Lib/C/Laser2.h
+++ ZXDev/Lib/C/Laser2.h
@@ -68,12 +68,13 @@ extern unsigned int Laser2_SPRT_ADR;  // Sprite file start address
 #   define Laser2_PTND  Laser2_PTND_INSCR
 #   define Laser2_PTXR  Laser2_PTXR_INSCR
 #endif
-void Laser2_INVM (unsigned char spn) __z88dk_callee __preserves_regs(iyl,iyh);
+extern void Laser2_INVM (unsigned char spn) __z88dk_fastcall __preserves_regs(iyl,iyh);
 
 // Procedures for screen windows processing:
 
-void Laser2_CLSV (unsigned char col, unsigned char row, unsigned char len, unsigned char hgt) __z88dk_callee __preserves_regs(iyl,iyh);
-void Laser2_INVV (unsigned char col, unsigned char row, unsigned char len, unsigned char hgt) __z88dk_callee __preserves_regs(iyl,iyh);
+extern void Laser2_CLSV (unsigned char col, unsigned char row, unsigned char len, unsigned char hgt) __z88dk_callee __preserves_regs(iyl,iyh);
+extern void Laser2_INVV (unsigned char col, unsigned char row, unsigned char len, unsigned char hgt) __z88dk_callee __preserves_regs(iyl,iyh);
+extern void Laser2_SETV (unsigned char col, unsigned char row, unsigned char len, unsigned char hgt) __z88dk_callee __preserves_regs(iyl,iyh);
 
 
 #define Laser2__init()
