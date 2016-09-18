ac95b2a8dfa7b720a63832ef2a324160b9164a34
Oleg N. Cher <allot@bk.ru>
2016-09-19 00:55:09 +0300

    + ZXDev/Lib/Supercode. Attribute_Scr_Left (#39) & Attribute_Scr_Right (#40).

---
+++ ZXDev/Lib/C/Supercode.h
@@ -0,0 +1,15 @@
+#ifndef Supercode__h
+#define Supercode__h
+
+
+extern void Supercode_Attribute_Scr_Left (unsigned char x, unsigned char y,
+  unsigned char width, unsigned char height, unsigned char wrap,
+  unsigned char atr) __z88dk_callee;
+extern void Supercode_Attribute_Scr_Right (unsigned char x, unsigned char y,
+  unsigned char width, unsigned char height, unsigned char wrap,
+  unsigned char atr) __z88dk_callee;
+
+
+#define Supercode__init()
+
+#endif
