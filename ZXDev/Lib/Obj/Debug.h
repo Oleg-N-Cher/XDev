8e1f3d3d21dfed6743ae2ff497eb14944d2b0efe
Oleg N. Cher <allot@bk.ru>
2016-12-21 01:09:44 +0200

    + ZXDev/Lib/Debug (as a part of XDev.lib).

---
+++ ZXDev/Lib/Obj/Debug.h
@@ -0,0 +1,19 @@
+/* Ofront+ 0.9 -xtspkae */
+
+#ifndef Debug__h
+#define Debug__h
+
+#include "SYSTEM.h"
+
+
+
+
+import void Debug_CheckIX (void);
+import void Debug_CheckRegs (void);
+import void Debug_SaveIX (void);
+import void Debug_SaveRegs (void);
+import void Debug_SaveRegsDef (void);
+import void *Debug__init(void);
+
+
+#endif
