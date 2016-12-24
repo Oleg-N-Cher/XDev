c094c9cfe71d6466869f6ee1a6c4912f61b1cc3b
Oleg N. Cher <allot@bk.ru>
2016-12-25 00:09:32 +0200

    * ZXDev/Spiral11 example.

--- ZXDev/Obj/Spiral11.bat
+++ ZXDev/Obj/Spiral11.bat
@@ -16,5 +16,4 @@
 @REM Link the target to TAP format
 @REM =============================
 %bin%\bin2tap.exe -c 24999 -a %CodeAddr% -r %CodeAddr% -b -o ..\%Name%.tap %Name%.bin
-pause
 START ..\%Name%.tap
