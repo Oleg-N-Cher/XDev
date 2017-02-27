2ed1c0f0f84a01cf3fb4c79d671647cdead6080e
Oleg N. Cher <allot@bk.ru>
2017-02-27 23:57:06 +0200

    / ZXDev/Lib/GrTiles: optimize (__z88dk_callee).

--- ZXDev/Lib/C/GrTiles.c
+++ ZXDev/Lib/C/GrTiles.c
@@ -1,16 +1,13 @@
-void GrTiles_DrawTile8x8 (unsigned char x, unsigned char y, unsigned int tile);
+void GrTiles_DrawTile8x8 (unsigned char x, unsigned char y, unsigned int tile) __z88dk_callee;
 void GrTiles_DrawMonoTile8x8 (
-  unsigned char x, unsigned char y, unsigned int tile, unsigned char colors);
+  unsigned char x, unsigned char y, unsigned int tile, unsigned char colors) __z88dk_callee;
 /*================================== Header ==================================*/
 
-void GrTiles_DrawTile8x8 (unsigned char x, unsigned char y, unsigned int tile) {
+void GrTiles_DrawTile8x8 (unsigned char x, unsigned char y, unsigned int tile) __z88dk_callee {
 __asm
-  POP  BC
+  POP  HL
   POP  DE      ; D = y; E = x
-  POP  HL      ; tile address
-  PUSH HL
-  PUSH DE
-  PUSH BC
+  EX   (SP),HL ; tile address
   LD   A,D     ; Здесь вычисляется адрес
   RRCA         ;  строки, номер которой задан
   RRCA         ;  в регистре A
@@ -49,14 +46,10 @@ __endasm;
 
 /*--------------------------------- Cut here ---------------------------------*/
 void GrTiles_DrawMonoTile8x8 (
-    unsigned char x, unsigned char y, unsigned int tile, unsigned char colors) {
+    unsigned char x, unsigned char y, unsigned int tile, unsigned char colors) __z88dk_callee {
 __asm
-  LD   HL, #2
-  ADD  HL, SP
-  LD   E, (HL) // E = x
-  INC  HL
-  LD   D, (HL) // D = y
-  INC  HL
+  POP  BC
+  POP  DE      ; D = y; E = x
 ; ====================================
   LD   A,D     ; Здесь вычисляется адрес
   RRCA         ;  строки, номер которой задан
@@ -70,13 +63,11 @@ __asm
   OR   #0x40
   LD   D,A     ; Счётчик цикла рисования
 ; ====================================
-  LD   A, (HL)
-  INC  HL
-  LD   B, (HL) // BA = tile address
-  INC  HL
-  LD   C, (HL) // C = colors
-  LD   L, A
-  LD   H, B    ; HL = tile address
+  POP  HL      ; HL = tile address
+  DEC  SP
+  POP  AF      ; A = colors
+  PUSH BC
+  LD   C,A     ; C = colors
 ; ====================================
   LD   B,#7
 DRLOOPM$:
--- ZXDev/Lib/C/GrTiles.h
+++ ZXDev/Lib/C/GrTiles.h
@@ -2,9 +2,8 @@
 #define GrTiles__h
 
 
-extern void GrTiles_DrawTile8x8 (unsigned char x, unsigned char y, unsigned int tile);
-extern void GrTiles_DrawMonoTile8x8 (unsigned char x, unsigned char y, unsigned int tile, unsigned char colors);
-extern void GrTiles_DrawTranspMonoTile8x8 (unsigned char x, unsigned char y, unsigned int tile, unsigned char colors);
+extern void GrTiles_DrawTile8x8 (unsigned char x, unsigned char y, unsigned int tile) __z88dk_callee __preserves_regs(c,iyl,iyh);
+extern void GrTiles_DrawMonoTile8x8 (unsigned char x, unsigned char y, unsigned int tile, unsigned char colors) __z88dk_callee __preserves_regs(iyl,iyh);
 
 #define GrTiles_DrawTile GrTiles_DrawTile8x8
 #define GrTiles_DrawMonoTile GrTiles_DrawMonoTile8x8
