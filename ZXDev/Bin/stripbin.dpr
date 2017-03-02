fe08ee6d032f6d54dea18b07073527671e672528
Oleg N. Cher <allot@bk.ru>
2017-03-02 22:00:24 +0200

    * ZXDev*/Bin/stripbin: * correct work with 1A FF FF FF FF FF

--- ZXDev/Bin/stripbin.dpr
+++ ZXDev/Bin/stripbin.dpr
@@ -5,28 +5,29 @@ USES Windows, KOL;
 
 {$R *.RES}
 CONST
-  PODRYAD = 16;
+  PODRYAD = 3;
 VAR
   bin: AnsiString;
-  binLen, i, idx, ff: INTEGER;
+  binLen, i, idx, ff: INTEGER; lastByte: CHAR;
 BEGIN
   bin := StrLoadFromFile(ParamStr(1));
   binLen := LENGTH(bin);
+  lastByte := bin[binLen]; IF NOT (lastByte IN [#$1A, #$FF]) THEN lastByte := #$FF;
   idx := binLen;
   ff := 0;
   FOR i := binLen DOWNTO 1 DO BEGIN
     idx := i;
-    IF bin[i] IN [#$1A, #$FF] THEN
+    IF bin[i] = lastByte THEN
       INC(ff)
     ELSE
       ff := 0;
     IF ff = PODRYAD THEN BREAK
   END;
   FOR i := idx DOWNTO 1 DO BEGIN
-    IF NOT (bin[i] IN [#$1A, #$FF]) THEN BEGIN idx := i; BREAK END
+    IF bin[i] <> lastByte THEN BEGIN idx := i; BREAK END
   END;
   WriteLn('New size is ', idx);
   WriteLn('You win ', binLen - idx, ' bytes');
   DELETE(bin, idx + 1, binLen);
   StrSaveToFile(ParamStr(1), bin)
-END.
+END {stripbin}.
