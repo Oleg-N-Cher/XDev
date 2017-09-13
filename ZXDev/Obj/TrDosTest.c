cdb7fbd32a5ec0a9637138fe6f146cabb9b656b7
Oleg N. Cher <allot@bk.ru>
2017-09-13 19:54:18 +0300

    * ZXDev/TrDosTest.

--- ZXDev/Obj/TrDosTest.c
+++ ZXDev/Obj/TrDosTest.c
@@ -1,4 +1,4 @@
-/*  Ofront 1.2 -xtspkaem */
+/* Ofront+ 0.9 -sm */
 #include "SYSTEM.h"
 #include "Console.h"
 #include "TrDos.h"
@@ -12,28 +12,28 @@ static CHAR TrDosTest_code;
 /*============================================================================*/
 
 
-export main(int argc, char **argv)
+int main(int argc, char **argv)
 {
 	__INIT(argc, argv);
 	__IMPORT(Console__init);
 	__IMPORT(TrDos__init);
 	__REGMAIN("TrDosTest", 0);
 /* BEGIN */
-	TrDosTest_code = TrDos_FindFile((CHAR*)"file1", (LONGINT)6, 0x05);
-	Console_WriteStr((CHAR*)"Found code: ", (LONGINT)13);
-	Console_WriteInt((int)TrDosTest_code);
+	TrDosTest_code = TrDos_FindFile((CHAR*)"file1", 6, 0x05);
+	Console_WriteStr((CHAR*)"Found code: ", 13);
+	Console_WriteInt((INTEGER)TrDosTest_code);
 	Console_WriteLn();
-	TrDosTest_code = TrDos_FindFile((CHAR*)"boot", (LONGINT)5, 0x04);
-	Console_WriteStr((CHAR*)"Found code: ", (LONGINT)13);
-	Console_WriteInt((int)TrDosTest_code);
+	TrDosTest_code = TrDos_FindFile((CHAR*)"boot", 5, 0x04);
+	Console_WriteStr((CHAR*)"Found code: ", 13);
+	Console_WriteInt((INTEGER)TrDosTest_code);
 	Console_WriteLn();
-	TrDosTest_code = TrDos_FindFile((CHAR*)"Das2", (LONGINT)5, 0x04);
-	Console_WriteStr((CHAR*)"Found code: ", (LONGINT)13);
-	Console_WriteInt((int)TrDosTest_code);
+	TrDosTest_code = TrDos_FindFile((CHAR*)"Das2", 5, 0x04);
+	Console_WriteStr((CHAR*)"Found code: ", 13);
+	Console_WriteInt((INTEGER)TrDosTest_code);
 	Console_WriteLn();
-	TrDosTest_code = TrDos_FindFile((CHAR*)"Dash", (LONGINT)5, 0x04);
-	Console_WriteStr((CHAR*)"Found code: ", (LONGINT)13);
-	Console_WriteInt((int)TrDosTest_code);
+	TrDosTest_code = TrDos_FindFile((CHAR*)"Dash", 5, 0x04);
+	Console_WriteStr((CHAR*)"Found code: ", 13);
+	Console_WriteInt((INTEGER)TrDosTest_code);
 	Console_WriteLn();
 	__FINI;
 }
