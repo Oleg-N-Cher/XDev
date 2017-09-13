5c8d36cf39e06b58bdd007ce5af2a65c0d2da8d8
Oleg N. Cher <allot@bk.ru>
2017-09-13 19:29:20 +0300

    * ZXDev/Unsigned.

--- ZXDev/Obj/Unsigned.bat
+++ ZXDev/Obj/Unsigned.bat
@@ -1,26 +1,4 @@
-@REM Compile Unsigned.c
-@REM ==================
+@SET Options=--no-std-crt0 --reserve-regs-iy
+@SET Target=REM
 
-@SET bin=..\Bin
-@SET lib=..\Lib
-@SET obj=..\..\Ofront\Obj
-
-@IF EXIST %obj%\Unsigned.h @DEL %obj%\Unsigned.h
-@IF EXIST %obj%\Unsigned.sym @DEL %obj%\Unsigned.sym
-@IF EXIST %obj%\Unsigned.c @MOVE /Y %obj%\Unsigned.c
-%bin%\sdcc -mz80 --code-loc 26000 --data-loc 0xF800 --opt-code-size -I %lib% -I %lib%\C -I %lib%\Obj -L %lib% --disable-warning 59 --disable-warning 85 --disable-warning 158 Unsigned.c Basic.lib
-@IF errorlevel 1 PAUSE
-
-@REM Convert Intel hex format to binary
-@REM ==================================
-%bin%\hex2bin Unsigned.ihx
-
-@REM Link the target to TAP format
-@REM =============================
-%bin%\bin2data.exe -rem -org 26000 Unsigned.bin ..\Unsigned.tap Unsigned
-
-@REM =====================================================================
-@ECHO Now you can run the image "Unsigned.tap" in your favourite emulator.
-@REM =====================================================================
-
-START ..\Unsigned.tap
+@..\Bin\build Unsigned
