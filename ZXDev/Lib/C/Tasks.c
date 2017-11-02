fa90d52c365b247c31a673ebfe33e828984bad3b
Oleg N. Cher <allot@bk.ru>
2017-11-02 20:55:14 +0200

    * ZXDev/Lib/Tasks: SpawnEx rewritten in asm; minor fixes.

--- ZXDev/Lib/C/Tasks.c
+++ ZXDev/Lib/C/Tasks.c
@@ -13,21 +13,22 @@ typedef
     // int IX, PC, _RETURN;
   } Tasks_Context;
 
-unsigned char Tasks_count; int Tasks_myid; // Both must go together
+int Tasks_myid; unsigned char Tasks_count; // Both must go together
 int Tasks_current;
 
 /*----------------------------------------------------------------------------*/
 void _Tasks_RETURN (void) __naked {
   __asm
-           LD   HL, #_Tasks_count
-           DEC  (HL)                 ; DEC(count)
-           INC  HL
+           XOR  A                    ;
+           LD   HL, #_Tasks_myid     ;
            LD   (HL), A              ;
            INC  HL                   ; myid := 0
            LD   (HL), A              ;
+           INC  HL                   ;
+           DEC  (HL)                 ; DEC(count)
 
 // Load Run() context (IX, SP)
-LOAD_RUN$: LD   HL, (_Tasks_current) ; ADR(Context.SP)
+           LD   HL, (_Tasks_current) ; ADR(Context.SP)
            JP   __Run_SP
 
 // Remove completed task, update Context.next
@@ -37,21 +38,21 @@ LOAD_RUN$: LD   HL, (_Tasks_current) ; ADR(Context.SP)
 
 /*----------------------------------------------------------------------------*/
 void Tasks_Spawn_Ex (Tasks_Context *ctx, unsigned int size, void (*proc)(void))
-  /*__z88dk_callee*/ {
+  __naked __z88dk_callee {
 
 // Context = RECORD
 //   SP, next: INTEGER;
 //   stack: ARRAY N OF BYTE;
 //   IX, PC, _RETURN: INTEGER;
 // END;
-
+/*
   // Find the last task in list
   if( Tasks_count == 0 ) {
     Tasks_current = (int)ctx; ctx->next = (int)ctx;
   } else { // Tasks_count >= 1
     int found = Tasks_current;
     for(;;) {
-      if( found == (int)ctx ) { Tasks_current = found; return; } // Already run
+      if( found == (int)ctx ) HALT(25); // Already run
       if (((Tasks_Context*)found)->next == Tasks_current) break;
       found = ((Tasks_Context*)found)->next;
     }
@@ -70,6 +71,107 @@ void Tasks_Spawn_Ex (Tasks_Context *ctx, unsigned int size, void (*proc)(void))
     ctx->SP = (int)stack;                 // task's SP
     Tasks_count++;
   }
+*/
+  __asm
+           LD   HL, #_Tasks_count
+           LD   A, (HL)
+           INC  (HL) // Tasks_count++;
+    // void Tasks_Spawn_Ex (
+    //   Tasks_Context *ctx, unsigned int size, void (*proc)(void))
+    //     __z88dk_callee {
+           POP  HL
+           EX   (SP), HL             ; ctx
+    // if( Tasks_count == 0 ) {
+           OR   A
+           JR   NZ, TASK_EXIST$
+    //   Tasks_current = (int)ctx; ctx->next = (int)ctx;
+           LD   (_Tasks_current), HL
+           LD   E, L
+           LD   D, H
+           INC  HL
+           INC  HL
+           LD   (HL), E
+           INC  HL
+           LD   (HL), D
+    // } else { // Tasks_count >= 1
+           JR   CREATE_STK$
+TASK_EXIST$:
+    //   int found = Tasks_current;
+           EX   DE, HL               ; DE = ctx
+           LD   HL, (_Tasks_current) ; HL = found
+           LD   C, L                 ;
+           LD   B, H                 ; BC = current
+    //   for(;;) {
+FIND_TASK$:
+
+#ifdef SAFE
+    //     if( found == (int)ctx ) HALT(25);
+           LD   A, L
+           CP   E
+           JR   NZ, NOT_EQUAL$
+           LD   A, H
+           CP   D
+           JR   NZ, NOT_EQUAL$
+           RST  8
+           .DB  25
+NOT_EQUAL$:
+#endif
+
+    //     if (((Tasks_Context*)found)->next == Tasks_current) break;
+           INC  HL
+           INC  HL
+           LD   A, (HL)
+           CP   C
+           JR   NZ, FIND_TASK$
+           INC  HL
+           LD   A, (HL)
+           DEC  HL
+           CP   B
+           JR   NZ, FIND_TASK$
+    //     found = ((Tasks_Context*)found)->next;
+    //   }
+    //   ((Tasks_Context*)found)->next = (int)ctx;
+           LD   (HL), E
+           INC  HL
+           LD   (HL), D
+    //   ctx->next = (int)Tasks_current;
+           LD   L, E
+           LD   H, D
+           INC  HL
+           INC  HL
+           LD   (HL), C
+           INC  HL
+           LD   (HL), B
+    // }
+CREATE_STK$:                         ; DE = ctx
+           POP  AF
+           POP  HL                   ; HL = size
+           POP  BC                   ; BC = proc
+           PUSH AF
+    // int *stack = (int*)((int)ctx + size); // Top of stack
+           ADD  HL, DE               ; ctx + size
+    // stack--;                              // 2 bytes for RETURN
+    // *stack = (int)_Tasks_RETURN;          // RETURN entry point
+           DEC  HL
+           LD   (HL), #>__Tasks_RETURN
+           DEC  HL
+           LD   (HL), #<__Tasks_RETURN
+    // stack--;                              // 2 bytes for PC
+    // *stack = (int)proc;                   // task's entry point
+           DEC  HL
+           LD   (HL), B
+           DEC  HL
+           LD   (HL), C
+    // stack--;                              // 2 bytes for IX
+           DEC  HL
+           DEC  HL
+    // ctx->SP = (int)stack;                 // task's SP
+           EX   DE, HL
+           LD   (HL), E
+           INC  HL
+           LD   (HL), D
+           RET
+  __endasm;
 } //Tasks_Spawn_Ex
 
 /*----------------------------------------------------------------------------*/
@@ -77,9 +179,12 @@ void Tasks_Run (void) __naked { // Run() MUST be started in "main" task
   __asm
 
 #ifdef SAFE
+           LD   A, (_Tasks_count)    ;
+           OR   A                    ;
+           RET  Z                    ; IF count = 0 THEN RETURN
            LD   A, (_Tasks_myid+1)   ;
            OR   A                    ;
-           JR   NZ, HALT_22$         ; IF myid # 0 THEN HALT(22)
+           JR   NZ, HALT_22$         ; IF myid IN {0..255} THEN HALT(22)
 #endif
 
 // Save IX, SP
@@ -158,13 +263,13 @@ HALT_23$:  RST  8
 /*----------------------------------------------------------------------------*/
 void Tasks__init (void) {
   __asm
-           LD   HL, #_Tasks_count
            XOR  A                    ;
-           LD   (HL), A              ; count = 0
-           INC  HL
+           LD   HL, #_Tasks_myid     ;
            LD   (HL), A              ;
-           INC  HL                   ; myid = 0
+           INC  HL                   ; myid := 0
            LD   (HL), A              ;
+           INC  HL                   ;
+           LD   (HL), A              ; count := 0
   __endasm;
 
 } //Tasks__init
