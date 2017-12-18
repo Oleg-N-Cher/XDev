41013638bfb7abd6861168d0528fa028313e621c
Oleg N. Cher <allot@bk.ru>
2017-12-18 23:03:38 +0200

    - WinDev/MoveWindow example.

--- WinDev/Obj64/MoveWindow.bat
+++
@@ -1,9 +0,0 @@
-@SET Bin=..\Bin
-@SET Lib=..\Lib
-@SET tcc=%Bin%\tcc64\tcc
-@CD ..\Obj64
-
-%tcc% MoveWindow.c -D_WINGUI -I "." -I %Lib% -I %Lib%\C -I %Lib%\Obj64 %Lib%\XDev64.a %Bin%\tcc64\lib\kernel32.def -o MoveWindow.exe
-@IF errorlevel 1 PAUSE
-@IF EXIST MoveWindow.exe MOVE MoveWindow.exe ..\x64
-@START ..\x64\MoveWindow.exe
