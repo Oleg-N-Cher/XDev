20db58693c3f7b89e0abf3426db2771f1b642eac
Oleg N. Cher <allot@bk.ru>
2019-08-02 04:03:54 +0300

    * ZXDev/C/SYSTEM.h: - __Module_Entry_Point__$ => .

--- ZXDev/Lib/C/SYSTEM.h
+++ ZXDev/Lib/C/SYSTEM.h
@@ -90,7 +90,7 @@ extern void SYSTEM_ENUMR();
 #  define __REGMOD(name, enum)	if(m==0)m=SYSTEM_REGMOD((CHAR*)name,enum); else return m
 #  define __ENDMOD	return m
 #else
-#  define __DEFMOD	__asm __Module_Entry_Point__$: LD HL,#__Module_Entry_Point__$ \
+#  define __DEFMOD	__asm LD HL,#. \
      LD (HL),#0xC9 \
      __endasm; // static char m; if(m!=0)return
 #  define __REGMOD(name, enum)
