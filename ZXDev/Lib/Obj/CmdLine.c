5a0e89b5b19bc53615cfa78e37ac063ad75737eb
Oleg N. Cher <allot@bk.ru>
2019-08-19 01:42:49 +0300

    * ZXDev: "@" signator for actual VAR-parameters in Oberon-3. Thanks to Andrey Golyantov - http://zx.oberon2.ru/forum/viewtopic.php?f=32&t=460#p3428

--- ZXDev/Lib/Obj/CmdLine.c
+++ ZXDev/Lib/Obj/CmdLine.c
@@ -18,12 +18,3 @@ void CmdLine_GetParam (BYTE n, CHAR *param, SHORTINT param__len)
 	param[0] = 0x00;
 }
 
-/*----------------------------------------------------------------------------*/
-
-export void *CmdLine__init(void)
-{
-	__DEFMOD;
-	__REGMOD("CmdLine", 0);
-/* BEGIN */
-	__ENDMOD;
-}
