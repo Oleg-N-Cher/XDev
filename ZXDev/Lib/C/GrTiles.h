30dc6a126956fb89e5d982cad84acb53fdb82e91
Oleg N. Cher <allot@bk.ru>
2019-09-12 14:09:26 +0300

    / ZXDev/Lib/GrTiles: + GetTile8x8, GetTile, GetMonoTile8x8, GetMonoTile.

--- ZXDev/Lib/C/GrTiles.h
+++ ZXDev/Lib/C/GrTiles.h
@@ -4,10 +4,14 @@
 
 extern void GrTiles_DrawTile8x8 (unsigned char x, unsigned char y, unsigned int tile) __z88dk_callee __preserves_regs(c,iyl,iyh);
 extern void GrTiles_DrawMonoTile8x8 (unsigned char x, unsigned char y, unsigned int tile, unsigned char colors) __z88dk_callee __preserves_regs(iyl,iyh);
+extern void GrTiles_GetTile8x8 (unsigned char x, unsigned char y, unsigned int tile) __z88dk_callee __preserves_regs(c,iyl,iyh);
+extern void GrTiles_GetMonoTile8x8 (unsigned char x, unsigned char y, unsigned int tile) __z88dk_callee __preserves_regs(c,iyl,iyh);
 
 #define GrTiles_DrawTile GrTiles_DrawTile8x8
 #define GrTiles_DrawMonoTile GrTiles_DrawMonoTile8x8
 #define GrTiles_DrawTranspMonoTile GrTiles_DrawMonoTile8x8
+#define GrTiles_GetTile GrTiles_GetTile8x8
+#define GrTiles_GetMonoTile GrTiles_GetMonoTile8x8
 
 #define GrTiles__init()
 
