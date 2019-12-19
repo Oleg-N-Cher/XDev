9f72c40ee70d3b6fc1b78991719e2a30b617973c
Oleg N. Cher <allot@bk.ru>
2019-12-19 09:45:09 +0200

    + ZXDev/Lib/GrFonts.MsDosCgaRus8x8; + TestFontRus example.

--- ZXDev/Lib/C/GrFonts.h
+++ ZXDev/Lib/C/GrFonts.h
@@ -4,13 +4,15 @@
 typedef
 	int GrFonts_Font;
 
-extern signed char GrFonts_ZxSpecRus8x8Font [768];
-extern signed char GrFonts_MsDosCga8x8Font [768];
+extern unsigned char GrFonts_ZxSpecRus8x8Font [768];
+extern unsigned char GrFonts_MsDosCga8x8Font [768];
+extern unsigned char GrFonts_MsDosCgaRus8x8 [1792];
 
 
 #define GrFonts_ZxSpecRom8x8 0x3D00
 #define GrFonts_ZxSpecRus8x8 (int)GrFonts_ZxSpecRus8x8Font
 #define GrFonts_MsDosCga8x8 (int)GrFonts_MsDosCga8x8Font
+#define GrFonts_MsDosCgaRus8x8 (int)GrFonts_MsDosCgaRus8x8
 
 
 #define GrFonts__init()
