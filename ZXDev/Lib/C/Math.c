63f54469beb05244bc70a34ec980c874905d236a
Oleg N. Cher <allot@bk.ru>
2020-02-10 23:17:28 +0200

    / ZXDev/Lib/Math: + Pow; optimized.

--- ZXDev/Lib/C/Math.c
+++ ZXDev/Lib/C/Math.c
@@ -1,72 +1,12 @@
-float Math_ArcTan (float x);
-float Math_Cos (float x);
-float Math_Exp (float x);
-float Math_Ln (float x);
-float Math_Sin (float x);
-float Math_Sqrt (float x);
-unsigned int Math_RndRange (unsigned int min, unsigned int max);
-void Math_Randomize (void);
-
-//#include <math.h>
-extern float atanf (const float x);
-extern float cosf (const float x);
-extern float expf (const float x);
-extern float logf (const float x);
-extern float sinf (const float x);
-extern float sqrtf (const float x);
-
-#define Math_arctan(x)	(atanf(x))
-#define Math_cos(x)	(cosf(x))
-#define Math_exp(x)	(expf(x))
-#define Math_ln(x)	(logf(x))
-#define Math_sin(x)	(sinf(x))
-#define Math_sqrt(x)	(sqrtf(x))
+extern unsigned int Math_RndRange (unsigned int min, unsigned int max);
+extern void Math_Randomize (void);
 
 /*================================== Header ==================================*/
-float Math_Sin (float x)
-{
-	return Math_sin(x);
-}
-
-/*--------------------------------- Cut here ---------------------------------*/
-float Math_Cos (float x)
-{
-	return Math_cos(x);
-}
-
-/*--------------------------------- Cut here ---------------------------------*/
-float Math_ArcTan (float x)
-{
-	return Math_arctan(x);
-}
-
-/*--------------------------------- Cut here ---------------------------------*/
-float Math_Exp (float x)
-{
-	return Math_exp(x);
-}
-
-/*--------------------------------- Cut here ---------------------------------*/
-float Math_Sqrt (float x)
-{
-	return Math_sqrt(x);
-}
-
-/*--------------------------------- Cut here ---------------------------------*/
-float Math_Ln (float x)
-{
-	return Math_ln(x);
-}
-
-/*--------------------------------- Cut here ---------------------------------*/
-/* SEED_RND address */
-#define SF_RND$ 0x5C76
-
-static unsigned int __Math_RandBB (void) /* Ripped from Beta Basic */
+static unsigned int __Math_RandBB (void) // Ripped from Beta Basic
 {
 __asm
   LD   D,#0
-  LD   BC,(#SF_RND$)
+  LD   BC,(0x5C76) ; SEED_RND
   LD   H,C
   LD   L,#0xFD
   LD   A,B
@@ -80,7 +20,7 @@ __asm
   JR   NC,R1$
   INC  HL
 R1$:
-  LD  (#SF_RND$),HL
+  LD  (0x5C76),HL
 __endasm;
 } //__Math_RandBB
 
