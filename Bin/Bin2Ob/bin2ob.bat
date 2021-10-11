44296a65434699b50b02c3c59eb257d5bf1d6074
Oleg N. Cher <allot@bk.ru>
2021-10-12 02:29:59 +0300

    * Bin/Bin2Ob

--- Bin/Bin2Ob/bin2ob.bat
+++ Bin/Bin2Ob/bin2ob.bat
@@ -1,5 +1,5 @@
 @ECHO OFF
-ECHO MODULE Rsrc;           > Rsrc.ob3
+ECHO MODULE [noinit] Rsrc;  > Rsrc.ob3
 bin2ob.exe Test\ball.png   >> Rsrc.ob3
 bin2ob.exe Test\bat.png    >> Rsrc.ob3
 bin2ob.exe Test\bricks.png >> Rsrc.ob3
