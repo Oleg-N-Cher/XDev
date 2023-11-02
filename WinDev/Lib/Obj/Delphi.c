31b1fbb16e5e90a0f7769c062bf760052a90a432
Oleg N. Cher <allot@bk.ru>
2023-11-02 03:33:59 +0200

    / WinDev/Lib/Delphi: + CompareMem

--- WinDev/Lib/Obj/Delphi.c
+++ WinDev/Lib/Obj/Delphi.c
@@ -4,6 +4,7 @@
 
 
 
+export BOOLEAN Delphi_CompareMem (INTEGER p1, INTEGER p2, INTEGER length);
 export INTEGER Delphi_HiWord (INTEGER i);
 export INTEGER Delphi_LoWord (INTEGER i);
 
@@ -19,6 +20,36 @@ INTEGER Delphi_HiWord (INTEGER i)
 INTEGER Delphi_LoWord (INTEGER i)
 {
   return (INTEGER)((SET)i & 0xffff);
+}
+
+/*----------------------------------------------------------------------------*/
+BOOLEAN Delphi_CompareMem (INTEGER p1, INTEGER p2, INTEGER length)
+{
+  BYTE byte1, byte2;
+  INTEGER word1, word2;
+  if (__MASK(p1, -4) == 0 && __MASK(p2, -4) == 0) {
+    while (length >= 4) {
+      __GET(p1, word1, INTEGER);
+      __GET(p2, word2, INTEGER);
+      if (word1 != word2) {
+        return 0;
+      }
+      p1 += 4;
+      p2 += 4;
+      length -= 4;
+    }
+  }
+  while (length > 0) {
+    __GET(p1, byte1, BYTE);
+    __GET(p2, byte2, BYTE);
+    if (byte1 != byte2) {
+      return 0;
+    }
+    p1 += 1;
+    p2 += 1;
+    length -= 1;
+  }
+  return 1;
 }
 
 /*----------------------------------------------------------------------------*/
--- WinDev/Lib/Obj64/Delphi.c
+++ WinDev/Lib/Obj64/Delphi.c
@@ -4,6 +4,7 @@
 
 
 
+export BOOLEAN Delphi_CompareMem (LONGINT p1, LONGINT p2, INTEGER length);
 export INTEGER Delphi_HiWord (INTEGER i);
 export INTEGER Delphi_LoWord (INTEGER i);
 
@@ -19,6 +20,36 @@ INTEGER Delphi_HiWord (INTEGER i)
 INTEGER Delphi_LoWord (INTEGER i)
 {
   return (INTEGER)((SET)i & 0xffff);
+}
+
+/*----------------------------------------------------------------------------*/
+BOOLEAN Delphi_CompareMem (LONGINT p1, LONGINT p2, INTEGER length)
+{
+  BYTE byte1, byte2;
+  LONGINT word1, word2;
+  if (__MASK(p1, -8) == 0 && __MASK(p2, -8) == 0) {
+    while (length >= 8) {
+      __GET(p1, word1, LONGINT);
+      __GET(p2, word2, LONGINT);
+      if (word1 != word2) {
+        return 0;
+      }
+      p1 += 8;
+      p2 += 8;
+      length -= 8;
+    }
+  }
+  while (length > 0) {
+    __GET(p1, byte1, BYTE);
+    __GET(p2, byte2, BYTE);
+    if (byte1 != byte2) {
+      return 0;
+    }
+    p1 += 1;
+    p2 += 1;
+    length -= 1;
+  }
+  return 1;
 }
 
 /*----------------------------------------------------------------------------*/
